8204d3f9b7e87d8e7b5d03aced475a3d502972a1
hurricup <hurricup@gmail.com>
2015-05-26 10:31:42 +0300

    Implemented code folding for import groups

--- src/com/perl5/lang/perl/idea/PerlFoldingBuilder.java
+++ src/com/perl5/lang/perl/idea/PerlFoldingBuilder.java
@@ -26,6 +26,10 @@ import com.intellij.psi.templateLanguages.OuterLanguageElementImpl;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.perl5.lang.perl.lexer.PerlElementTypes;
+import com.perl5.lang.perl.psi.PerlRequireStatement;
+import com.perl5.lang.perl.psi.PerlRequireTerm;
+import com.perl5.lang.perl.psi.PerlStatementModifier;
+import com.perl5.lang.perl.psi.PerlUseStatement;
 import com.perl5.lang.perl.psi.impl.PerlAnonArrayImpl;
 import com.perl5.lang.perl.psi.impl.PerlAnonHashImpl;
 import com.perl5.lang.perl.psi.impl.PerlBlockImpl;
@@ -59,10 +63,17 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		descriptors.addAll(getDescriptorsFor(root, document, PsiComment.class, 0, 1));
 
 		descriptors.addAll(getCommentsDescriptors(root, document));
+		descriptors.addAll(getImportDescriptors(root, document));
 
 		return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
 	}
 
+	/**
+	 * Searching for sequential comments (starting from newline or subblock beginning) and making folding descriptors for such blocks of size > 1
+	 * @param root	root to search in
+	 * @param document	document to search in
+	 * @return	list of FoldingDescriptros
+	 */
 	private List<FoldingDescriptor> getCommentsDescriptors(@NotNull PsiElement root, @NotNull Document document)
 	{
 		List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
@@ -110,7 +121,7 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 					PsiElement currentComment = comment;
 					int commentsNumber = 0;
 
-					while (currentComment.getNextSibling() != null)
+					while (currentComment != null)
 					{
 						if (currentComment.getNode().getElementType() == PerlElementTypes.PERL_COMMENT)
 						{
@@ -118,18 +129,17 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 							commentsNumber++;
 						}
 
-						IElementType nextType = currentComment.getNextSibling().getNode().getElementType();
-						if (nextType != PerlElementTypes.PERL_COMMENT && nextType != TokenType.WHITE_SPACE && nextType != TokenType.NEW_LINE_INDENT)
-						{
-							if (blockEnd != blockStart && commentsNumber > 1)
-							{
-								currentOffset = blockEnd;
-								descriptors.add(new FoldingDescriptor(blockNode, new TextRange(blockStart, blockEnd)));
-							}
+						IElementType tokenType = currentComment.getNode().getElementType();
+						if (tokenType != PerlElementTypes.PERL_COMMENT && tokenType != TokenType.WHITE_SPACE && tokenType != TokenType.NEW_LINE_INDENT)
 							break;
-						}
 
 						currentComment = currentComment.getNextSibling();
+					}
+
+					if (blockEnd != blockStart && commentsNumber > 1)
+					{
+						currentOffset = blockEnd;
+						descriptors.add(new FoldingDescriptor(blockNode, new TextRange(blockStart, blockEnd)));
 					}
 				}
 			}
@@ -138,6 +148,68 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		return descriptors;
 	}
 
+	/**
+	 * Searching for sequential uses and requires, ignoring stament modifieers, comments, pods and whitespaces and making folding descriptors for such blocks of size > 1
+	 * @param root	root to search in
+	 * @param document	document to search in
+	 * @return	list of FoldingDescriptros
+	 */
+	private List<FoldingDescriptor> getImportDescriptors(@NotNull PsiElement root, @NotNull Document document)
+	{
+		List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
+
+		Collection<PsiElement> imports = PsiTreeUtil.findChildrenOfAnyType(root, PerlUseStatement.class, PerlRequireStatement.class);
+
+		int currentOffset = 0;
+
+		for( PsiElement perlImport: imports)
+		{
+			if( currentOffset < perlImport.getTextOffset() )
+			{
+				int blockStart = perlImport.getTextOffset();
+				int blockEnd = blockStart;
+				ASTNode blockNode = perlImport.getNode();
+				PsiElement currentImport = perlImport;
+
+				int importsNumber = 0;
+
+				while (currentImport != null)
+				{
+					if (currentImport instanceof PerlUseStatement || currentImport instanceof PerlStatementModifier || currentImport instanceof PerlRequireStatement)
+					{
+						blockEnd = currentImport.getTextOffset() + currentImport.getTextLength() - 1;
+						if( !(currentImport instanceof PerlStatementModifier) )
+							importsNumber++;
+					}
+					else if( currentImport.getNode().getElementType() == PerlElementTypes.PERL_SEMI )
+						blockEnd = currentImport.getTextOffset() + 1;
+					else if( !(currentImport instanceof PsiComment || currentImport instanceof PsiWhiteSpace ) )
+						break;
+
+					currentImport = currentImport.getNextSibling();
+				}
+
+				if (blockEnd != blockStart && importsNumber > 1)
+				{
+					currentOffset = blockEnd;
+					descriptors.add(new FoldingDescriptor(blockNode, new TextRange(blockStart, blockEnd)));
+				}
+			}
+		}
+
+		return descriptors;
+	}
+
+	/**
+	 * Finding psi elements of specific types and add Folding descriptor for them if they are more than certain lines lenght
+	 * @param root root element for searching
+	 * @param document	document for searching
+	 * @param c	PsiElement class to search for
+	 * @param startMargin beginning margin for collapsable block
+	 * @param endMargin end margin for collapsable block
+	 * @param <T> PsiElement subclass
+	 * @return list of folding descriptors
+	 */
 	private <T extends PsiElement> List<FoldingDescriptor> getDescriptorsFor(@NotNull PsiElement root, @NotNull Document document, Class<? extends T> c, int startMargin, int endMargin)
 	{
 		List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
@@ -170,7 +242,7 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		else if ( elementType == PerlElementTypes.ANON_HASH)
 			return "{hash}";
 		else if ( elementType == PerlElementTypes.PARENTHESISED_EXPR)
-			return "(list expression)";
+			return "(list expression...)";
 		else if ( elementType == PerlElementTypes.PERL_HEREDOC)
 			return "<< heredoc >>";
 		else if ( elementType == PerlElementTypes.PERL_POD)
@@ -180,7 +252,9 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		else if ( elementType == PerlElementTypes.PERL_COMMENT_BLOCK)
 			return "# Block comment";
 		else if ( elementType == PerlElementTypes.PERL_COMMENT)
-			return "# Comments";
+			return "# comments...";
+		else if ( elementType == PerlElementTypes.USE_STATEMENT || elementType == PerlElementTypes.REQUIRE_STATEMENT)
+			return "imports...";
 		else
 			return "unknown entity";
 	}
@@ -194,6 +268,8 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		else if ( elementType == PerlElementTypes.PERL_POD)
 			return true;
 		else if ( elementType == PerlElementTypes.PERL_COMMENT)
+			return true;
+		else if ( elementType == PerlElementTypes.USE_STATEMENT || elementType == PerlElementTypes.REQUIRE_STATEMENT)
 			return true;
 		else
 			return false;
