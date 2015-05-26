c876bb4946052fdb0b56c127c3381e2ae6ab4c80
hurricup <hurricup@gmail.com>
2015-05-26 07:27:20 +0300

    Implemented comments groups folding, fixes #80

--- src/com/perl5/lang/perl/idea/PerlFoldingBuilder.java
+++ src/com/perl5/lang/perl/idea/PerlFoldingBuilder.java
@@ -24,10 +24,7 @@ import com.intellij.openapi.editor.FoldingGroup;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.PsiBlockStatement;
-import com.intellij.psi.PsiComment;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLiteralExpression;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.source.tree.PsiCommentImpl;
 import com.intellij.psi.impl.source.tree.java.PsiBlockStatementImpl;
 import com.intellij.psi.templateLanguages.OuterLanguageElementImpl;
@@ -68,9 +65,54 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		descriptors.addAll(getDescriptorsFor(root, document, PerlParenthesisedExprImpl.class, 0, 0));
 		descriptors.addAll(getDescriptorsFor(root, document, PsiComment.class, 0, 0));
 
-		// sequential blocks
+		descriptors.addAll(getSequentialDescriptorsFor(root, document, PsiComment.class, PerlElementTypes.PERL_COMMENT, 0, 0));
 
 		return descriptors.toArray(new FoldingDescriptor[descriptors.size()]);
+	}
+
+	private <T extends PsiElement> List<FoldingDescriptor> getSequentialDescriptorsFor(@NotNull PsiElement root, @NotNull Document document, Class<? extends T> c, IElementType elementTypeFilter, int startMargin, int endMargin)
+	{
+		List<FoldingDescriptor> descriptors = new ArrayList<FoldingDescriptor>();
+
+		Collection<T> comments = PsiTreeUtil.findChildrenOfType(root,c);
+
+		int currentOffset = 0;
+
+		for( T comment: comments)
+		{
+			if( currentOffset < comment.getTextOffset() )
+			{
+				int blockStart = comment.getTextOffset();
+				int blockEnd = blockStart;
+				ASTNode blockNode = comment.getNode();
+				PsiElement currentComment = comment;
+				int commentsNumber = 0;
+
+				while( currentComment.getNextSibling() != null )
+				{
+					if( currentComment.getNode().getElementType() == PerlElementTypes.PERL_COMMENT )
+					{
+						blockEnd = currentComment.getTextOffset() + currentComment.getTextLength() - 1;
+						commentsNumber++;
+					}
+
+					IElementType nextType = currentComment.getNextSibling().getNode().getElementType();
+					if( nextType != PerlElementTypes.PERL_COMMENT && nextType != TokenType.WHITE_SPACE && nextType != TokenType.NEW_LINE_INDENT)
+					{
+						if( blockEnd != blockStart && commentsNumber > 1)
+						{
+							currentOffset = blockEnd;
+							descriptors.add(new FoldingDescriptor(blockNode, new TextRange(blockStart, blockEnd)));
+						}
+						break;
+					}
+
+					currentComment = currentComment.getNextSibling();
+				}
+			}
+		}
+
+		return descriptors;
 	}
 
 	private <T extends PsiElement> List<FoldingDescriptor> getDescriptorsFor(@NotNull PsiElement root, @NotNull Document document, Class<? extends T> c, int startMargin, int endMargin)
@@ -113,7 +155,9 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		else if ( elementType == PerlElementTypes.TEMPLATE_BLOCK_HTML)
 			return ">? HTML block <?";
 		else if ( elementType == PerlElementTypes.PERL_COMMENT_BLOCK)
-			return "# Block comment #";
+			return "# Block comment";
+		else if ( elementType == PerlElementTypes.PERL_COMMENT)
+			return "# Comments";
 		else
 			return "unknown entity";
 	}
@@ -125,6 +169,8 @@ public class PerlFoldingBuilder extends FoldingBuilderEx
 		if ( elementType == PerlElementTypes.PERL_COMMENT_BLOCK)
 			return true;
 		else if ( elementType == PerlElementTypes.PERL_POD)
+			return true;
+		else if ( elementType == PerlElementTypes.PERL_COMMENT)
 			return true;
 		else
 			return false;
