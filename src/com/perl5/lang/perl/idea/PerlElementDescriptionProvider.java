80fe8f623e8e5fab9df33ec57c4600aa44c49804
hurricup <hurricup@gmail.com>
2015-06-05 17:19:07 +0300

    More improvements to ElementDescriptionProvider

--- src/com/perl5/lang/perl/idea/PerlElementDescriptionProvider.java
+++ src/com/perl5/lang/perl/idea/PerlElementDescriptionProvider.java
@@ -17,17 +17,14 @@
 package com.perl5.lang.perl.idea;
 
 import com.intellij.codeInsight.highlighting.HighlightUsagesDescriptionLocation;
-import com.intellij.psi.ElementDescriptionLocation;
-import com.intellij.psi.ElementDescriptionProvider;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiNamedElement;
-import com.intellij.usageView.UsageViewLongNameLocation;
+import com.intellij.psi.*;
 import com.intellij.usageView.UsageViewNodeTextLocation;
 import com.intellij.usageView.UsageViewShortNameLocation;
 import com.intellij.usageView.UsageViewTypeLocation;
-import com.perl5.lang.perl.psi.*;
+import com.perl5.lang.perl.psi.PerlNamespaceDefinition;
+import com.perl5.lang.perl.psi.PerlSubDeclaration;
+import com.perl5.lang.perl.psi.PerlSubDefinition;
 import com.perl5.lang.perl.psi.impl.PerlFileElementImpl;
-import com.perl5.lang.perl.psi.properties.PerlNamedElement;
 import com.perl5.lang.perl.psi.properties.PerlPackageMember;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -36,35 +33,37 @@ public class PerlElementDescriptionProvider implements ElementDescriptionProvide
 {
 	@Nullable
 	@Override
-	public String getElementDescription( @NotNull PsiElement element, @NotNull ElementDescriptionLocation location)
+	public String getElementDescription(@NotNull PsiElement element, @NotNull ElementDescriptionLocation location)
 	{
 		PsiElement parent = element.getParent();
 
-		if( location == HighlightUsagesDescriptionLocation.INSTANCE 	// ???
-				|| location == UsageViewNodeTextLocation.INSTANCE 		// child element of find usages
+		if (location == HighlightUsagesDescriptionLocation.INSTANCE    // ???
+				|| location == UsageViewNodeTextLocation.INSTANCE        // child element of find usages
 				)
 			return getElementDescription(element, UsageViewShortNameLocation.INSTANCE);
-		else if( location == UsageViewTypeLocation.INSTANCE )
+		else if (location == UsageViewTypeLocation.INSTANCE)
 		{
-			if( element instanceof PerlSubDeclaration || parent instanceof PerlSubDeclaration)
-				return "Sub declaration";
-			else if( element instanceof PerlSubDefinition || parent instanceof PerlSubDefinition)
-				return "Sub definition";
-			else if( element instanceof PerlNamespaceDefinition || parent instanceof PerlNamespaceDefinition)
-				return "Namespace definition";
-			else if( element instanceof PerlFileElementImpl)
-				return "Perl file";
-			else return "Undefined type for class: " + element.getClass();
+			if (element instanceof PerlSubDeclaration || parent instanceof PerlSubDeclaration)
+				return "sub declaration";
+			else if (element instanceof PerlSubDefinition || parent instanceof PerlSubDefinition)
+				return "sub definition";
+			else if (element instanceof PerlNamespaceDefinition || parent instanceof PerlNamespaceDefinition)
+				return "namespace definition";
+			else if (element instanceof PerlFileElementImpl)
+				return "perl file";
+			else if (element instanceof PsiPackage)
+				return "directory";
+			return null;
 		}
 		// file renaming
-		else if( location == UsageViewShortNameLocation.INSTANCE)
+		else if (location == UsageViewShortNameLocation.INSTANCE)
 		{
-			if( element instanceof PerlPackageMember)
+			if (element instanceof PerlPackageMember)
 				return ((PerlPackageMember) element).getCanonicalName();
-			else if( element instanceof PsiNamedElement)
+			else if (element instanceof PsiNamedElement)
 				return ((PsiNamedElement) element).getName();
 		}
-
-		return "Unhandled description of "+ element.getClass() + " in " + location.getClass();
+//		return "Unhandled description of "+ element.getClass() + " in " + location.getClass();
+		return null;
 	}
 }
