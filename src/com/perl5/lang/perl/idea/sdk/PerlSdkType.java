9fbc106781671135c581206b8641aae1033002f0
hurricup <hurricup@gmail.com>
2015-06-06 23:29:11 +0300

    Implemented SDK path suggestion by Perl itself Implemented version string reading

--- src/com/perl5/lang/perl/idea/sdk/PerlSdkType.java
+++ src/com/perl5/lang/perl/idea/sdk/PerlSdkType.java
@@ -7,92 +7,177 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by ELI-HOME on 04-Jun-15.
  */
-public class PerlSdkType extends SdkType {
+public class PerlSdkType extends SdkType
+{
 
-    private String binaryFile = "";
+	private String perlExecutablePath = "";
 
-    public PerlSdkType() {
-        super("Perl5");
-    }
+	public PerlSdkType()
+	{
+		super("Perl5");
+	}
 
-    @NotNull
-    @Override
-    public String getName() {
-        return "Perl5";
-    }
+	@NotNull
+	@Override
+	public String getName()
+	{
+		return "Perl5";
+	}
 
-    @Override
-    public void saveAdditionalData(@NotNull SdkAdditionalData sdkAdditionalData, @NotNull Element element) {
+	@Override
+	public void saveAdditionalData(@NotNull SdkAdditionalData sdkAdditionalData, @NotNull Element element)
+	{
 
-    }
+	}
 
-    @Override
-    public String getPresentableName() {
-        return "Perl5 SDK";
-    }
+	@Override
+	public String getPresentableName()
+	{
+		return "Perl5 SDK";
+	}
 
-    @Nullable
-    @Override
-    public String suggestHomePath() {
-        if (OSUtil.isLinux() || OSUtil.isLinux()) {
-            return "/usr/";
-        }
-        return System.getenv("PERL_HOME");
-    }
+	@Nullable
+	@Override
+	public String suggestHomePath()
+	{
+		String perlPath = askPerlForPath();
+		if( perlPath != null )
+			return perlPath;
 
-    @Override
-    public String suggestSdkName(String currentSdkName, String sdkHome) {
-        return "Perl 5 SDK (" + sdkHome + ")";
-    }
+		if (OSUtil.isLinux() || OSUtil.isLinux())
+		{
+			return "/usr/";
+		}
+		return System.getenv("PERL_HOME");
+	}
 
-    @Nullable
-    @Override
-    public AdditionalDataConfigurable createAdditionalDataConfigurable(SdkModel sdkModel, SdkModificator sdkModificator) {
-        return null;
-    }
+	@Override
+	public String suggestSdkName(String currentSdkName, String sdkHome)
+	{
+		return "Perl " + getPerlVersionString();
+	}
 
-    @Override
-    public boolean isValidSdkHome(String sdkHome) {
-        if (!(sdkHome.endsWith("/") && sdkHome.endsWith("\\"))) {
-            sdkHome += File.separator;
-        }
-        if (OSUtil.isKnown()) {
-            try {
-                File f = new File(executablePath(sdkHome));
-                this.binaryFile = f.getCanonicalPath();
-                return f.exists();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return false;
-    }
+	@Nullable
+	@Override
+	public AdditionalDataConfigurable createAdditionalDataConfigurable(SdkModel sdkModel, SdkModificator sdkModificator)
+	{
+		return null;
+	}
 
-    private String executablePath(String sdkHome) {
-        if (OSUtil.isLinux() || OSUtil.isMacOS()) {
-            return sdkHome + "bin/perl";
-        } else if (OSUtil.isWindows()) {
-            return sdkHome + "bin/perl.exe";
-        }
-        throw new RuntimeException("Unknown OS");
-    }
+	@Override
+	public boolean isValidSdkHome(String sdkHome)
+	{
+		if (!(sdkHome.endsWith("/") && sdkHome.endsWith("\\")))
+		{
+			sdkHome += File.separator;
+		}
+		if (OSUtil.isKnown())
+		{
+			try
+			{
+				File f = new File(executablePath(sdkHome));
+				this.perlExecutablePath = f.getCanonicalPath();
+				return f.exists();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public Icon getIcon() {
-        return PerlIcons.PERL_LANGUAGE;
-    }
+	private String executablePath(String sdkHome)
+	{
+		if (OSUtil.isLinux() || OSUtil.isMacOS())
+		{
+			return sdkHome + "bin/perl";
+		} else if (OSUtil.isWindows())
+		{
+			return sdkHome + "bin/perl.exe";
+		}
+		throw new RuntimeException("Unknown OS");
+	}
 
-    @Nullable
-    @Override
-    public String getVersionString(@NotNull Sdk sdk) {
-        return "5";//todo: get from perl exact version
-    }
+	@Override
+	public Icon getIcon()
+	{
+		return PerlIcons.PERL_LANGUAGE;
+	}
+
+	@Nullable
+	@Override
+	public String getVersionString(@NotNull Sdk sdk)
+	{
+		return getPerlVersionString();
+	}
+
+
+	Pattern perlVersionStringPattern = Pattern.compile("\\(([^)]+?)\\) built for (.+)");
+
+	public String getPerlVersionString()
+	{
+		List<String> versionLines = readFromProgram(perlExecutablePath + " -v");
+
+		if (versionLines.size() > 0)
+		{
+			Matcher m = perlVersionStringPattern.matcher(versionLines.get(0));
+
+			if( m.find() )
+				return m.group(1) + " (" + m.group(2) + ")";
+
+			return "Unknown version";
+		}
+
+		throw new RuntimeException("Error getting perl version text");
+	}
+
+
+	public String askPerlForPath()
+	{
+		List<String> perlPathLines = readFromProgram("perl -le \"print $^X\"");
+
+		if( perlPathLines.size() == 1)
+		{
+			int binIndex = perlPathLines.get(0).lastIndexOf("bin");
+			if( binIndex > 0 )
+				return perlPathLines.get(0).substring(0, binIndex);
+
+		}
+		return null;
+	}
+
+	public List<String> readFromProgram(String command)
+	{
+		try
+		{
+			List<String> result = new ArrayList<>();
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			String line;
+			while ((line = in.readLine()) != null)
+				if( !line.isEmpty() )
+					result.add(line);
+
+			in.close();
+			return result;
+		} catch (Exception e)
+		{
+			return null;
+		}
+	}
 
 
 }
