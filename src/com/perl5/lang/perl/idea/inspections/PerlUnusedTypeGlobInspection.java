366ed124acbba197fb284dfe6060a6774134326d
hurricup <hurricup@gmail.com>
2015-08-14 22:08:50 +0300

    Implemented unused typeglob inspection

---
+++ src/com/perl5/lang/perl/idea/inspections/PerlUnusedTypeGlobInspection.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.perl.idea.inspections;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.perl5.lang.perl.psi.PerlVisitor;
+import com.perl5.lang.perl.psi.PsiPerlAssignExpr;
+import com.perl5.lang.perl.psi.PsiPerlGlobVariable;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by hurricup on 14.08.2015.
+ */
+public class PerlUnusedTypeGlobInspection extends PerlInspection
+{
+	@NotNull
+	@Override
+	public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly)
+	{
+		return new PerlVisitor()
+		{
+			@Override
+			public void visitGlobVariable(@NotNull PsiPerlGlobVariable o)
+			{
+				if (o.getParent() instanceof PsiPerlAssignExpr
+						&& o.getNextSibling() != null
+						&& ReferencesSearch.search(o, GlobalSearchScope.allScope(o.getProject())).findAll().size() == 0
+						)
+					holder.registerProblem(o, "Unused typeglob alias", ProblemHighlightType.LIKE_UNUSED_SYMBOL);
+
+			}
+		};
+	}
+}
