14d74d21e448509e3f8c0d7f48819cabf3fba52d
hurricup <hurricup@gmail.com>
2015-09-02 00:30:37 +0300

    Moved icons into /src/icons

--- src/com/perl5/PerlIcons.java
+++ src/com/perl5/PerlIcons.java
@@ -26,30 +26,30 @@ import javax.swing.*;
 
 public class PerlIcons
 {
-	public static final Icon PERL_LANGUAGE = IconLoader.getIcon("/com/perl5/icons/perl5language.png");
-	public static final Icon PERL_OPTION = IconLoader.getIcon("/com/perl5/icons/option.png");
-	public static final Icon PERL_OPTIONS = IconLoader.getIcon("/com/perl5/icons/options.png");
-	public static final Icon PM_FILE = IconLoader.getIcon("/com/perl5/icons/perl5.png");
-	public static final Icon MOJO_FILE = IconLoader.getIcon("/com/perl5/icons/mojolicious.png");
-	public static final Icon POD_FILE = IconLoader.getIcon("/com/perl5/icons/podicon.png");
-	public static final Icon SCRIPT_FILE = IconLoader.getIcon("/com/perl5/icons/perl5classic.png");
-	public static final Icon XS_FILE = IconLoader.getIcon("/com/perl5/icons/xsicon.png");
+	public static final Icon PERL_LANGUAGE = IconLoader.getIcon("/icons/perl5language.png");
+	public static final Icon PERL_OPTION = IconLoader.getIcon("/icons/option.png");
+	public static final Icon PERL_OPTIONS = IconLoader.getIcon("/icons/options.png");
+	public static final Icon PM_FILE = IconLoader.getIcon("/icons/perl5.png");
+	public static final Icon MOJO_FILE = IconLoader.getIcon("/icons/mojolicious.png");
+	public static final Icon POD_FILE = IconLoader.getIcon("/icons/podicon.png");
+	public static final Icon SCRIPT_FILE = IconLoader.getIcon("/icons/perl5classic.png");
+	public static final Icon XS_FILE = IconLoader.getIcon("/icons/xsicon.png");
 
-	public static final Icon LIB_ROOT = IconLoader.getIcon("/com/perl5/icons/library_root.png");
+	public static final Icon LIB_ROOT = IconLoader.getIcon("/icons/library_root.png");
 
-	public static final Icon EMBEDDED_PERL_FILE = IconLoader.getIcon("/com/perl5/icons/embeddedperl.png");
-	public static final Icon PACKAGE_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/package_gutter_icon.png");
-	public static final Icon PRAGMA_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/pragmaicon.png");
+	public static final Icon EMBEDDED_PERL_FILE = IconLoader.getIcon("/icons/embeddedperl.png");
+	public static final Icon PACKAGE_GUTTER_ICON = IconLoader.getIcon("/icons/package_gutter_icon.png");
+	public static final Icon PRAGMA_GUTTER_ICON = IconLoader.getIcon("/icons/pragmaicon.png");
 
-	public static final Icon SUB_DECLARATION_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/sub_declaration_gutter_icon.png");
-	public static final Icon METHOD_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/method_gutter_icon.png");
-	public static final Icon SUB_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/subroutine_gutter_icon.png");
-	public static final Icon CONSTANT_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/constant_gutter_icon.png");
+	public static final Icon SUB_DECLARATION_GUTTER_ICON = IconLoader.getIcon("/icons/sub_declaration_gutter_icon.png");
+	public static final Icon METHOD_GUTTER_ICON = IconLoader.getIcon("/icons/method_gutter_icon.png");
+	public static final Icon SUB_GUTTER_ICON = IconLoader.getIcon("/icons/subroutine_gutter_icon.png");
+	public static final Icon CONSTANT_GUTTER_ICON = IconLoader.getIcon("/icons/constant_gutter_icon.png");
 
-	public static final Icon ANNOTATION_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/annotation_gutter_icon.png");
+	public static final Icon ANNOTATION_GUTTER_ICON = IconLoader.getIcon("/icons/annotation_gutter_icon.png");
 
-	public static final Icon SCALAR_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/scalar_gutter_icon.png");
-	public static final Icon ARRAY_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/array_gutter_icon.png");
-	public static final Icon HASH_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/hash_gutter_icon.png");
-	public static final Icon GLOB_GUTTER_ICON = IconLoader.getIcon("/com/perl5/icons/glob_gutter_icon.png");
+	public static final Icon SCALAR_GUTTER_ICON = IconLoader.getIcon("/icons/scalar_gutter_icon.png");
+	public static final Icon ARRAY_GUTTER_ICON = IconLoader.getIcon("/icons/array_gutter_icon.png");
+	public static final Icon HASH_GUTTER_ICON = IconLoader.getIcon("/icons/hash_gutter_icon.png");
+	public static final Icon GLOB_GUTTER_ICON = IconLoader.getIcon("/icons/glob_gutter_icon.png");
 }
