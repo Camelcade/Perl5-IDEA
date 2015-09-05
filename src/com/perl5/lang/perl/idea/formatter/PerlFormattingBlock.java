83e0bda4d9224b6fbbb2c2262ee42589ba4bbc4c
hurricup <hurricup@gmail.com>
2015-09-05 21:32:44 +0300

    #411 Implemented forced space between statements and labels

--- src/com/perl5/lang/perl/idea/formatter/PerlFormattingBlock.java
+++ src/com/perl5/lang/perl/idea/formatter/PerlFormattingBlock.java
@@ -21,6 +21,7 @@ import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.TokenType;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
+import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.formatter.common.AbstractBlock;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
@@ -37,10 +38,42 @@ import java.util.List;
  */
 public class PerlFormattingBlock extends AbstractBlock implements PerlElementTypes
 {
-	protected final static TokenSet LEAF_ELEMENTS = TokenSet.create(
+	/**
+	 * Composit elements that should be treated as leaf elements, no children
+	 */
+	public final static TokenSet LEAF_ELEMENTS = TokenSet.create(
 			HEREDOC,
 			HEREDOC_QX,
 			HEREDOC_QQ
+	);
+
+	/**
+	 * Elements that must have LF between them
+	 */
+	public final static TokenSet LF_ELEMENTS = TokenSet.create(
+			LABEL_DECLARATION,
+			STATEMENT,
+			FOR_COMPOUND,
+			FOREACH_COMPOUND,
+			WHILE_COMPOUND,
+			WHEN_COMPOUND,
+			UNTIL_COMPOUND,
+			IF_COMPOUND,
+			USE_STATEMENT,
+			USE_STATEMENT_CONSTANT,
+			USE_VARS_STATEMENT
+	);
+
+	public final static TokenSet BLOCK_OPENERS = TokenSet.create(
+			LEFT_BRACE,
+			LEFT_BRACKET,
+			LEFT_PAREN
+	);
+
+	public final static TokenSet BLOCK_CLOSERS = TokenSet.create(
+			RIGHT_BRACE,
+			RIGHT_BRACKET,
+			RIGHT_PAREN
 	);
 
 	private final Indent myIndent;
@@ -48,8 +81,11 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 	private final PerlCodeStyleSettings myPerl5Settings;
 	private final SpacingBuilder mySpacingBuilder;
 	private final Alignment myAlignment;
+	private final boolean myBlockLike;
+	private final boolean myIsFirst;
+	private final boolean myIsLast;
+	private final IElementType myElementType;
 	private List<Block> mySubBlocks;
-
 
 	public PerlFormattingBlock(
 			@NotNull ASTNode node,
@@ -67,6 +103,10 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 		mySpacingBuilder = spacingBuilder;
 		myAlignment = alignment;
 		myIndent = new PerlIndentProcessor(perlCodeStyleSettings).getChildIndent(node, binaryExpressionIndex);
+		myBlockLike = PerlIndentProcessor.BLOCK_LIKE_TOKENS.contains(node.getElementType());
+		myIsFirst = FormatterUtil.getPreviousNonWhitespaceSibling(node) == null;
+		myIsLast = FormatterUtil.getNextNonWhitespaceSibling(node) == null;
+		myElementType = node.getElementType();
 	}
 
 	private static boolean shouldCreateBlockFor(ASTNode node)
@@ -81,6 +121,7 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 		if (mySubBlocks == null)
 			mySubBlocks = buildSubBlocks();
 
+		// fixme what is re-creation for?
 		return new ArrayList<Block>(mySubBlocks);
 	}
 
@@ -115,6 +156,24 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 	@Override
 	public Spacing getSpacing(Block child1, @NotNull Block child2)
 	{
+		if (child1 instanceof PerlFormattingBlock && child2 instanceof PerlFormattingBlock)
+		{
+			ASTNode child1Node = ((PerlFormattingBlock) child1).getNode();
+			IElementType child1Type = child1Node.getElementType();
+			ASTNode child2Node = ((PerlFormattingBlock) child2).getNode();
+			IElementType child2Type = child2Node.getElementType();
+
+			// LF after opening brace and before closing
+			if (isCodeBlock() &&
+					(BLOCK_OPENERS.contains(child1Type) && ((PerlFormattingBlock) child1).isFirst()
+							|| BLOCK_CLOSERS.contains(child2Type) && ((PerlFormattingBlock) child2).isLast()
+					))
+				return Spacing.createSpacing(0, 0, 1, true, 1);
+
+			// LF between statements and so on
+			if (LF_ELEMENTS.contains(child1Type) && LF_ELEMENTS.contains(child2Type))
+				return Spacing.createSpacing(0, 0, 1, true, 1);
+		}
 		return mySpacingBuilder.getSpacing(this, child1, child2);
 	}
 
@@ -142,10 +201,7 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 	@Override
 	protected Indent getChildIndent()
 	{
-//		System.err.println("Invoced getchild indent");
-		IElementType type = getNode().getElementType();
-
-		if (PerlIndentProcessor.BLOCK_LIKE_TOKENS.contains(type))
+		if (isBlockLike())
 			return Indent.getNormalIndent();
 
 		return Indent.getNoneIndent();
@@ -158,4 +214,30 @@ public class PerlFormattingBlock extends AbstractBlock implements PerlElementTyp
 		int start = myNode.getStartOffset();
 		return new TextRange(start, start + myNode.getText().length());
 	}
+
+	public boolean isLast()
+	{
+		return myIsLast;
+	}
+
+	public boolean isFirst()
+	{
+		return myIsFirst;
+	}
+
+	public boolean isBlockLike()
+	{
+		return myBlockLike;
+	}
+
+	public IElementType getElementType()
+	{
+		return myElementType;
+	}
+
+	public boolean isCodeBlock()
+	{
+		return getElementType() == BLOCK;
+	}
+
 }
