03191e5a82bcb8d91e06f218569b52eab18a29e7
hurricup <hurricup@gmail.com>
2015-09-06 15:05:03 +0300

    #411 Implemented spacing for assignment operators

--- src/com/perl5/lang/perl/idea/formatter/PerlFormattingModelBuilder.java
+++ src/com/perl5/lang/perl/idea/formatter/PerlFormattingModelBuilder.java
@@ -26,7 +26,7 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
-import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.perl5.lang.perl.PerlLanguage;
 import com.perl5.lang.perl.idea.formatter.settings.PerlCodeStyleSettings;
 import com.perl5.lang.perl.lexer.PerlElementTypes;
@@ -38,26 +38,44 @@ import org.jetbrains.annotations.Nullable;
  */
 public class PerlFormattingModelBuilder implements FormattingModelBuilder, PerlElementTypes
 {
+	public static final TokenSet OPERATORS_ASSIGNMENT = TokenSet.create(
+			OPERATOR_ASSIGN,
+			OPERATOR_POW_ASSIGN,
+			OPERATOR_PLUS_ASSIGN,
+			OPERATOR_MINUS_ASSIGN,
+			OPERATOR_MUL_ASSIGN,
+			OPERATOR_DIV_ASSIGN,
+			OPERATOR_MOD_ASSIGN,
+			OPERATOR_CONCAT_ASSIGN,
+			OPERATOR_X_ASSIGN,
+			OPERATOR_BITWISE_AND_ASSIGN,
+			OPERATOR_BITWISE_OR_ASSIGN,
+			OPERATOR_BITWISE_XOR_ASSIGN,
+			OPERATOR_SHIFT_LEFT_ASSIGN,
+			OPERATOR_SHIFT_RIGHT_ASSIGN,
+			OPERATOR_AND_ASSIGN,
+			OPERATOR_OR_ASSIGN,
+			OPERATOR_OR_DEFINED_ASSIGN
+	);
+
+	public static final TokenSet RESERVED_VARIABLE_DECLARATION = TokenSet.create(
+			RESERVED_MY,
+			RESERVED_OUR,
+			RESERVED_LOCAL,
+			RESERVED_STATE
+	);
+
 	private static SpacingBuilder createSpacingBuilder(@NotNull CommonCodeStyleSettings settings, @NotNull PerlCodeStyleSettings perlSettings)
 	{
-		SpacingBuilder spacingBuilder = new SpacingBuilder(settings.getRootSettings(), PerlLanguage.INSTANCE)
+		return new SpacingBuilder(settings.getRootSettings(), PerlLanguage.INSTANCE)
 				.before(HEREDOC_END).none()
-
-				.after(RESERVED_MY).spaceIf(perlSettings.SPACE_AFTER_VARIABLE_DECLARATION_KEYWORD)
-				.after(RESERVED_OUR).spaceIf(perlSettings.SPACE_AFTER_VARIABLE_DECLARATION_KEYWORD)
-				.after(RESERVED_LOCAL).spaceIf(perlSettings.SPACE_AFTER_VARIABLE_DECLARATION_KEYWORD)
-				.after(RESERVED_STATE).spaceIf(perlSettings.SPACE_AFTER_VARIABLE_DECLARATION_KEYWORD)
-
-				.around(OPERATOR_ASSIGN).spaceIf(settings.SPACE_AROUND_ASSIGNMENT_OPERATORS)
-
+				.after(RESERVED_VARIABLE_DECLARATION).spaceIf(perlSettings.SPACE_AFTER_VARIABLE_DECLARATION_KEYWORD)
+				.around(OPERATORS_ASSIGNMENT).spaceIf(settings.SPACE_AROUND_ASSIGNMENT_OPERATORS)
 				.before(OPERATOR_COMMA).spaceIf(settings.SPACE_BEFORE_COMMA)
 				.after(OPERATOR_COMMA).spaceIf(settings.SPACE_AFTER_COMMA)
-				.around(OPERATOR_COMMA_ARROW).spacing(1, Integer.MAX_VALUE, 0, true, 1);
-
-		for (IElementType type : PerlFormattingBlock.LF_ELEMENTS.getTypes())
-			spacingBuilder = spacingBuilder.after(type).lineBreakInCode();
-
-		return spacingBuilder;
+				.around(OPERATOR_COMMA_ARROW).spacing(1, Integer.MAX_VALUE, 0, true, 1)
+				.after(PerlFormattingBlock.LF_ELEMENTS).lineBreakInCode()
+				;
 	}
 
 	@NotNull
