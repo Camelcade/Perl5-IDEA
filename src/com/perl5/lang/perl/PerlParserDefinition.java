f6e795a4d530fd15f5dd6970e655ef88558c1a42
hurricup <hurricup@gmail.com>
2015-11-22 20:58:12 +0300

    Moved here-doc tokens from whitespaces to comments Tried to enable embedded files formatting, but seems used settings are wrong

--- src/com/perl5/lang/perl/PerlParserDefinition.java
+++ src/com/perl5/lang/perl/PerlParserDefinition.java
@@ -51,15 +51,15 @@ public class PerlParserDefinition implements ParserDefinition, PerlElementTypes
 
 	public static final TokenSet WHITE_SPACES = TokenSet.create(
 			TokenType.WHITE_SPACE,
-			TokenType.NEW_LINE_INDENT,
+			TokenType.NEW_LINE_INDENT
+	);
+	public static final TokenSet COMMENTS = TokenSet.create(
+			COMMENT_LINE, COMMENT_BLOCK,
+			HEREDOC, HEREDOC_QQ, HEREDOC_QX, HEREDOC_END,
 
 			// these should be in other place
 			EMBED_MARKER, EMBED_MARKER_OPEN, EMBED_MARKER_CLOSE,
 			TEMPLATE_BLOCK_HTML
-	);
-	public static final TokenSet COMMENTS = TokenSet.create(
-			COMMENT_LINE, COMMENT_BLOCK,
-			HEREDOC, HEREDOC_QQ, HEREDOC_QX, HEREDOC_END
 	);
 
 	public static final TokenSet WHITE_SPACE_AND_COMMENTS = TokenSet.orSet(WHITE_SPACES, COMMENTS);
