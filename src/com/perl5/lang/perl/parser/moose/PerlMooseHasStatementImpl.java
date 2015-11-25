dda7897c623e045b70d6574a4efff17457c6d211
hurricup <hurricup@gmail.com>
2015-11-25 21:20:12 +0300

    Implemented Moose statements classes

---
+++ src/com/perl5/lang/perl/parser/moose/PerlMooseHasStatementImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.perl.parser.moose;
+
+import com.intellij.lang.ASTNode;
+import com.perl5.lang.perl.psi.impl.PsiPerlStatementImpl;
+
+/**
+ * Created by hurricup on 25.11.2015.
+ */
+public class PerlMooseHasStatementImpl extends PsiPerlStatementImpl implements PerlMooseHasStatement
+{
+	public PerlMooseHasStatementImpl(ASTNode node)
+	{
+		super(node);
+	}
+}
