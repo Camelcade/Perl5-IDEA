55bad8a63b0955fee4b264f56b22832314b8c196
hurricup <hurricup@gmail.com>
2015-12-06 10:26:20 +0300

    PerlElementDescriptionProvider now works only with perl entities, #CAMELCADE-36 fixed

--- src/com/perl5/lang/perl/idea/PerlElementDescriptionProvider.java
+++ src/com/perl5/lang/perl/idea/PerlElementDescriptionProvider.java
@@ -21,6 +21,7 @@ import com.intellij.psi.*;
 import com.intellij.usageView.UsageViewNodeTextLocation;
 import com.intellij.usageView.UsageViewShortNameLocation;
 import com.intellij.usageView.UsageViewTypeLocation;
+import com.perl5.lang.perl.PerlLanguage;
 import com.perl5.lang.perl.psi.*;
 import com.perl5.lang.perl.psi.impl.PerlFileImpl;
 import com.perl5.lang.perl.psi.properties.PerlNamedElement;
@@ -35,59 +36,61 @@ public class PerlElementDescriptionProvider implements ElementDescriptionProvide
 	@Override
 	public String getElementDescription(@NotNull PsiElement element, @NotNull ElementDescriptionLocation location)
 	{
-		PsiElement parent = element.getParent();
+//		PsiElement parent = element.getParent();
 
 //		System.out.println(element + " for " + location);
-
-		if (location == HighlightUsagesDescriptionLocation.INSTANCE    // ???
-				|| location == UsageViewNodeTextLocation.INSTANCE        // child element of find usages
-				)
-			return getElementDescription(element, UsageViewShortNameLocation.INSTANCE);
-		else if (location == UsageViewTypeLocation.INSTANCE)
+		if (element.getLanguage() == PerlLanguage.INSTANCE)
 		{
-			if (element instanceof PerlSubDeclaration)
-				return "Sub declaration";
-			else if (element instanceof PerlHeredocOpener)
-				return "Heredoc marker";
-			else if (element instanceof PerlSubDefinition)
-				return "Sub definition";
-			else if (element instanceof PerlMethodDefinition)
-				return "Method definition";
-			else if (element instanceof PerlFuncDefinition)
-				return "Function definition";
-			else if (element instanceof PerlConstant)
-				return "Constant";
-			else if (element instanceof PerlNamespaceDefinition)
-				return "Namespace definition";
-			else if (element instanceof PerlFileImpl)
-				return "perl file";
-			else if (element instanceof PsiDirectoryContainer)
-				return "Directory";
-			else if (element instanceof PerlVariableNameElement)
-				return getElementDescription(element.getParent(), location);
-			else if (element instanceof PerlGlobVariable)
-				return "Typeglob";
-			else if (element instanceof PerlVariable)
+			if (location == HighlightUsagesDescriptionLocation.INSTANCE    // ???
+					|| location == UsageViewNodeTextLocation.INSTANCE        // child element of find usages
+					)
+				return getElementDescription(element, UsageViewShortNameLocation.INSTANCE);
+			else if (location == UsageViewTypeLocation.INSTANCE)
 			{
-				PerlVariableType actualType = ((PerlVariable) element).getActualType();
-				if (actualType == PerlVariableType.ARRAY)
-					return "Array variable";
-				else if (actualType == PerlVariableType.HASH)
-					return "Hash variable";
-				else if (actualType == PerlVariableType.SCALAR)
-					return "Scalar variable";
+				if (element instanceof PerlSubDeclaration)
+					return "Sub declaration";
+				else if (element instanceof PerlHeredocOpener)
+					return "Heredoc marker";
+				else if (element instanceof PerlSubDefinition)
+					return "Sub definition";
+				else if (element instanceof PerlMethodDefinition)
+					return "Method definition";
+				else if (element instanceof PerlFuncDefinition)
+					return "Function definition";
+				else if (element instanceof PerlConstant)
+					return "Constant";
+				else if (element instanceof PerlNamespaceDefinition)
+					return "Namespace definition";
+				else if (element instanceof PerlFileImpl)
+					return "perl file";
+				else if (element instanceof PsiDirectoryContainer)
+					return "Directory";
+				else if (element instanceof PerlVariableNameElement)
+					return getElementDescription(element.getParent(), location);
+				else if (element instanceof PerlGlobVariable)
+					return "Typeglob";
+				else if (element instanceof PerlVariable)
+				{
+					PerlVariableType actualType = ((PerlVariable) element).getActualType();
+					if (actualType == PerlVariableType.ARRAY)
+						return "Array variable";
+					else if (actualType == PerlVariableType.HASH)
+						return "Hash variable";
+					else if (actualType == PerlVariableType.SCALAR)
+						return "Scalar variable";
+				}
+				return null;
 			}
-			return null;
-		}
-		// file renaming
-		else if (location == UsageViewShortNameLocation.INSTANCE)
-		{
-			if (element instanceof PerlNamedElement)
-				return ((PerlNamedElement) element).getPresentableName();
-			else if (element instanceof PerlPackageMember)
-				return ((PerlPackageMember) element).getCanonicalName();
-			else if (element instanceof PsiNamedElement)
-				return ((PsiNamedElement) element).getName();
+			// file renaming
+			else if (location == UsageViewShortNameLocation.INSTANCE)
+			{
+				if (element instanceof PerlNamedElement)
+					return ((PerlNamedElement) element).getPresentableName();
+				else if (element instanceof PerlPackageMember)
+					return ((PerlPackageMember) element).getCanonicalName();
+				else if (element instanceof PsiNamedElement)
+					return ((PsiNamedElement) element).getName();
+			}
 		}
 //		return "Unhandled description of "+ element.getClass() + " in " + location.getClass();
 		return null;
