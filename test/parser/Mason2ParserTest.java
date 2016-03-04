2848d05dfd1d57a283c7e51fbcbe98b95b5e12a7
hurricup <hurricup@gmail.com>
2016-03-04 23:42:43 +0300

    Created tests for mason components, but temporary disabled them.

---
+++ test/parser/Mason2ParserTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package parser;
+
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.testFramework.TestDataFile;
+import com.perl5.lang.mason2.MasonParserDefinition;
+import org.jetbrains.annotations.NonNls;
+import org.junit.Ignore;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by hurricup on 04.03.2016.
+ */
+@Ignore
+public class Mason2ParserTest extends PerlParserTestBase
+{
+	public Mason2ParserTest()
+	{
+		super("", "mp", new MasonParserDefinition());
+	}
+
+	@Override
+	protected String getTestDataPath()
+	{
+		return "testData/parser/mason2/perl";
+	}
+
+	protected String loadFile(@NonNls @TestDataFile String name) throws IOException
+	{
+		return FileUtil.loadFile(new File(myFullDataPath, name.replace(".mp", ".code")), CharsetToolkit.UTF8, true).trim();
+	}
+
+	public void testComponent()
+	{
+		doTest("test_component", true);
+	}
+
+
+}
---
+++ test/parser/Mason2TemplatingParserTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package parser;
+
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.LightVirtualFile;
+import com.intellij.testFramework.TestDataFile;
+import com.perl5.lang.mason2.MasonTemplatingParserDefinition;
+import com.perl5.lang.mason2.filetypes.MasonTopLevelComponentFileType;
+import org.jetbrains.annotations.NonNls;
+import org.junit.Ignore;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by hurricup on 04.03.2016.
+ */
+@Ignore
+public class Mason2TemplatingParserTest extends PerlParserTestBase
+{
+	public Mason2TemplatingParserTest()
+	{
+		super("", "mc", new MasonTemplatingParserDefinition());
+	}
+
+	@Override
+	protected String getTestDataPath()
+	{
+		return "testData/parser/mason2/template";
+	}
+
+
+	@Override
+	protected PsiFile createFile(LightVirtualFile virtualFile)
+	{
+		virtualFile.setFileType(MasonTopLevelComponentFileType.INSTANCE);
+		return super.createFile(virtualFile);
+	}
+
+	protected String loadFile(@NonNls @TestDataFile String name) throws IOException
+	{
+		return FileUtil.loadFile(new File(myFullDataPath, name.replace(".mc", ".code")), CharsetToolkit.UTF8, true).trim();
+	}
+
+	public void testComponent()
+	{
+		doTest("test_component", true);
+	}
+}
