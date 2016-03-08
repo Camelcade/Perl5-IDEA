08b472117ee0b901f0a781dea45af5892d018ee0
hurricup <hurricup@gmail.com>
2016-03-08 19:21:19 +0300

    Implemented WordsScanner and find usages, fixes #888 [ci skip]

---
+++ src/com/perl5/lang/htmlmason/idea/findusages/HTMLMasonWordsScanner.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.htmlmason.idea.findusages;
+
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner;
+import com.perl5.lang.htmlmason.lexer.HTMLMasonLexerAdapter;
+import com.perl5.lang.perl.PerlParserDefinition;
+
+/**
+ * Created by hurricup on 08.03.2016.
+ */
+public class HTMLMasonWordsScanner extends DefaultWordsScanner
+{
+	public HTMLMasonWordsScanner()
+	{
+		super(new HTMLMasonLexerAdapter(null),
+				PerlParserDefinition.IDENTIFIERS,
+				PerlParserDefinition.COMMENTS,
+				PerlParserDefinition.LITERALS);
+		setMayHaveFileRefsInLiterals(true);
+	}
+}
