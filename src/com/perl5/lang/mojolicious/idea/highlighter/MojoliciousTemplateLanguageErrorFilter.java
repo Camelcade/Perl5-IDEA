cfa06f025129ee19039cd7a709c4b6fb900c0e40
hurricup <hurricup@gmail.com>
2016-03-16 22:01:59 +0300

    Implemented TemplateLanguageErrorFilter for Mojolicious, fixes #920

---
+++ src/com/perl5/lang/mojolicious/idea/highlighter/MojoliciousTemplateLanguageErrorFilter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.mojolicious.idea.highlighter;
+
+import com.intellij.codeInsight.highlighting.TemplateLanguageErrorFilter;
+import com.intellij.psi.tree.TokenSet;
+import com.perl5.lang.mojolicious.MojoliciousElementTypes;
+import com.perl5.lang.mojolicious.MojoliciousFileViewProvider;
+
+/**
+ * Created by hurricup on 16.03.2016.
+ */
+public class MojoliciousTemplateLanguageErrorFilter extends TemplateLanguageErrorFilter implements MojoliciousElementTypes
+{
+	private static final TokenSet START_TOKENS = TokenSet.create(
+			MOJO_BLOCK_OPENER,
+			MOJO_BLOCK_EXPR_OPENER,
+			MOJO_BLOCK_EXPR_ESCAPED_OPENER,
+			MOJO_LINE_OPENER,
+			MOJO_LINE_EXPR_OPENER,
+			MOJO_LINE_EXPR_ESCAPED_OPENER
+	);
+
+	public MojoliciousTemplateLanguageErrorFilter()
+	{
+		super(START_TOKENS, MojoliciousFileViewProvider.class, "HTML");
+	}
+}
