bda6ba4b7bf9c4350afa1533b852b47281fb24c9
hurricup <hurricup@gmail.com>
2016-03-16 22:11:56 +0300

    Implemented TemplateLanguageErrorFilter for Mason2, fixes #919

---
+++ src/com/perl5/lang/mason2/idea/highlighter/Mason2TemplateLanguageErrorFilter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.mason2.idea.highlighter;
+
+import com.intellij.codeInsight.highlighting.TemplateLanguageErrorFilter;
+import com.intellij.psi.tree.TokenSet;
+import com.perl5.lang.mason2.Mason2TemplatingFileViewProvider;
+import com.perl5.lang.mason2.elementType.MasonElementTypes;
+
+/**
+ * Created by hurricup on 16.03.2016.
+ */
+public class Mason2TemplateLanguageErrorFilter extends TemplateLanguageErrorFilter implements MasonElementTypes
+{
+	private static final TokenSet START_TOKENS = TokenSet.create(
+			MASON_BLOCK_OPENER,
+			MASON_PERL_OPENER,
+			MASON_CALL_OPENER,
+			MASON_LINE_OPENER
+	);
+
+	public Mason2TemplateLanguageErrorFilter()
+	{
+		super(START_TOKENS, Mason2TemplatingFileViewProvider.class, "HTML");
+	}
+}
