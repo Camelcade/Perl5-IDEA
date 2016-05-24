3a1b3286bc730b29fb89f896dad10481f2921d70
hurricup <hurricup@gmail.com>
2016-05-24 21:26:09 +0300

    Implemented tests for string collecting [ci skip]

---
+++ test/psi/PerlStringCollectionTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package psi;
+
+import base.PerlLightCodeInsightFixtureTestCase;
+import com.perl5.lang.perl.psi.PerlUseStatement;
+
+import java.util.Arrays;
+
+/**
+ * Created by hurricup on 24.05.2016.
+ */
+public class PerlStringCollectionTest extends PerlLightCodeInsightFixtureTestCase
+{
+	@Override
+	protected String getTestDataPath()
+	{
+		return "testData/psi/stringCollection";
+	}
+
+	public void testQ()
+	{
+		doTest("q", "testq I:/mylib/");
+	}
+
+	public void testQQ()
+	{
+		doTest("qq", "testqq I:/mylib/");
+	}
+
+	public void testQX()
+	{
+		doTest("qx", "testqx I:/mylib/");
+	}
+
+	public void testQW()
+	{
+		doTest("qw", "testqw", "I:/mylib/");
+	}
+
+	public void testBare()
+	{
+		doTest("bare", "testbare", "I:/mylib/");
+	}
+
+	public void testMixed()
+	{
+		doTest("mixed", "test", "I:/mylib/", "something", "else", "one", "three", "and", "even", "more");
+	}
+
+	protected void doTest(String fileName, String... result)
+	{
+		initWithFileAsScript(fileName);
+		PerlUseStatement useStatement = getElementAtCaret(PerlUseStatement.class);
+		assertNotNull(useStatement);
+		assertEquals(Arrays.asList(result), useStatement.getImportParameters());
+	}
+
+}
