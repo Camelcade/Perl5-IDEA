7e28a9b538dd5e385fa40c8f88a4fa7f8bb2d687
hurricup <hurricup@gmail.com>
2016-06-05 22:40:58 +0300

    Implemented TT2 brace matcher

---
+++ src/com/perl5/lang/tt2/idea/editor/TemplateToolkitBraceMatcher.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.tt2.idea.editor;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import com.perl5.lang.tt2.elementTypes.TemplateToolkitElementTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by hurricup on 05.06.2016.
+ */
+public class TemplateToolkitBraceMatcher implements PairedBraceMatcher, TemplateToolkitElementTypes
+{
+	private static final BracePair[] PAIRS = new BracePair[]{
+			new BracePair(TT2_OPEN_TAG, TT2_CLOSE_TAG, false),
+	};
+
+	@Override
+	public BracePair[] getPairs()
+	{
+		return PAIRS;
+	}
+
+	@Override
+	public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType)
+	{
+		return false;
+	}
+
+	@Override
+	public int getCodeConstructStart(PsiFile file, int openingBraceOffset)
+	{
+		return openingBraceOffset;
+	}
+}
