9388395eb613cee0b467682f4611aca1fba5c4bf
hurricup <hurricup@gmail.com>
2016-06-05 15:50:48 +0300

    Implemented classes for TT2 filetype and language

---
+++ src/com/perl5/lang/tt2/filetypes/TemplateToolkitFileType.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.tt2.filetypes;
+
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.perl5.PerlIcons;
+import com.perl5.lang.tt2.TemplateToolkitLanguage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Created by hurricup on 05.06.2016.
+ */
+public class TemplateToolkitFileType extends LanguageFileType
+{
+	public static final FileType INSTANCE = new TemplateToolkitFileType();
+
+	public TemplateToolkitFileType()
+	{
+		super(TemplateToolkitLanguage.INSTANCE);
+	}
+
+	@NotNull
+	@Override
+	public String getName()
+	{
+		return "Template Toolkit";
+	}
+
+	@NotNull
+	@Override
+	public String getDescription()
+	{
+		return "Template Toolkit Template";
+	}
+
+	@NotNull
+	@Override
+	public String getDefaultExtension()
+	{
+		return "tt";
+	}
+
+	@Nullable
+	@Override
+	public Icon getIcon()
+	{
+		return PerlIcons.TTK2_ICON;
+	}
+
+}
