10ad4edaeee9ba226a3138e493d7c10add67f694
hurricup <hurricup@gmail.com>
2016-06-14 21:54:15 +0300

    Adjusted PerlVisitor to visit constants as sub definitions

--- src/com/perl5/lang/perl/psi/PerlVisitor.java
+++ src/com/perl5/lang/perl/psi/PerlVisitor.java
@@ -146,4 +146,10 @@ public class PerlVisitor extends PsiPerlVisitor
 	{
 		visitSubDefinitionBase(o);
 	}
+
+	@Override
+	public void visitConstantDefinition(@NotNull PsiPerlConstantDefinition o)
+	{
+		visitSubDefinitionBase(o);
+	}
 }
