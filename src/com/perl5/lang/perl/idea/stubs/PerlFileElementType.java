9e529137c09ac28b2d4be191988c482aa2f04af4
hurricup <hurricup@gmail.com>
2016-06-15 12:13:08 +0300

    Implemented error filter, fixes #1208

--- src/com/perl5/lang/perl/idea/stubs/PerlFileElementType.java
+++ src/com/perl5/lang/perl/idea/stubs/PerlFileElementType.java
@@ -28,7 +28,7 @@ import org.jetbrains.annotations.Nullable;
  */
 public class PerlFileElementType extends IStubFileElementType
 {
-	private static final int VERSION = 1;
+	private static final int VERSION = 2;
 
 	public PerlFileElementType(String debugName, Language language)
 	{
---
+++ src/com/perl5/lang/tt2/idea/highlighting/TemplateToolkitTemplateLanguageErrorFilter.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.tt2.idea.highlighting;
+
+import com.intellij.codeInsight.highlighting.TemplateLanguageErrorFilter;
+import com.intellij.psi.tree.TokenSet;
+import com.perl5.lang.tt2.TemplateToolkitFileViewProvider;
+import com.perl5.lang.tt2.elementTypes.TemplateToolkitElementTypes;
+
+/**
+ * Created by hurricup on 15.06.2016.
+ */
+public class TemplateToolkitTemplateLanguageErrorFilter extends TemplateLanguageErrorFilter implements TemplateToolkitElementTypes
+{
+	private static final TokenSet START_TOKENS = TokenSet.create(
+			TT2_OPEN_TAG,
+			TT2_OUTLINE_TAG
+	);
+
+	public TemplateToolkitTemplateLanguageErrorFilter()
+	{
+		super(START_TOKENS, TemplateToolkitFileViewProvider.class);
+	}
+}
