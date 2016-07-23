3351dd25aa521c46619d781a00c2c878ede0e881
hurricup <hurricup@gmail.com>
2016-07-23 10:01:20 +0300

    Unknown annotations now being highlighted as comments, fixes #1307

--- src/com/perl5/lang/perl/idea/annotators/PerlAnnotatorMisc.java
+++ src/com/perl5/lang/perl/idea/annotators/PerlAnnotatorMisc.java
@@ -25,6 +25,7 @@ import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.PsiWhiteSpace;
@@ -39,6 +40,7 @@ import com.perl5.lang.perl.psi.PerlAnnotation;
 import com.perl5.lang.perl.psi.PerlConstantDefinition;
 import com.perl5.lang.perl.psi.PerlLabel;
 import com.perl5.lang.perl.psi.PsiPerlNyiStatement;
+import com.perl5.lang.perl.psi.impl.PerlAnnotationContainerImpl;
 import com.perl5.lang.perl.psi.impl.PerlStringContentElementImpl;
 import com.perl5.lang.perl.util.PerlGlobUtil;
 import org.jetbrains.annotations.NotNull;
@@ -132,6 +134,18 @@ public class PerlAnnotatorMisc extends PerlAnnotator
 			if (lastChild instanceof PerlStringContentElementImpl)
 			{
 				holder.createInfoAnnotation(lastChild, null).setTextAttributes(PerlSyntaxHighlighter.PERL_SQ_STRING);
+			}
+		}
+		else if (element instanceof PerlAnnotationContainerImpl)
+		{
+			PsiElement run = element.getFirstChild();
+			while (run != null && !(run instanceof PsiComment || run instanceof PerlAnnotation))
+			{
+				if (!(run instanceof PsiWhiteSpace))
+				{
+					decorateElement(run, holder, PerlSyntaxHighlighter.PERL_COMMENT);
+				}
+				run = run.getNextSibling();
 			}
 		}
 		else if (element instanceof PerlLabel)
