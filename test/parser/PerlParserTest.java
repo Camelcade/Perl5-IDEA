7cfa956ab40bfcb5a86c9c1e307efeac44657ba4
hurricup <hurricup@gmail.com>
2016-10-22 15:47:38 +0300

    improved ambiguous variables parsing

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -31,6 +31,11 @@ public class PerlParserTest extends PerlParserTestBase
 		return "testData/parser/perl";
 	}
 
+	public void testAmbiguousSigils()
+	{
+		doTest();
+	}
+
 	public void testTryCatchHybrid()
 	{
 		doTest();
