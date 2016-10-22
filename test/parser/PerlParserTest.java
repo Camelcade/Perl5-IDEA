507cbb5c83ee653b9ae2cdd6133a0a123c4db5c5
hurricup <hurricup@gmail.com>
2016-10-22 13:09:24 +0300

    implemented hashes lexing after keys/values/each keywords

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -31,6 +31,11 @@ public class PerlParserTest extends PerlParserTestBase
 		return "testData/parser/perl";
 	}
 
+	public void testHashAcceptors()
+	{
+		doTest();
+	}
+
 	public void testKeywordAsLabel()
 	{
 		doTest();
