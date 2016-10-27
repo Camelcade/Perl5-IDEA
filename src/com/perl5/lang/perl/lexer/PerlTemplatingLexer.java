1e112a316d0396132f52d160ee57de531d36a514
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2016-10-27 11:44:49 +0300

    implemented Perl templating lexer that can delegate lexing to the perl lexer

---
+++ src/com/perl5/lang/perl/lexer/PerlTemplatingLexer.java
@@ -0,0 +1,64 @@
+package com.perl5.lang.perl.lexer;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+
+public abstract class PerlTemplatingLexer extends PerlProtoLexer
+{
+	private final PerlLexer myPerlLexer = new PerlLexer(null);
+
+	/**
+	 * syncronizes position of perl lexer with main one
+	 */
+	private void syncPerlLexer()
+	{
+		myPerlLexer.setTokenStart(getTokenStart());
+		myPerlLexer.setTokenEnd(getTokenEnd());
+	}
+
+	/**
+	 * syncronizes position the main lexer with perl one
+	 */
+	private void syncMainLexer()
+	{
+		setTokenStart(myPerlLexer.getTokenStart());
+		setTokenEnd(myPerlLexer.getTokenEnd());
+	}
+
+	@Override
+	public IElementType advance() throws IOException
+	{
+		if (myPerlLexer.hasPreparsedTokens())
+		{
+			IElementType result = myPerlLexer.advance();
+			if (!myPerlLexer.hasPreparsedTokens())
+			{
+				syncMainLexer();
+			}
+			return result;
+		}
+		return super.advance();
+	}
+
+	/**
+	 * Delegating current position to the perl lexer
+	 */
+	protected IElementType delegateLexing()
+	{
+		yypushback(yylength());
+		syncPerlLexer();
+		try
+		{
+			IElementType result = myPerlLexer.advance();
+			syncMainLexer();
+			return result;
+		}
+		catch (Exception ignore)
+		{
+		}
+		throw new RuntimeException("Something bad happened");
+	}
+}
