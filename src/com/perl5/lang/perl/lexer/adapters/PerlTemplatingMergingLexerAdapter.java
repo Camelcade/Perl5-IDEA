635792a0b2c06c7e563555d32b5527c6407ab4df
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2016-10-27 11:45:28 +0300

    implemented PerlTemplatingMergingLexerAdapter, that can merge template elements

---
+++ src/com/perl5/lang/perl/lexer/adapters/PerlTemplatingMergingLexerAdapter.java
@@ -0,0 +1,23 @@
+package com.perl5.lang.perl.lexer.adapters;
+
+import com.intellij.lexer.FlexAdapter;
+import com.intellij.lexer.FlexLexer;
+import com.intellij.lexer.MergingLexerAdapter;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.tree.TokenSet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class PerlTemplatingMergingLexerAdapter extends MergingLexerAdapter
+{
+	public PerlTemplatingMergingLexerAdapter(@Nullable Project project, @NotNull FlexLexer flexLexer, TokenSet tokensToMerge)
+	{
+		super(new PerlMergingLexerAdapter(
+						new PerlSublexingLexerAdapter(
+								project,
+								new FlexAdapter(flexLexer), false)
+				),
+				tokensToMerge
+		);
+	}
+}
