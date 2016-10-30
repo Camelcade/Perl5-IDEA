0b04b75d71ba8bcaa2f2146f868db428b246f894
hurricup <hurricup@gmail.com>
2016-10-30 12:08:30 +0300

    cleaned masonsettings map

--- src/com/perl5/lang/htmlmason/idea/configuration/HTMLMasonSettings.java
+++ src/com/perl5/lang/htmlmason/idea/configuration/HTMLMasonSettings.java
@@ -26,7 +26,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.psi.LanguageSubstitutor;
 import com.intellij.psi.LanguageSubstitutors;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import com.intellij.util.xmlb.annotations.Transient;
 import com.perl5.lang.htmlmason.elementType.HTMLMasonElementTypes;
@@ -41,7 +40,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 /**
  * Created by hurricup on 05.03.2016.
@@ -60,21 +58,11 @@ public class HTMLMasonSettings extends AbstractMasonSettings implements Persiste
 	public String autoHandlerName = "autohandler";
 	public String defaultHandlerName = "dhandler";
 	public List<String> substitutedExtensions = new ArrayList<String>();
-	public List<HTMLMasonCustomTag> customTags = new ArrayList<HTMLMasonCustomTag>();
-	@Transient
-	protected Pattern mySimpleOpenersPattern;
-	@Transient
-	protected Pattern myOpenersPattern;
-	@Transient
-	protected Pattern myClosersPattern;
+	public List<HTMLMasonCustomTag> customTags = new ArrayList<>();
 	@Transient
 	private Map<String, Pair<Language, LanguageSubstitutor>> substitutorMap = new THashMap<String, Pair<Language, LanguageSubstitutor>>();
 	@Transient
-	private Map<String, IElementType> myOpenTokensMap;
-	@Transient
 	private Map<String, String> myOpenCloseMap;
-	@Transient
-	private Map<String, IElementType> myCloseTokensMap;
 
 	public HTMLMasonSettings()
 	{
@@ -162,71 +150,12 @@ public class HTMLMasonSettings extends AbstractMasonSettings implements Persiste
 	public void settingsUpdated()
 	{
 		super.settingsUpdated();
-		mySimpleOpenersPattern = null;
-		myOpenersPattern = null;
-		myClosersPattern = null;
-		myOpenTokensMap = null;
 		myOpenCloseMap = null;
-		myCloseTokensMap = null;
 	}
 
 	public void prepareLexerConfiguration()
 	{
-		// regexp for simple openers
-		StringBuilder simpleBuilder = new StringBuilder("<%(");
-		boolean addOr = false;
-
-		for (String token : BUILTIN_TAGS_SIMPLE)
-		{
-			if (addOr)
-			{
-				simpleBuilder.append('|');
-			}
-			else
-			{
-				addOr = true;
-			}
-
-			simpleBuilder.append(token);
-		}
-
-		// regex for complex tags
-		StringBuilder complexBuilder = new StringBuilder("%(");
-		addOr = false;
-
-		for (String token : BUILTIN_TAGS_COMPLEX)
-		{
-			if (addOr)
-			{
-				complexBuilder.append('|');
-			}
-			else
-			{
-				addOr = true;
-			}
-
-			complexBuilder.append(token);
-		}
-
-		// map for open token keyword => open token token
-		myOpenTokensMap = new THashMap<String, IElementType>();
-		myOpenTokensMap.put(KEYWORD_PERL_OPENER, HTML_MASON_PERL_OPENER);
-		myOpenTokensMap.put(KEYWORD_INIT_OPENER, HTML_MASON_INIT_OPENER);
-		myOpenTokensMap.put(KEYWORD_CLEANUP_OPENER, HTML_MASON_CLEANUP_OPENER);
-		myOpenTokensMap.put(KEYWORD_ONCE_OPENER, HTML_MASON_ONCE_OPENER);
-		myOpenTokensMap.put(KEYWORD_SHARED_OPENER, HTML_MASON_SHARED_OPENER);
-		myOpenTokensMap.put(KEYWORD_FLAGS_OPENER, HTML_MASON_FLAGS_OPENER);
-		myOpenTokensMap.put(KEYWORD_ATTR_OPENER, HTML_MASON_ATTR_OPENER);
-		myOpenTokensMap.put(KEYWORD_ARGS_OPENER, HTML_MASON_ARGS_OPENER);
-		myOpenTokensMap.put(KEYWORD_FILTER_OPENER, HTML_MASON_FILTER_OPENER);
-		myOpenTokensMap.put(KEYWORD_DOC_OPENER, HTML_MASON_DOC_OPENER);
-		myOpenTokensMap.put(KEYWORD_TEXT_OPENER, HTML_MASON_TEXT_OPENER);
-
-		// parametrized
-		myOpenTokensMap.put(KEYWORD_METHOD_OPENER, HTML_MASON_METHOD_OPENER);
-		myOpenTokensMap.put(KEYWORD_DEF_OPENER, HTML_MASON_DEF_OPENER);
-
-		myOpenCloseMap = new THashMap<String, String>();
+		myOpenCloseMap = new THashMap<>();
 		myOpenCloseMap.put(KEYWORD_PERL_OPENER, KEYWORD_PERL_CLOSER);
 		myOpenCloseMap.put(KEYWORD_INIT_OPENER, KEYWORD_INIT_CLOSER);
 		myOpenCloseMap.put(KEYWORD_CLEANUP_OPENER, KEYWORD_CLEANUP_CLOSER);
@@ -243,62 +172,13 @@ public class HTMLMasonSettings extends AbstractMasonSettings implements Persiste
 		myOpenCloseMap.put(KEYWORD_METHOD_OPENER, KEYWORD_METHOD_CLOSER);
 		myOpenCloseMap.put(KEYWORD_DEF_OPENER, KEYWORD_DEF_CLOSER);
 
-		myCloseTokensMap = new THashMap<String, IElementType>();
-		myCloseTokensMap.put(KEYWORD_PERL_CLOSER, HTML_MASON_PERL_CLOSER);
-		myCloseTokensMap.put(KEYWORD_INIT_CLOSER, HTML_MASON_INIT_CLOSER);
-		myCloseTokensMap.put(KEYWORD_CLEANUP_CLOSER, HTML_MASON_CLEANUP_CLOSER);
-		myCloseTokensMap.put(KEYWORD_ONCE_CLOSER, HTML_MASON_ONCE_CLOSER);
-		myCloseTokensMap.put(KEYWORD_SHARED_CLOSER, HTML_MASON_SHARED_CLOSER);
-		myCloseTokensMap.put(KEYWORD_FLAGS_CLOSER, HTML_MASON_FLAGS_CLOSER);
-		myCloseTokensMap.put(KEYWORD_ATTR_CLOSER, HTML_MASON_ATTR_CLOSER);
-		myCloseTokensMap.put(KEYWORD_ARGS_CLOSER, HTML_MASON_ARGS_CLOSER);
-		myCloseTokensMap.put(KEYWORD_FILTER_CLOSER, HTML_MASON_FILTER_CLOSER);
-		myCloseTokensMap.put(KEYWORD_DOC_CLOSER, HTML_MASON_DOC_CLOSER);
-		myCloseTokensMap.put(KEYWORD_TEXT_CLOSER, HTML_MASON_TEXT_CLOSER);
-
-		// parametrized
-		myCloseTokensMap.put(KEYWORD_METHOD_CLOSER, HTML_MASON_METHOD_CLOSER);
-		myCloseTokensMap.put(KEYWORD_DEF_CLOSER, HTML_MASON_DEF_CLOSER);
-
 		// iterate custom tags
 		for (HTMLMasonCustomTag customTag : customTags)
 		{
-			HTMLMasonCustomTagRole role = customTag.getRole();
-
-			myOpenTokensMap.put(customTag.getOpenTagText(), role.getOpenToken());
-			myCloseTokensMap.put(customTag.getCloseTagText(), role.getCloseToken());
 			myOpenCloseMap.put(customTag.getOpenTagText(), customTag.getCloseTagText());
-
-			if (role.isSimple()) // simple tag
-			{
-				simpleBuilder.append('|');
-				simpleBuilder.append(customTag.getText());
-			}
-			else // custom tag
-			{
-				complexBuilder.append('|');
-				complexBuilder.append(customTag.getText());
-			}
 		}
-
-		simpleBuilder.append(")>");
-		complexBuilder.append(")");
-
-		// compiling patterns
-		mySimpleOpenersPattern = Pattern.compile(simpleBuilder.toString());
-		myOpenersPattern = Pattern.compile("<" + complexBuilder.toString());
-		myClosersPattern = Pattern.compile("</" + complexBuilder.toString() + ">");
 
 //		System.err.println("HTML::Mason lexer settings prepared");
-	}
-
-	public Map<String, IElementType> getCloseTokensMap()
-	{
-		if (myCloseTokensMap == null)
-		{
-			prepareLexerConfiguration();
-		}
-		return myCloseTokensMap;
 	}
 
 	public Map<String, String> getOpenCloseMap()
@@ -310,39 +190,4 @@ public class HTMLMasonSettings extends AbstractMasonSettings implements Persiste
 		return myOpenCloseMap;
 	}
 
-	public Map<String, IElementType> getOpenTokensMap()
-	{
-		if (myOpenTokensMap == null)
-		{
-			prepareLexerConfiguration();
-		}
-		return myOpenTokensMap;
-	}
-
-	public Pattern getClosersPattern()
-	{
-		if (myClosersPattern == null)
-		{
-			prepareLexerConfiguration();
-		}
-		return myClosersPattern;
-	}
-
-	public Pattern getOpenersPattern()
-	{
-		if (myOpenersPattern == null)
-		{
-			prepareLexerConfiguration();
-		}
-		return myOpenersPattern;
-	}
-
-	public Pattern getSimpleOpenersPattern()
-	{
-		if (mySimpleOpenersPattern == null)
-		{
-			prepareLexerConfiguration();
-		}
-		return mySimpleOpenersPattern;
-	}
 }
