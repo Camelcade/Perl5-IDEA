f1ef1a1f3cca105d64e8ab822df781afa69a3e96
hurricup <hurricup@gmail.com>
2016-11-02 15:45:08 +0300

    added support for dynaloader-like calls

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -31,6 +31,11 @@ public class PerlParserTest extends PerlParserTestBase
 		return "testData/parser/perl";
 	}
 
+	public void testDynaLoaderCall()
+	{
+		doTest();
+	}
+
 	public void testFancyImport()
 	{
 		doTest();
