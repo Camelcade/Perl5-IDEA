6e8e15e18ebaa4a89c70b2a65d867734e57173e1
hurricup <hurricup@gmail.com>
2016-11-04 17:00:45 +0300

    re-factored heredoc reference

--- src/com/perl5/lang/perl/psi/references/PerlHeredocReference.java
+++ src/com/perl5/lang/perl/psi/references/PerlHeredocReference.java
@@ -16,31 +16,83 @@
 
 package com.perl5.lang.perl.psi.references;
 
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.resolve.ResolveCache;
+import com.intellij.psi.*;
 import com.intellij.util.IncorrectOperationException;
+import com.perl5.lang.perl.psi.PerlHeredocOpener;
 import com.perl5.lang.perl.psi.PerlHeredocTerminatorElement;
-import com.perl5.lang.perl.psi.references.resolvers.PerlHeredocResolver;
+import com.perl5.lang.perl.psi.impl.PerlHeredocElementImpl;
 import com.perl5.lang.perl.psi.utils.PerlElementFactory;
+import com.perl5.lang.perl.psi.utils.PerlPsiUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class PerlHeredocReference extends PerlReference<PerlHeredocTerminatorElement>
+public class PerlHeredocReference extends PerlCachingReference<PerlHeredocTerminatorElement>
 {
-	private static final PerlHeredocResolver RESOLVER = new PerlHeredocResolver();
-
 	public PerlHeredocReference(@NotNull PerlHeredocTerminatorElement element, TextRange textRange)
 	{
 		super(element, textRange);
 	}
 
-	@Nullable
 	@Override
-	public PsiElement resolve()
+	protected ResolveResult[] resolveInner(boolean incompleteCode)
 	{
-		return ResolveCache.getInstance(myElement.getProject()).resolveWithCaching(this, RESOLVER, true, false);
+		PsiElement result = null;
+		PsiElement run = getElement();
+		if ((run = run.getPrevSibling()) != null) // new line or here-doc
+		{
+			if (run instanceof PerlHeredocElementImpl)
+			{
+				run = run.getPrevSibling();    // moving to newline before heredoc
+				if (run == null)
+				{
+					return ResolveResult.EMPTY_ARRAY;
+				}
+			}
+
+			PsiElement predecessor = run;
+			if (predecessor instanceof PsiWhiteSpace) // empty here-docs has no newline after them
+			{
+				predecessor = predecessor.getPrevSibling();
+			}
+
+			if (predecessor instanceof PerlHeredocTerminatorElement)    // sequential here-docs
+			{
+				PsiReference reference = predecessor.getReference();
+				if (reference != null)
+				{
+					PsiElement prevOpener = reference.resolve();
+					if (prevOpener != null)
+					{
+						HeredocSeeker seeker = new HeredocSeeker(run.getNode().getStartOffset(), prevOpener);
+						PerlPsiUtil.iteratePsiElementsRight(prevOpener, seeker);
+						result = seeker.getResult();
+					}
+				}
+			}
+			else    // first here-doc in a row
+			{
+				final PsiDocumentManager manager = PsiDocumentManager.getInstance(run.getProject());
+				final PsiFile file = run.getContainingFile();
+				final Document document = manager.getDocument(file);
+
+				if (document != null)
+				{
+					int lineNumber = document.getLineNumber(run.getNode().getStartOffset());
+					PsiElement firstLineElement = file.findElementAt(document.getLineStartOffset(lineNumber));
+					if (firstLineElement != null)
+					{
+						HeredocSeeker seeker = new HeredocSeeker(run.getNode().getStartOffset(), null);
+						PerlPsiUtil.iteratePsiElementsRight(firstLineElement, seeker);
+						result = seeker.getResult();
+					}
+				}
+			}
+
+		}
+		return result == null ? ResolveResult.EMPTY_ARRAY : PsiElementResolveResult.createResults(result);
 	}
 
 	@Override
@@ -58,4 +110,37 @@ public class PerlHeredocReference extends PerlReference<PerlHeredocTerminatorEle
 
 		return result;
 	}
+
+	static private class HeredocSeeker extends PerlPsiUtil.HeredocProcessor
+	{
+		protected PerlHeredocOpener myResult = null;
+		protected PsiElement myAnchor = null;
+
+		public HeredocSeeker(int lineEndOffset, @Nullable PsiElement anchor)
+		{
+			super(lineEndOffset);
+			myAnchor = anchor;
+		}
+
+		@Override
+		public boolean process(PsiElement element)
+		{
+			if (element == null || element.getNode().getStartOffset() >= lineEndOffset)
+			{
+				return false;
+			}
+			if (element != myAnchor && element instanceof PerlHeredocOpener)
+			{
+				myResult = (PerlHeredocOpener) element;
+				return false;
+			}
+			return true;
+		}
+
+		public PerlHeredocOpener getResult()
+		{
+			return myResult;
+		}
+	}
+
 }
