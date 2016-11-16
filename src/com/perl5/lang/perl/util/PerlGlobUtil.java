db862898a5aae8f5901bb6fb2885ef0130256e9d
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2016-11-16 12:02:59 +0300

    fixed merge errors

--- src/com/perl5/lang/perl/util/PerlGlobUtil.java
+++ src/com/perl5/lang/perl/util/PerlGlobUtil.java
@@ -28,6 +28,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 
 /**
--- src/com/perl5/lang/perl/util/PerlScalarUtil.java
+++ src/com/perl5/lang/perl/util/PerlScalarUtil.java
@@ -31,6 +31,7 @@ import com.perl5.lang.perl.util.processors.PerlScalarImportsCollector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
