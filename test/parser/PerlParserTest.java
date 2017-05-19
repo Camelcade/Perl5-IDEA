63664f2b382ad003d1d88cff9b7bc3cd19fd86fd
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2017-05-19 14:32:06 +0300

    fixed lexing of \w\d bare here-doc openers, fixes #1434

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -29,6 +29,8 @@ public class PerlParserTest extends PerlParserTestBase {
     return "testData/parser/perl";
   }
 
+  public void testIssue1434() {doTest();}
+
   public void testLexicalSubs() {doTest();}
 
   public void testComplexModifiers() {doTest();}
