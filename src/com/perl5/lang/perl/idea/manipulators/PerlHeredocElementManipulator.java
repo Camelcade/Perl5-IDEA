361d6e4de858795780595312b3360a3dc7532e30
hurricup <hurricup@gmail.com>
2017-05-25 22:31:11 +0300

    impelemented here-doc auto-adjustment on update

--- src/com/perl5/lang/perl/idea/manipulators/PerlHeredocElementManipulator.java
+++ src/com/perl5/lang/perl/idea/manipulators/PerlHeredocElementManipulator.java
@@ -18,7 +18,10 @@ package com.perl5.lang.perl.idea.manipulators;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
+import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
 import com.intellij.util.IncorrectOperationException;
+import com.perl5.lang.perl.PerlLanguage;
 import com.perl5.lang.perl.psi.impl.PerlHeredocElementImpl;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,9 +33,40 @@ public class PerlHeredocElementManipulator extends PerlTextContainerManipulator<
   @Override
   public PerlHeredocElementImpl handleContentChange(@NotNull PerlHeredocElementImpl element, @NotNull TextRange range, String newContent)
     throws IncorrectOperationException {
+    StringBuilder sb = new StringBuilder(newContent);
     if (element.getTextLength() == range.getEndOffset() && !StringUtil.endsWith(newContent, "\n")) {
-      newContent = newContent + "\n";
+      sb.append("\n");
     }
-    return super.handleContentChange(element, range, newContent);
+
+    int indentSize = element.getIndentSize();
+    if (indentSize > 0) {
+      CommonCodeStyleSettings.IndentOptions indentOptions =
+        CodeStyleSettingsManager.getInstance(element.getProject()).getCurrentSettings().
+          getCommonSettings(PerlLanguage.INSTANCE)
+          .getIndentOptions();
+
+      String indenter = StringUtil.repeat(indentOptions != null && indentOptions.USE_TAB_CHARACTER ? "\t" : " ", indentSize);
+      int currentSourceOffset = 0;
+      int currentLineStart = 0;
+      int targetOffset = 0;
+      sb.insert(targetOffset, indenter);
+      targetOffset += indentSize;
+      int sourceLength = newContent.length();
+      while (currentSourceOffset < sourceLength) {
+        if (newContent.charAt(currentSourceOffset++) == '\n' && currentSourceOffset < sourceLength) {
+          int lineSize = currentSourceOffset - currentLineStart;
+          if (lineSize > 1) {
+            sb.insert(targetOffset + lineSize, indenter);
+            targetOffset += lineSize + indentSize;
+          }
+          else {
+            targetOffset += lineSize;
+          }
+          currentLineStart = currentSourceOffset;
+        }
+      }
+    }
+
+    return super.handleContentChange(element, range, sb.toString());
   }
 }
