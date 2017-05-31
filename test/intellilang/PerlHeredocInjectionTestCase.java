ecda5da52289f34d5394b6c8822fe032f8f357d4
hurricup <hurricup@gmail.com>
2017-05-31 23:11:47 +0300

    Improved heredoc element search

--- test/intellilang/PerlHeredocInjectionTestCase.java
+++ test/intellilang/PerlHeredocInjectionTestCase.java
@@ -1,7 +1,9 @@
 package intellilang;
 
 import base.PerlLightCodeInsightFixtureTestCase;
+import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.perl5.lang.perl.psi.impl.PerlHeredocElementImpl;
 import org.jetbrains.annotations.NotNull;
@@ -9,10 +11,19 @@ import org.jetbrains.annotations.NotNull;
 public abstract class PerlHeredocInjectionTestCase extends PerlLightCodeInsightFixtureTestCase {
   @NotNull
   protected PerlHeredocElementImpl getHeredocUnderCursor() {
-    PsiElement leafElement = getFile().getViewProvider().findElementAt(getEditor().getCaretModel().getOffset());
-    assert leafElement != null;
-    PerlHeredocElementImpl heredocElement = PsiTreeUtil.getParentOfType(leafElement, PerlHeredocElementImpl.class);
-    assert heredocElement != null;
+    PsiElement sourceElement = getFile().getViewProvider().findElementAt(getEditor().getCaretModel().getOffset());
+    assertNotNull(sourceElement);
+    PsiFile leafFile = sourceElement.getContainingFile();
+    if (InjectedLanguageManager.getInstance(getProject()).isInjectedFragment(leafFile)) {
+      sourceElement = leafFile.getContext();
+      assertNotNull(sourceElement);
+    }
+
+    if (sourceElement instanceof PerlHeredocElementImpl) {
+      return (PerlHeredocElementImpl)sourceElement;
+    }
+    PerlHeredocElementImpl heredocElement = PsiTreeUtil.getParentOfType(sourceElement, PerlHeredocElementImpl.class);
+    assertNotNull(heredocElement);
     return heredocElement;
   }
 }
