f2c609a76a1d28e71acd2dfb5eeda24cddb22285
hurricup <hurricup@gmail.com>
2017-06-27 22:55:59 +0300

    Added helpers resolve test

--- test/resolve/mojo/MojoResolveTest.java
+++ test/resolve/mojo/MojoResolveTest.java
@@ -31,4 +31,6 @@ public class MojoResolveTest extends PerlResolveTestCase {
   }
 
   public void testImplicitVariables() {doTestWithFileCheck();}
+
+  public void testHelpers() {doTestWithFileCheck();}
 }
