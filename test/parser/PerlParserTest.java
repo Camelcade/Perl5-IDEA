23fad218e4ffaa522abecd686b15bd9b69d46a21
hurricup <hurricup@gmail.com>
2017-06-28 22:14:14 +0300

    Implemented lexing of a single -bareword in use statement

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -29,6 +29,8 @@ public class PerlParserTest extends PerlParserTestBase {
     return "testData/parser/perl";
   }
 
+  public void testUseBareword() {doTest();}
+
   public void testClassAccessor() {doTest();}
 
   public void testExceptionClass() {doTest();}
