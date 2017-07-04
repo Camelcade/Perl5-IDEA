c45ce3523c5d9aa2f6c89c9e92105da6722b05f0
hurricup <hurricup@gmail.com>
2017-07-04 23:05:27 +0300

    Added test for fileLevelInspection

--- test/annotator/PerlAnnotatorTest.java
+++ test/annotator/PerlAnnotatorTest.java
@@ -30,6 +30,8 @@ public class PerlAnnotatorTest extends PerlLightCodeInsightFixtureTestCase {
     return "testData/annotator/perl";
   }
 
+  public void testFileLevelVariableInspection() {doTest(PerlFileLevelVariableInspection.class);}
+
   public void testBuiltInVariableRedeclarationInspection() {doTest(PerlBuiltinVariableRedeclarationInspection.class);}
 
   public void testUseStrictInspection() {doTest(PerlUseStrictInspection.class);}
