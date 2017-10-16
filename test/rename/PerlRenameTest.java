287d2e5d50e8239d51992ab6a38813c400575b4e
hurricup <hurricup@gmail.com>
2017-10-16 20:39:55 +0300

    Added rename test #1599

--- test/rename/PerlRenameTest.java
+++ test/rename/PerlRenameTest.java
@@ -25,6 +25,8 @@ public class PerlRenameTest extends PerlRenameTestCase {
     return "testData/rename/perl";
   }
 
+  public void testPackageTag() {doTest();}
+
   public void testAccessorModification() {doTest();}
 
   public void testMojoAttrs() {doTest();}
