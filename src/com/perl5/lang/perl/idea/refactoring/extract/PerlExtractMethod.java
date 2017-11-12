b9fea2c2e4bf6e87bb0ea8509183ed569227344b
William Taylor <williamt@rebelfactions.com>
2017-11-12 15:00:16 -0800

    changes based on feedback

---
+++ src/com/perl5/lang/perl/idea/refactoring/extract/PerlExtractMethod.java
@@ -0,0 +1,353 @@
+/*
+ * Copyright 2015-2017 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.perl.idea.refactoring.extract;
+
+import com.intellij.refactoring.util.AbstractVariableData;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PerlExtractMethod {
+
+  private String codeSelection;
+  private String className = "";
+
+  private final Set<String> hashVariables = new HashSet<>();
+  private final Set<String> arrayVariables = new HashSet<>();
+  private final Set<String> scalarVariables = new HashSet<>();
+  private final Set<String> loopScalarVariables = new HashSet<>();
+  private final Set<String> localScalarVariables = new HashSet<>();
+  private final Set<String> localArrayVariables = new HashSet<>();
+  private final Set<String> localHashVariables = new HashSet<>();
+
+
+  private final ArrayList<String> parameters = new ArrayList<>();
+  private final ArrayList<String> innerVariables = new ArrayList<>();
+  private final ArrayList<String> outerVariables = new ArrayList<>();
+
+  /**
+   * @return The code selection passed in and modified from refactoring
+   */
+  public String getSelectedCode() {
+    return codeSelection;
+  }
+
+  /**
+   * @return An array of parameters to be passed in to a method
+   */
+  public List<String> getParameters() {
+    return parameters;
+  }
+
+  /**
+   * @return An array of parameters to be returned from a method
+   */
+  public List<String> getInnerVariables() {
+    return innerVariables;
+  }
+
+  /**
+   * @return returns an array of parameters with arrays and hashes as scalars.
+   */
+  public List<String> getOuterVariablesModified() {
+    String reg = "[@%](.*)";
+    Pattern regex = Pattern.compile(reg);
+    ArrayList<String> ovs = new ArrayList<>();
+
+    for (String s : this.outerVariables) {
+      Matcher matcher = regex.matcher(s);
+      if (matcher.find()) {
+        s = s.replaceAll(reg, "\\$" + matcher.group(1));
+      }
+      ovs.add(s);
+    }
+
+    return ovs;
+  }
+
+  public PerlExtractMethod(String codeSelection) {
+    this.codeSelection = codeSelection;
+
+    parseVariables();
+    parseLocalVariables();
+    extractMethodVariables();
+  }
+
+  /**
+   * @param methodName       Name of method to generate
+   * @param methodParameters parameters to pass into method
+   * @param methodBody       body of the generated method
+   * @param methodReturn     return string ie "return ($foo);"
+   * @return returns a built method
+   */
+  public String buildMethod(@NotNull String methodName,
+                            @Nullable AbstractVariableData[] methodParameters,
+                            @NotNull String methodBody,
+                            @Nullable String methodReturn) {
+    StringBuilder builder = new StringBuilder("sub ");
+    builder.append(methodName).append(" {\n");
+
+    if (methodParameters != null) {
+      // Make sure we are passing in parameters
+      for (AbstractVariableData mp : methodParameters) {
+        if (mp.passAsParameter) {
+          builder.append(this.buildMethodParameters(methodParameters)).append("\n\n");
+          break;
+        }
+      }
+    }
+
+    builder.append(methodBody).append("\n");
+
+    builder.append(methodReturn).append("\n");
+    builder.append("}\n");
+
+    return builder.toString();
+  }
+
+  private void extractMethodVariables() {
+    String reg1;
+    String reg2;
+    String arrayRef;
+
+    for (String param : this.scalarVariables) {
+      if (!this.localScalarVariables.contains(param)) {
+        this.parameters.add(param);
+      }
+      else {
+        if (this.loopScalarVariables.contains(param)) {
+          continue;
+        }
+
+        if (!param.matches("\\$\\d+$")) {
+          this.innerVariables.add(param);
+          this.outerVariables.add(param);
+        }
+      }
+    }
+
+    for (String param : this.arrayVariables) {
+      param = param.replace("$", "@");
+
+      if (!this.localArrayVariables.contains(param)) {
+        this.parameters.add(param);
+        reg2 = String.format("\\%s", param);
+        arrayRef = param;
+        arrayRef = arrayRef.replace("@", "$");
+        this.codeSelection = this.codeSelection.replaceAll(reg2, "@" + arrayRef);
+        param = param.replace("@", "$");
+        reg1 = String.format("\\%s\\[", param);
+        this.codeSelection = this.codeSelection.replaceAll(reg1, param + "->[");
+      }
+      else {
+        this.innerVariables.add("\\" + param);
+        this.outerVariables.add(param);
+      }
+    }
+
+    for (String param : this.hashVariables) {
+      param = param.replace("$", "%");
+
+      if (!this.localHashVariables.contains(param)) {
+        this.parameters.add(param);
+        reg1 = String.format("\\%s", param);
+        String hashRef = param.replace("%", "$");
+        this.codeSelection = this.codeSelection.replaceAll(reg1, String.format("%%\\%s", hashRef));
+
+        param = param.replace("%", "$");
+        reg2 = String.format("\\%s\\{", param);
+        this.codeSelection = this.codeSelection.replaceAll(reg2, "\\" + param + "->{");
+      }
+      else {
+        this.innerVariables.add("\\" + param);
+        this.outerVariables.add(param);
+      }
+    }
+  }
+
+
+  /**
+   * @return method return string
+   */
+  public String buildMethodReturn() {
+    StringBuilder builder = new StringBuilder();
+    if (this.innerVariables.size() > 0) {
+      builder.append("\n    return (");
+      boolean first = true;
+      for (String innerVariable : this.innerVariables) {
+        if (first) {
+          first = false;
+        }
+        else {
+          builder.append(", ");
+        }
+        builder.append(innerVariable);
+      }
+      builder.append(");");
+    }
+    else {
+      builder.append("\n    return;");
+    }
+    return builder.toString();
+  }
+
+  /**
+   * @param variableData
+   * @return built string of method parameters
+   */
+  public String buildMethodParameters(@NotNull AbstractVariableData[] variableData) {
+    StringBuilder builder = new StringBuilder();
+
+    if (!this.className.isEmpty()) {
+      builder.append("    my ").append(this.className).append(" = shift;\n");
+    }
+
+    builder.append("    my (");
+    boolean first = true;
+    for (AbstractVariableData vd : variableData) {
+      if (!vd.passAsParameter) {
+        continue;
+      }
+
+      if (first) {
+        first = false;
+      }
+      else {
+        builder.append(", ");
+      }
+      builder.append(vd.name.replaceAll("[%@]", "\\$"));
+    }
+    builder.append(") = @_;");
+    return builder.toString();
+  }
+
+  private void parseLocalVariables() {
+
+    for (String var : this.scalarVariables) {
+      String reg1 = String.format("\\s*my\\s*\\%s\\s*[=;(]", var);
+      String reg2 = String.format("\\s*my\\s*\\(.*?\\%s.*?\\)", var);
+      String reg3 = String.format("(?:for|foreach)\\s+my\\s*\\%s\\s*\\(", var);
+
+      Pattern pattern1 = Pattern.compile(reg1);
+      Pattern pattern2 = Pattern.compile(reg2);
+      Pattern pattern3 = Pattern.compile(reg3);
+
+      if (var.matches("(?:\\$\\d+$|\\$[ab]$)")) {
+        this.localScalarVariables.add(var);
+      }
+      else if (pattern1.matcher(codeSelection).find() || pattern2.matcher(codeSelection).find()) {
+        this.localScalarVariables.add(var);
+
+        // Skip loop variables
+        if (pattern3.matcher(codeSelection).find()) {
+          this.loopScalarVariables.add(var);
+        }
+      }
+    }
+
+    for (String var : this.arrayVariables) {
+      var = var.replace("$", "@");
+
+      String reg1 = String.format("\\s*my\\s*\\%s\\s*[=;(]", var);
+      String reg2 = String.format("\\s*my\\s*\\(.*?\\%s.*?\\)", var);
+      Pattern pattern1 = Pattern.compile(reg1);
+      Pattern pattern2 = Pattern.compile(reg2);
+
+      if (pattern1.matcher(codeSelection).find() || pattern2.matcher(codeSelection).find()) {
+        localArrayVariables.add(var);
+      }
+    }
+
+    for (String var : this.hashVariables) {
+      var = var.replace("$", "%");
+      String reg1 = String.format("\\s*my\\s*\\%s\\s*[=;(]", var);
+      String reg2 = String.format("\\s*my\\s*\\(.*?\\%s.*?\\)", var);
+      Pattern pattern1 = Pattern.compile(reg1);
+      Pattern pattern2 = Pattern.compile(reg2);
+
+      if (pattern1.matcher(codeSelection).find() || pattern2.matcher(codeSelection).find()) {
+        this.localHashVariables.add(var);
+      }
+    }
+  }
+
+  private void parseVariables() {
+    Pattern regex = Pattern.compile("([$@]\\w+?)(\\W)");
+    Pattern classNameRegex = Pattern.compile("^(\\$self|\\$this|\\$class)$");
+
+    String[] codeArray = this.codeSelection.split("\n");
+    for (String code : codeArray) {
+      // Strip out comments
+      code = code.replaceAll("#.*", "");
+
+      Matcher matcher = regex.matcher(code);
+      while (matcher.find()) {
+        String variable = matcher.group(1);
+        String hint = matcher.group(2);
+
+        // TODO: This might need to be .* or pattern/matcher
+        // Skip $_
+        if (variable.matches("^\\$_$")) {
+          continue;
+        }
+
+        // Save class naming
+        Matcher classMatcher = classNameRegex.matcher(variable);
+        if (classMatcher.find()) {
+          this.className = classMatcher.group(1);
+          continue;
+        }
+
+        if (hint.matches("^\\{")) {
+          variable = variable.replace("$", "%");
+          this.hashVariables.add(variable);
+        }
+        else if (hint.matches("^\\[")) {
+          variable = variable.replace("$", "@");
+          this.arrayVariables.add(variable);
+        }
+        else if (hint.matches("^@")) {
+          this.arrayVariables.add(variable);
+        }
+        else if (hint.matches("^%")) {
+          this.hashVariables.add(variable);
+        }
+        else {
+          this.scalarVariables.add(variable);
+        }
+      }
+    }
+  }
+}
--- src/com/perl5/lang/perl/idea/refactoring/extract/PerlExtractMethodHandler.java
+++ src/com/perl5/lang/perl/idea/refactoring/extract/PerlExtractMethodHandler.java
@@ -16,10 +16,20 @@
 package com.perl5.lang.perl.idea.refactoring.extract;
 
 import com.intellij.codeInsight.codeFragment.CodeFragment;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.RefactoringActionHandler;
@@ -28,9 +38,9 @@ import com.intellij.refactoring.extractMethod.ExtractMethodDecorator;
 import com.intellij.refactoring.extractMethod.ExtractMethodValidator;
 import com.intellij.refactoring.util.AbstractVariableData;
 import com.intellij.util.containers.ArrayListSet;
+import com.intellij.util.containers.ContainerUtil;
+import com.perl5.PerlBundle;
 import com.perl5.lang.perl.fileTypes.PerlFileTypeScript;
-import com.perl5.lang.perl.idea.refactoring.PerlRefactorUtils;
-import com.perl5.lang.perl.idea.refactoring.PerlRefactor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import java.util.Set;
@@ -40,50 +50,40 @@ public class PerlExtractMethodHandler implements RefactoringActionHandler {
   @Override
   public void invoke(@NotNull Project project, Editor editor, PsiFile file, DataContext dataContext) {
 
-    final SelectionModel selectionModel = editor.getSelectionModel();
-    if (!selectionModel.hasSelection()) selectionModel.selectLineAtCaret();
+    SelectionModel selectionModel = editor.getSelectionModel();
+    if (!selectionModel.hasSelection()) {
+      selectionModel.selectLineAtCaret();
+    }
 
-    final int selectionStartLine = selectionModel.getSelectionStart();
-    final int selectionLength = selectionModel.getSelectionEnd() - selectionStartLine;
+    int selectionStartLine = selectionModel.getSelectionStart();
+    int selectionLength = selectionModel.getSelectionEnd() - selectionStartLine;
     String selectedText = selectionModel.getSelectedText();
-
-    if (selectedText == null || selectedText.isEmpty()) {
+    if (StringUtil.isEmpty(selectedText)) {
       return;
     }
 
-    PerlRefactor perlRefactor = new PerlRefactor(selectedText, "myNewMethod");
-
-    Set<String> parameterVariables = new ArrayListSet<>();
-    parameterVariables.addAll(perlRefactor.getParameters());
+    PerlExtractMethod perlExtractMethod = new PerlExtractMethod(selectedText);
+    Set<String> parameterVariables = ContainerUtil.newHashSet();
+    parameterVariables.addAll(perlExtractMethod.getParameters());
 
     Set<String> returnVariables = new ArrayListSet<>();
-    returnVariables.addAll(perlRefactor.getInnerVariables());
+    returnVariables.addAll(perlExtractMethod.getInnerVariables());
 
     // TODO: create PerlCodeFragment that contains my(...) = foo(bar);
     // TODO: figure how to use returnInside
-    CodeFragment fragment = new CodeFragment(parameterVariables, returnVariables,false);
+    CodeFragment fragment = new CodeFragment(parameterVariables, returnVariables, false);
 
     // This populates the parameters and signature
-    final ExtractMethodDecorator decorator = (methodName, variableData) -> {
+    ExtractMethodDecorator decorator = (methodName, variableData) -> {
 
       StringBuilder builder = new StringBuilder();
-      boolean first = true;
-
-      if (perlRefactor.getOuterVariablesModified().size() > 0) {
+      if (perlExtractMethod.getOuterVariablesModified().size() > 0) {
         builder.append("my (");
-        for (String ov : perlRefactor.getOuterVariablesModified()) {
-          if (first) {
-            first = false;
-          }
-          else {
-            builder.append(", ");
-          }
-          builder.append(ov);
-        }
+        builder.append(StringUtil.join(perlExtractMethod.getOuterVariablesModified(), ","));
         builder.append(") = ");
-        first = true;
       }
 
+      boolean first = true;
       builder.append(methodName).append("(");
       for (AbstractVariableData vd : variableData) {
         if (!vd.passAsParameter) continue;
@@ -101,7 +101,7 @@ public class PerlExtractMethodHandler implements RefactoringActionHandler {
     };
 
     //This validator should check if name will clash with existing methods
-    final ExtractMethodValidator validator = new ExtractMethodValidator() {
+    ExtractMethodValidator validator = new ExtractMethodValidator() {
       // TODO: Check for existing methods
       @Nullable
       @Override
@@ -115,7 +115,7 @@ public class PerlExtractMethodHandler implements RefactoringActionHandler {
       }
     };
 
-    final AbstractExtractMethodDialog dialog = new AbstractExtractMethodDialog(project, "myNewMethod",
+    AbstractExtractMethodDialog dialog = new AbstractExtractMethodDialog(project, "myNewMethod",
                                                                                fragment, validator, decorator,
                                                                                PerlFileTypeScript.INSTANCE);
     dialog.show();
@@ -123,17 +123,60 @@ public class PerlExtractMethodHandler implements RefactoringActionHandler {
     if (dialog.isOK()) {
       String methodName = dialog.getMethodName();
       AbstractVariableData[] variableData = dialog.getAbstractVariableData();
-      final PerlRefactorUtils refactoring = new PerlRefactorUtils(file, selectedText, methodName, selectionStartLine, selectionLength);
 
       String methodSignature = decorator.createMethodSignature(methodName, variableData);
 
-      String method = perlRefactor.buildMethod(methodName, variableData, perlRefactor.getCodeSelection(), perlRefactor.buildMethodReturn());
-      refactoring.writeMethod(methodSignature, method);
+      String method = perlExtractMethod.buildMethod(methodName, variableData, perlExtractMethod.getSelectedCode(), perlExtractMethod.buildMethodReturn());
+      this.writeMethod(file, project, selectionStartLine, selectionLength, methodSignature, method);
     }
   }
 
   @Override
   public void invoke(@NotNull Project project, @NotNull PsiElement[] elements, DataContext dataContext) {
     // Does Nothing
+  }
+
+  public void writeMethod(PsiFile file, Project project, int selectionStartLine, int selectionLength, String methodCall, String methodBody) {
+
+    Document document = file.getViewProvider().getDocument();
+    if (document == null) return;
+
+    VirtualFile virtualFile = file.getVirtualFile();
+    if (virtualFile == null) return;
+
+
+    new Task.Backgroundable(project, PerlBundle.message("perl.refactor.running"), false) {
+      @Override
+      public void run(@NotNull ProgressIndicator indicator) {
+        try {
+          // TODO: run methodBody through perl tidy
+          // TODO: figure out best place to add new method to document. Look how eclipse or padre does it?
+          // TODO: update bundle messages
+
+          // Write method call
+          WriteCommandAction.runWriteCommandAction(project, () -> {
+            document.replaceString(selectionStartLine, selectionStartLine + selectionLength, StringUtil.join("\n",methodCall, "\n"));
+            PsiDocumentManager.getInstance(project).commitDocument(document);
+          });
+
+          // Write method body
+          WriteCommandAction.runWriteCommandAction(project, () -> {
+            document.insertString(
+              document.getLineEndOffset(
+                document.getLineCount() - 1),
+              StringUtil.join(methodBody, "\n")
+            );
+            PsiDocumentManager.getInstance(project).commitDocument(document);
+          });
+        }
+        catch (Exception e) {
+          Notifications.Bus.notify(new Notification(
+            "PERL5_PERL_REFACTOR",
+            "Error Refactoring",
+            e.getMessage(),
+            NotificationType.ERROR));
+        }
+      }
+    }.queue();
   }
 }
