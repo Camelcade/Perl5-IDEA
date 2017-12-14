4b729f491677f0ece55078c1f970fc5ded9dfafa
hurricup <hurricup@gmail.com>
2017-12-14 21:37:48 +0300

    Fixed bug with $1x parsing
    Fixes #1667

--- test/parser/PerlParserTest.java
+++ test/parser/PerlParserTest.java
@@ -30,6 +30,8 @@ public class PerlParserTest extends PerlParserTestBase {
     return "testData/parser/perl";
   }
 
+  public void testNumericX() {doTest();}
+
   public void testStringCmpWithPackage() {doTest();}
 
   public void testIssue1655() {doTest();}
