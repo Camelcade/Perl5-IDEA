12399db3a60ea217bf844f40a837449c35a50865
Alexandr Evstigneev <hurricup@gmail.com>
2019-02-19 21:01:57 +0300

    Moved FindUsages highlighting test to appropriate package

--- core/test/findUsages/perl/PerlUsagesHighlightingTest.java
+++ core/test/findUsages/perl/PerlUsagesHighlightingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2017 Alexandr Evstigneev
+ * Copyright 2015-2018 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-package editor;
+package findUsages.perl;
 
 import base.PerlLightTestCase;
 
 public class PerlUsagesHighlightingTest extends PerlLightTestCase {
   @Override
   protected String getTestDataPath() {
-    return "testData/highlighting/usages/perl";
+    return "testData/findusages/highlighting/perl";
   }
 
   public void testCaptureGroupsScalars() {doTest();}
