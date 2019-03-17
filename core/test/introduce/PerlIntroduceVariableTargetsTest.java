9e5616cc6b554dedad9c47449b3b5e4a00e2c287
Alexandr Evstigneev <hurricup@gmail.com>
2019-03-17 14:13:50 +0300

    Added tests for target selection in different cases

--- core/test/introduce/PerlIntroduceVariableTargetsTest.java
+++ core/test/introduce/PerlIntroduceVariableTargetsTest.java
@@ -29,7 +29,62 @@ public class PerlIntroduceVariableTargetsTest extends PerlLightTestCase {
     return "testData/introduce/targets";
   }
 
+  public void testAnd() {doTest();}
+
+  public void testBitwiseAnd() {doTest();}
+
+  public void testBitwiseOr() {doTest();}
+
+  public void testCondition() {doTest();}
+
   public void testConstructorCall() {doTest();}
+
+  public void testDereference() {doTest();}
+
+  public void testIfModifier() {doTest();}
+
+  public void testLpAnd() {doTest();}
+
+  public void testLpOr() {doTest();}
+
+  public void testMinus() {doTest();}
+
+  public void testMul() {doTest();}
+
+  public void testOr() {doTest();}
+
+  public void testPlainStringQ() {doTest();}
+
+  public void testPlainStringQQ() {doTest();}
+
+  public void testPlainStringQQWithVariable() {doTest();}
+
+  public void testPlainStringQWithVariable() {doTest();}
+
+  public void testPlainStringQX() {doTest();}
+
+  public void testPlainStringQXWithVariable() {doTest();}
+
+  public void testPlus() {doTest();}
+
+  public void testQqString() {doTest();}
+
+  public void testQqStringWithVariable() {doTest();}
+
+  public void testQString() {doTest();}
+
+  public void testQStringWithVariable() {doTest();}
+
+  public void testQxString() {doTest();}
+
+  public void testQxStringWithVariable() {doTest();}
+
+  public void testQwList() {doTest();}
+
+  public void testQwListWithVariable() {doTest();}
+
+  public void testShift() {doTest();}
+
 
   private void doTest() {
     initWithFileSmartWithoutErrors();
