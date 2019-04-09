a2226244ee6dd30a03aa688bb3134337ece647db
Alexandr Evstigneev <hurricup@gmail.com>
2019-04-09 19:50:22 +0300

    Removed constant logic for now

--- core/src/com/perl5/lang/perl/idea/codeInsight/typeInference/value/PerlReferenceValue.java
+++ core/src/com/perl5/lang/perl/idea/codeInsight/typeInference/value/PerlReferenceValue.java
@@ -29,16 +29,13 @@ import java.util.Collections;
 import java.util.Set;
 
 public final class PerlReferenceValue extends PerlOperationValue {
-  private final boolean myIsConstantReference;
 
-  private PerlReferenceValue(@NotNull PerlValue referrent, boolean isConstantReference) {
+  private PerlReferenceValue(@NotNull PerlValue referrent) {
     super(referrent);
-    myIsConstantReference = isConstantReference;
   }
 
   PerlReferenceValue(@NotNull StubInputStream dataStream) throws IOException {
     super(dataStream);
-    myIsConstantReference = dataStream.readBoolean();
   }
 
   @Override
@@ -49,7 +46,6 @@ public final class PerlReferenceValue extends PerlOperationValue {
   @Override
   protected void serializeData(@NotNull StubOutputStream dataStream) throws IOException {
     super.serializeData(dataStream);
-    dataStream.writeBoolean(myIsConstantReference);
   }
 
   @NotNull
@@ -69,30 +65,6 @@ public final class PerlReferenceValue extends PerlOperationValue {
   @NotNull
   public PerlValue getTarget() {
     return getBaseValue();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    if (!super.equals(o)) {
-      return false;
-    }
-
-    PerlReferenceValue value = (PerlReferenceValue)o;
-
-    return myIsConstantReference == value.myIsConstantReference;
-  }
-
-  @Override
-  public int computeHashCode() {
-    int result = super.computeHashCode();
-    result = 31 * result + (myIsConstantReference ? 1 : 0);
-    return result;
   }
 
   @Override
