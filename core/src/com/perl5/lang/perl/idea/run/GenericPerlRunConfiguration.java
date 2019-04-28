8e90a8ef6aad71e15f1c1639254072fa69aa9909
Alexandr Evstigneev <hurricup@gmail.com>
2019-04-28 21:40:53 +0300

    CAMELCADE-1570 Disabled async refresh

--- core/src/com/perl5/lang/perl/idea/run/GenericPerlRunConfiguration.java
+++ core/src/com/perl5/lang/perl/idea/run/GenericPerlRunConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 Alexandr Evstigneev
+ * Copyright 2015-2019 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -456,7 +456,7 @@ public abstract class GenericPerlRunConfiguration extends LocatableConfiguration
       if (StringUtil.isEmpty(pathName)) {
         continue;
       }
-      ContainerUtil.addIfNotNull(virtualFiles, VfsUtil.findFileByIoFile(new File(pathName), true));
+      ContainerUtil.addIfNotNull(virtualFiles, VfsUtil.findFileByIoFile(new File(pathName), false));
     }
     return virtualFiles;
   }
