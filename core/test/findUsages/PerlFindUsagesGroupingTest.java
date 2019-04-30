ff7120371ce4d133dc28961ce6f968d53edad8ac
Alexandr Evstigneev <hurricup@gmail.com>
2019-04-30 05:48:48 +0300

    Refactoring: Moved tests and test data

--- core/test/findUsages/PerlFindUsagesGroupingTest.java
+++ core/test/findUsages/PerlFindUsagesGroupingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 Alexandr Evstigneev
+ * Copyright 2015-2019 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-package findUsages.perl;
+package findUsages;
 
 import base.PerlLightTestCase;
 
-public class PerlUsagesGroupingTest extends PerlLightTestCase {
+public class PerlFindUsagesGroupingTest extends PerlLightTestCase {
   @Override
   protected String getTestDataPath() {
-    return "testData/findusages/grouping/perl/";
+    return "testData/findusages/perl/grouping";
   }
 
   public void testBlessExpr() {doTest();}
