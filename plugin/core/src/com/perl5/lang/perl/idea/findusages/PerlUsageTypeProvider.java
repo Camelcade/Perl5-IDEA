d79e220e1d4b58d176ac06e5cb2c967320f4a006
Alexandr Evstigneev <hurricup@gmail.com>
2020-04-08 21:54:07 +0300

    Got rid of deprecated UsageType constructor

--- plugin/core/src/com/perl5/lang/perl/idea/findusages/PerlUsageTypeProvider.java
+++ plugin/core/src/com/perl5/lang/perl/idea/findusages/PerlUsageTypeProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2020 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,13 @@ package com.perl5.lang.perl.idea.findusages;
 import com.intellij.psi.PsiElement;
 import com.intellij.usages.impl.rules.UsageType;
 import com.intellij.usages.impl.rules.UsageTypeProvider;
+import com.perl5.PerlBundle;
 import com.perl5.lang.pod.PodLanguage;
 import org.jetbrains.annotations.Nullable;
 
 
 public class PerlUsageTypeProvider implements UsageTypeProvider {
-  public static final UsageType DOCUMENT_USAGE = new UsageType("Documentation");
+  public static final UsageType DOCUMENT_USAGE = new UsageType(() -> PerlBundle.message("perl.usage.type.documentation"));
 
   @Nullable
   @Override
