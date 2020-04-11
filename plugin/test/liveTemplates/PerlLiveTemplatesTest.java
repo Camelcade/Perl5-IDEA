5319383557a71f608fb1325500c1b61f14da7503
Alexandr Evstigneev <hurricup@gmail.com>
2020-04-11 15:21:37 +0300

    Added `as` livetemplate for async sub definition
    #2144

--- plugin/test/liveTemplates/PerlLiveTemplatesTest.java
+++ plugin/test/liveTemplates/PerlLiveTemplatesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2020 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,9 @@ package liveTemplates;
 
 import org.junit.Test;
 public class PerlLiveTemplatesTest extends PerlLiveTemplatesTestCase {
+
+  @Test
+  public void testAsyncSub() {doTest("as"); }
 
   @Test
   public void testSayStdout() {doTest("sout"); }
