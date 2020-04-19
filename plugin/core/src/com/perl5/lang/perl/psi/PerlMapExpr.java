9df871d34900182d043f3f996a96e2e878b17d28
Alexandr Evstigneev <hurricup@gmail.com>
2020-04-19 20:52:56 +0300

    `sort BLOCK` is now containing block instead of anon sub

--- plugin/core/src/com/perl5/lang/perl/psi/PerlMapExpr.java
+++ plugin/core/src/com/perl5/lang/perl/psi/PerlMapExpr.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2020 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,6 @@ import org.jetbrains.annotations.Nullable;
 
 
 public interface PerlMapExpr extends PsiElement, PerlLabelScope, PerlBlockOwner {
-
-  @Nullable
-  PsiPerlBlock getBlock();
-
   @Nullable
   PsiPerlExpr getExpr();
 
--- plugin/core/src/com/perl5/lang/perl/psi/PerlSortExpr.java
+++ plugin/core/src/com/perl5/lang/perl/psi/PerlSortExpr.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2020 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,9 +24,6 @@ import org.jetbrains.annotations.Nullable;
 
 
 public interface PerlSortExpr extends PsiElement, PerlLabelScope, PerlBlockOwner, PerlReturnScope {
-
-  @Nullable
-  PsiPerlExpr getExpr();
 
   @Nullable
   PsiPerlMethod getMethod();
