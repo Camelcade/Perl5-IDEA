96b35497165760228b56df8dbbbe80bdc0928b1c
Alexandr Evstigneev <hurricup@gmail.com>
2020-06-04 16:28:24 +0300

    Revert "Set write permissions to the files downloaded via cp"
    This reverts commit 281de63d
    #2179

--- docker/src/com/perl5/lang/perl/idea/sdk/host/docker/PerlDockerAdapter.java
+++ docker/src/com/perl5/lang/perl/idea/sdk/host/docker/PerlDockerAdapter.java
@@ -131,10 +131,7 @@ class PerlDockerAdapter {
           UnixSystem system = new UnixSystem();
           long gid = system.getGid();
           long uid = system.getUid();
-          additionalArguments = Arrays.asList(
-            "chown -R " + uid + ":" + gid + " " + remoteCachePath,
-            "chmod u+w " + remoteCachePath + "/*"
-          );
+          additionalArguments = Collections.singletonList("chown -R " + uid + ":" + gid + " " + remoteCachePath);
         }
         File script = createCommandScript(new PerlCommandLine(
           BASH_COPY, String.join("/", remotePath, "*"), remoteCachePath), additionalArguments);
