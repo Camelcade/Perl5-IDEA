09a83190b5ad9f2a07b0cc5dda1d4e0323e088c1
Alexandr Evstigneev <hurricup@gmail.com>
2020-06-21 21:44:55 +0300

    Implemented parsing of chained comparisons
    Can't implement proper chain/non-associative parsing for equality expressions. So we need to handle this with annotator.
    
    #2208

--- plugin/test/parser/PerlParserTest.java
+++ plugin/test/parser/PerlParserTest.java
@@ -42,6 +42,9 @@ public class PerlParserTest extends PerlParserTestBase {
   }
 
   @Test
+  public void testChainedComparison() {doTest();}
+
+  @Test
   public void testIsaExpr() {doTest();}
 
   @Test
