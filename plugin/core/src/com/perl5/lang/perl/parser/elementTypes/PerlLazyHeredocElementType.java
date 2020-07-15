16a7d6914597cfea33c89ec2868a72057f87896d
Alexandr Evstigneev <hurricup@gmail.com>
2020-07-15 22:06:06 +0300

    Implemented not optimal heredoc re-parsing algorithm
    As far as we do have only parent node, we check all heredocs of the same types and reparsing only if they are all reparsable.
    
    Assumption: in real cases there are no so many heredocs with different markers and we are checking each marker only once
    
    #2243

--- plugin/core/src/com/perl5/lang/perl/parser/elementTypes/PerlLazyHeredocElementType.java
+++ plugin/core/src/com/perl5/lang/perl/parser/elementTypes/PerlLazyHeredocElementType.java
@@ -17,10 +17,25 @@
 package com.perl5.lang.perl.parser.elementTypes;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.lang.Language;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiUtilCore;
+import com.intellij.util.text.CharArrayUtil;
 import com.perl5.lang.perl.lexer.PerlLexingContext;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import static com.perl5.lang.perl.lexer.PerlElementTypesGenerated.HEREDOC_END;
+import static com.perl5.lang.perl.lexer.PerlElementTypesGenerated.HEREDOC_END_INDENTABLE;
 
 
 public class PerlLazyHeredocElementType extends PerlLazyBlockElementType {
@@ -29,6 +44,36 @@ public class PerlLazyHeredocElementType extends PerlLazyBlockElementType {
   public PerlLazyHeredocElementType(@NotNull String name, int startLexerState) {
     super(name);
     myStartLexerState = startLexerState;
+  }
+
+  /**
+   * @implSpec this is low-performance heuristic reparsing detection. See #2243
+   */
+  @ApiStatus.ScheduledForRemoval(inVersion = "2020.3")
+  @Deprecated
+  @Override
+  public boolean isParsable(@Nullable ASTNode parent,
+                            @NotNull CharSequence buffer,
+                            @NotNull Language fileLanguage,
+                            @NotNull Project project) {
+    if (parent == null) {
+      return false;
+    }
+    ASTNode run = parent.getFirstChildNode();
+    if (run == null) {
+      return false;
+    }
+
+    Checker checker = new Checker(buffer);
+
+    while (run != null) {
+      if (!checker.check(run)) {
+        return false;
+      }
+      run = run.getTreeNext();
+    }
+
+    return checker.result();
   }
 
   public PerlLazyHeredocElementType(@NotNull String debugName,
@@ -41,5 +86,72 @@ public class PerlLazyHeredocElementType extends PerlLazyBlockElementType {
   @Override
   protected @NotNull PerlLexingContext getLexingContext(@NotNull Project project, @NotNull ASTNode chameleon) {
     return super.getLexingContext(project, chameleon).withEnforcedInitialState(myStartLexerState);
+  }
+
+  private class Checker {
+    private Set<String> myNormalEndsChecked = null;
+    private Set<String> myIndentedEndsChecked = null;
+    private final @NotNull CharSequence myNodeText;
+
+    public Checker(@NotNull CharSequence nodeText) {
+      int index = CharArrayUtil.shiftForward(nodeText, 0, " \t");
+      myNodeText = index == 0 ? nodeText : nodeText.subSequence(index, nodeText.length());
+    }
+
+    /**
+     * @return true iff it's ok to continue iteration, false otherwise.
+     */
+    boolean check(@NotNull ASTNode node) {
+      IElementType elementType = node.getElementType();
+      if (elementType != PerlLazyHeredocElementType.this) {
+        return true;
+      }
+
+      ASTNode closeMarker = node.getTreeNext();
+      if (PsiUtilCore.getElementType(closeMarker) == TokenType.WHITE_SPACE) {
+        closeMarker = closeMarker.getTreeNext();
+      }
+      if (closeMarker == null) {
+        return true;
+      }
+
+      String closeMarkerText = closeMarker.getText() + "\n";
+      IElementType closeMarkerType = closeMarker.getElementType();
+      if (closeMarkerType == HEREDOC_END) {
+        if (myNormalEndsChecked == null) {
+          myNormalEndsChecked = new HashSet<>();
+        }
+        if (!myNormalEndsChecked.add(closeMarkerText)) {
+          return true;
+        }
+        return !(StringUtil.startsWith(myNodeText, closeMarkerText) || StringUtil.contains(myNodeText, "\n" + closeMarkerText));
+      }
+      else if (closeMarkerType == HEREDOC_END_INDENTABLE) {
+        if (myNormalEndsChecked == null) {
+          myNormalEndsChecked = new HashSet<>();
+        }
+        if (myIndentedEndsChecked == null) {
+          myIndentedEndsChecked = new HashSet<>();
+        }
+        myNormalEndsChecked.add(closeMarkerText);
+        if (!myIndentedEndsChecked.add(closeMarkerText)) {
+          return true;
+        }
+        if (StringUtil.startsWith(myNodeText, closeMarkerText)) {
+          return false;
+        }
+        if (myNodeText.length() < closeMarkerText.length()) {
+          return true;
+        }
+        return !Pattern.compile("\n\\s*" + closeMarkerText).matcher(myNodeText).find();
+      }
+      else {
+        return false;
+      }
+    }
+
+    boolean result() {
+      return myNormalEndsChecked != null || myIndentedEndsChecked != null;
+    }
   }
 }
