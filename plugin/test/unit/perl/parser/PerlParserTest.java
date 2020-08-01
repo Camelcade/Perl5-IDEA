6062f9b0cf8dd314f22dc827dbce7273c41f6e4d
Alexandr Evstigneev <hurricup@gmail.com>
2020-08-01 21:29:19 +0300

    Proper fallback for Function::Parameters keywords

--- plugin/test/unit/perl/parser/PerlParserTest.java
+++ plugin/test/unit/perl/parser/PerlParserTest.java
@@ -27,6 +27,9 @@ public class PerlParserTest extends PerlParserTestBase {
   }
 
   @Test
+  public void testFpFallback() {doTest();}
+
+  @Test
   public void testStatementModifiers() {doTest();}
 
   @Test
