Camelcade/Perl5-IDEA

commit ea1025489200b23a55b1502af45d9f4b2b5f8150

Proper work with PerlModuleExtension modifiable model

plugin/core/src/com/perl5/lang/perl/idea/configuration/settings/sdk/PerlContentEntriesTreeEditor.java
@@ -26,6 +26,8 @@ import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ex.CustomComponentAction;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.fileChooser.FileElement;
@@ -35,6 +37,7 @@ import com.intellij.openapi.fileChooser.ex.FileSystemTreeImpl;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.options.UnnamedConfigurable;
 import com.intellij.openapi.projectRoots.impl.PerlModuleExtension;
+import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ui.configuration.actions.IconWithTextAction;
 import com.intellij.openapi.ui.VerticalFlowLayout;
@@ -57,11 +60,13 @@ import java.util.List;
 
 
 public class PerlContentEntriesTreeEditor implements UnnamedConfigurable, Disposable {
+  private static final Logger LOG = Logger.getInstance(PerlContentEntriesTreeEditor.class);
   private JPanel myTreePanel;
   private Tree myTree = new Tree();
   private FileSystemTreeImpl myFileSystemTree;
   private Module myModule;
   private DefaultActionGroup myEditingActionsGroup = new DefaultActionGroup();
+  private ModifiableRootModel myModifiableRootModel;
   private PerlModuleExtension myModifiableModel;
 
   public PerlContentEntriesTreeEditor(@NotNull Module module, @NotNull Disposable parentDisposable) {
@@ -147,17 +152,20 @@ public class PerlContentEntriesTreeEditor implements UnnamedConfigurable, Dispos
 
   @Override
   public void apply() {
-    assert myModifiableModel != null;
-    myModifiableModel.commit();
+    LOG.assertTrue(myModifiableRootModel != null);
+    LOG.assertTrue(myModifiableModel != null);
+    WriteAction.run(() -> myModifiableRootModel.commit());
   }
 
   @Override
   public void reset() {
-    if (myModifiableModel != null) {
-      myModifiableModel.dispose();
+    if (myModifiableRootModel != null) {
+      myModifiableRootModel.dispose();
+      myModifiableRootModel = null;
       myModifiableModel = null;
     }
-    myModifiableModel = (PerlModuleExtension)PerlModuleExtension.getInstance(myModule).getModifiableModel(true);
+    myModifiableRootModel = ModuleRootManager.getInstance(myModule).getModifiableModel();
+    myModifiableModel = myModifiableRootModel.getModuleExtension(PerlModuleExtension.class);
   }
 
   @Override
@@ -172,6 +180,9 @@ public class PerlContentEntriesTreeEditor implements UnnamedConfigurable, Dispos
   @Override
   public void dispose() {
     disposeUIResources();
+    if (myModifiableRootModel != null) {
+      myModifiableRootModel.dispose();
+    }
   }
 
   @NotNull

plugin/core/src/com/intellij/openapi/projectRoots/impl/PerlModuleExtension.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.components.PersistentStateComponentWithModificationT
 import com.intellij.openapi.components.impl.ModulePathMacroManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleExtension;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
@@ -32,6 +33,7 @@ import com.intellij.util.containers.Predicate;
 import com.perl5.lang.perl.idea.modules.PerlSourceRootType;
 import gnu.trove.THashMap;
 import org.jdom.Element;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.model.module.JpsModuleSourceRootType;
@@ -42,6 +44,7 @@ import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class PerlModuleExtension extends ModuleExtension implements PersistentStateComponentWithModificationTracker<Element> {
@@ -69,11 +72,13 @@ public class PerlModuleExtension extends ModuleExtension implements PersistentSt
     myOriginal = original;
   }
 
+  @ApiStatus.OverrideOnly
   @Override
   public @NotNull ModuleExtension getModifiableModel(boolean writable) {
     return new PerlModuleExtension(this, writable);
   }
 
+  @ApiStatus.OverrideOnly
   @Override
   public void commit() {
     LOG.assertTrue(myOriginal != null, "Attempt to commit non-modifiable model");
@@ -193,6 +198,27 @@ public class PerlModuleExtension extends ModuleExtension implements PersistentSt
 
   public static PerlModuleExtension getInstance(@NotNull Module module) {
     return ModuleRootManager.getInstance(module).getModuleExtension(PerlModuleExtension.class);
+  }
+
+  /**
+   * Modifies {@link PerlModuleExtension} for the {@code module} using {@code mutator}. Incapsulates proper logic of working with modifiable
+   * model, described in {@link ModuleExtension#getModifiableModel(boolean) javadoc}.
+   *
+   * @see ModuleExtension#getModifiableModel(boolean)
+   */
+  public static void modify(@NotNull Module module, @NotNull Consumer<PerlModuleExtension> mutator) {
+    ModifiableRootModel modifiableModel = ModuleRootManager.getInstance(module).getModifiableModel();
+    PerlModuleExtension moduleExtensionModifiableModel = modifiableModel.getModuleExtension(PerlModuleExtension.class);
+    try {
+      mutator.accept(moduleExtensionModifiableModel);
+    }
+    catch (Throwable e) {
+      LOG.error(e);
+      modifiableModel.dispose();
+    }
+    finally {
+      WriteAction.run(modifiableModel::commit);
+    }
   }
 
   private static @Nullable JpsModuleSourceRootPropertiesSerializer<?> getSerializer(Predicate<JpsModuleSourceRootPropertiesSerializer<?>> predicate) {

plugin/core/src/com/perl5/lang/perl/idea/actions/PerlMarkSourceRootAction.java
@@ -75,10 +75,10 @@ public abstract class PerlMarkSourceRootAction extends PerlSourceRootAction {
   }
 
   public final void markRoot(@NotNull Module module, @NotNull VirtualFile... files) {
-    PerlModuleExtension modifiableModel = (PerlModuleExtension)PerlModuleExtension.getInstance(module).getModifiableModel(true);
-    for (VirtualFile virtualFile : files) {
-      modifiableModel.setRoot(virtualFile, myType);
-    }
-    modifiableModel.commit();
+    PerlModuleExtension.modify(module, it -> {
+      for (VirtualFile virtualFile : files) {
+        it.setRoot(virtualFile, myType);
+      }
+    });
   }
 }

plugin/core/src/com/perl5/lang/perl/idea/actions/PerlUnmarkSourceRootAction.java
@@ -75,12 +75,11 @@ public class PerlUnmarkSourceRootAction extends PerlSourceRootAction {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     RootsSelection selection = getSelection(e);
-    PerlModuleExtension perlModuleExtension =
-      (PerlModuleExtension)PerlModuleExtension.getInstance(selection.myModule).getModifiableModel(true);
-    for (VirtualFile virtualFile : getFilesFromSelection(selection)) {
-      perlModuleExtension.removeRoot(virtualFile);
-    }
-    perlModuleExtension.commit();
+    PerlModuleExtension.modify(selection.myModule, it -> {
+      for (VirtualFile virtualFile : getFilesFromSelection(selection)) {
+        it.removeRoot(virtualFile);
+      }
+    });
   }
 
   @Override

plugin/test/base/PerlLightTestCaseBase.java
@@ -346,10 +346,7 @@ public abstract class PerlLightTestCaseBase extends BasePlatformTestCase {
    */
   protected void removePerlSourceRoot(@NotNull VirtualFile libDir) {
     assertTrue(libDir.isValid() && libDir.isDirectory());
-    PerlModuleExtension
-      modifiableModel1 = (PerlModuleExtension)PerlModuleExtension.getInstance(myFixture.getModule()).getModifiableModel(true);
-    modifiableModel1.removeRoot(libDir);
-    modifiableModel1.commit();
+    PerlModuleExtension.modify(getModule(), it -> it.removeRoot(libDir));
   }
 
   /**
@@ -359,10 +356,7 @@ public abstract class PerlLightTestCaseBase extends BasePlatformTestCase {
    */
   protected void markAsLibRoot(@NotNull VirtualFile libDir, boolean removeAutomatically) {
     assertTrue(libDir.isValid() && libDir.isDirectory());
-    PerlModuleExtension
-      modifiableModel = (PerlModuleExtension)PerlModuleExtension.getInstance(myFixture.getModule()).getModifiableModel(true);
-    modifiableModel.setRoot(libDir, PerlLibrarySourceRootType.INSTANCE);
-    modifiableModel.commit();
+    PerlModuleExtension.modify(getModule(), it -> it.setRoot(libDir, PerlLibrarySourceRootType.INSTANCE));
     if (removeAutomatically) {
       addPerlTearDownListener(() -> removePerlSourceRoot(libDir));
     }

commit b3a8995570e91459308a2ff138664a10d50ab5a0

Re-create modifiable models after applying changes from ContentEntriesTreeEditor
This should handle the case user press apply, then continue editing and press apply/ok again

caused by ea1025489200b23a55b1502af45d9f4b2b5f8150
Fixes: #CAMELCADE-2577

plugin/core/src/com/perl5/lang/perl/idea/configuration/settings/sdk/PerlContentEntriesTreeEditor.java
@@ -155,14 +155,14 @@ public class PerlContentEntriesTreeEditor implements UnnamedConfigurable, Dispos
     LOG.assertTrue(myModifiableRootModel != null);
     LOG.assertTrue(myModifiableModel != null);
     WriteAction.run(() -> myModifiableRootModel.commit());
+    myModifiableRootModel = null;
+    reset();
   }
 
   @Override
   public void reset() {
     if (myModifiableRootModel != null) {
       myModifiableRootModel.dispose();
-      myModifiableRootModel = null;
-      myModifiableModel = null;
     }
     myModifiableRootModel = ModuleRootManager.getInstance(myModule).getModifiableModel();
     myModifiableModel = myModifiableRootModel.getModuleExtension(PerlModuleExtension.class);