8db73fecb869568779aa653aeeafec3629cc9717
Alexandr Evstigneev <hurricup@gmail.com>
2021-04-05 16:58:43 +0300

    Deprecated code: ShowContainersAction supplier API

--- plugin/core/src/com/perl5/lang/perl/extensions/generation/PerlCodeGeneratorImpl.java
+++ plugin/core/src/com/perl5/lang/perl/extensions/generation/PerlCodeGeneratorImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2021 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -193,7 +193,7 @@ public class PerlCodeGeneratorImpl implements PerlCodeGenerator {
 
           @Override
           protected ShowContainersAction getShowContainersAction() {
-            return new ShowContainersAction(IdeBundle.message("action.show.classes"), PerlIcons.PACKAGE_GUTTER_ICON);
+            return new ShowContainersAction(() -> IdeBundle.message("action.show.classes"), PerlIcons.PACKAGE_GUTTER_ICON);
           }
         };
 
