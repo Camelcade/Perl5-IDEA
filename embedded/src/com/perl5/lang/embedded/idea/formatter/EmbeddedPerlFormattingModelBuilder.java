6011bb264099d627c8911ce30e6b4ce02d27f864
Alexandr Evstigneev <hurricup@gmail.com>
2021-04-16 17:39:03 +0300

    Embedded perl: proper xml-based formatter
    Fixes #762

--- embedded/src/com/perl5/lang/embedded/idea/formatter/EmbeddedPerlFormattingModelBuilder.java
+++ embedded/src/com/perl5/lang/embedded/idea/formatter/EmbeddedPerlFormattingModelBuilder.java
@@ -17,19 +17,41 @@
 package com.perl5.lang.embedded.idea.formatter;
 
 import com.intellij.formatting.FormattingContext;
-import com.intellij.formatting.FormattingModel;
-import com.intellij.formatting.FormattingModelProvider;
-import com.perl5.lang.perl.idea.formatter.PerlTemplatingFormattingModelBuilder;
-import com.perl5.lang.perl.idea.formatter.blocks.PerlFormattingBlock;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiUtilCore;
+import com.perl5.lang.embedded.EmbeddedPerlLanguage;
+import com.perl5.lang.embedded.psi.EmbeddedPerlElementTypes;
+import com.perl5.lang.perl.idea.formatter.PerlXmlTemplateFormattingModelBuilder;
 import org.jetbrains.annotations.NotNull;
 
 
-public class EmbeddedPerlFormattingModelBuilder extends PerlTemplatingFormattingModelBuilder {
+public class EmbeddedPerlFormattingModelBuilder
+  extends PerlXmlTemplateFormattingModelBuilder<EmbeddedPerlFormattingContext, EmbeddedPerlFormattingBlock> {
+
   @Override
-  public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
-    var element = formattingContext.getPsiElement();
-    PerlFormattingBlock block = new EmbeddedPerlFormattingBlock(element.getNode(), new EmbeddedPerlFormattingContext(formattingContext));
-    return FormattingModelProvider.createFormattingModelForPsiFile(element.getContainingFile(), block,
-                                                                   formattingContext.getCodeStyleSettings());
+  protected @NotNull EmbeddedPerlFormattingBlock createBlock(ASTNode node, FormattingContext formattingContext) {
+    return new EmbeddedPerlFormattingBlock(node, createContext(formattingContext));
+  }
+
+  @Override
+  protected @NotNull EmbeddedPerlFormattingContext createContext(@NotNull FormattingContext formattingContext) {
+    return new EmbeddedPerlFormattingContext(formattingContext);
+  }
+
+  @Override
+  protected boolean isTemplateFile(PsiFile file) {
+    return file.getLanguage().isKindOf(EmbeddedPerlLanguage.INSTANCE);
+  }
+
+  @Override
+  public boolean isOuterLanguageElement(PsiElement element) {
+    return PsiUtilCore.getElementType(element) == EmbeddedPerlElementTypes.EMBED_OUTER_ELEMENT_TYPE;
+  }
+
+  @Override
+  public boolean isMarkupLanguageElement(PsiElement element) {
+    return PsiUtilCore.getElementType(element) == EmbeddedPerlElementTypes.EMBED_TEMPLATE_BLOCK_HTML;
   }
 }
---
+++ plugin/core/src/com/perl5/lang/perl/idea/formatter/PerlXmlTemplateFormattingModelBuilder.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015-2021 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.perl5.lang.perl.idea.formatter;
+
+import com.intellij.formatting.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.formatter.xml.XmlFormattingPolicy;
+import com.intellij.psi.templateLanguages.SimpleTemplateLanguageFormattingModelBuilder;
+import com.intellij.xml.template.formatter.AbstractXmlTemplateFormattingModelBuilder;
+import com.perl5.lang.perl.idea.formatter.blocks.PerlFormattingBlock;
+import com.perl5.lang.perl.psi.PerlMultiplePsiFilesPerDocumentFileViewProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class PerlXmlTemplateFormattingModelBuilder<Ctx extends PerlFormattingContext, Blk extends PerlFormattingBlock>
+  extends AbstractXmlTemplateFormattingModelBuilder {
+  private static final Logger LOG = Logger.getInstance(PerlXmlTemplateFormattingModelBuilder.class);
+
+  @Override
+  public @NotNull FormattingModel createModel(@NotNull FormattingContext formattingContext) {
+    LOG.debug("Creating model for ", formattingContext);
+    return useXmlFormattingModelBuilder(formattingContext.getPsiElement()) ?
+           super.createModel(formattingContext) :
+           new SimpleTemplateLanguageFormattingModelBuilder().createModel(formattingContext);
+  }
+
+
+  @Override
+  protected Block createTemplateLanguageBlock(ASTNode node,
+                                              CodeStyleSettings settings,
+                                              XmlFormattingPolicy xmlFormattingPolicy,
+                                              Indent indent,
+                                              @Nullable Alignment alignment,
+                                              @Nullable Wrap wrap) {
+    // fixme we should not re-create a context each time. Probably we should override the parent class and copy-paste it.
+    return createBlock(node, FormattingContext.create(node.getPsi(), settings));
+  }
+
+  protected abstract @NotNull Blk createBlock(ASTNode node, FormattingContext formattingContext);
+
+  protected abstract @NotNull Ctx createContext(@NotNull FormattingContext formattingContext);
+
+  protected boolean useXmlFormattingModelBuilder(@NotNull PsiElement element) {
+    FileViewProvider viewProvider = element.getContainingFile().getViewProvider();
+    LOG.assertTrue(viewProvider instanceof PerlMultiplePsiFilesPerDocumentFileViewProvider, "Got " + viewProvider);
+    return ((PerlMultiplePsiFilesPerDocumentFileViewProvider)viewProvider).getTemplateDataLanguage().isKindOf(XMLLanguage.INSTANCE);
+  }
+}
