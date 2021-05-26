0a8c5d7d52ac823a3cbabae9e221b7deaef2ef69
Alexandr Evstigneev <hurricup@gmail.com>
2021-05-26 10:11:33 +0300

    Block and line openers are now comments

--- tt2/src/com/perl5/lang/tt2/TemplateToolkitParserDefinition.java
+++ tt2/src/com/perl5/lang/tt2/TemplateToolkitParserDefinition.java
@@ -42,7 +42,10 @@ public class TemplateToolkitParserDefinition implements ParserDefinition, Templa
   public static final TokenSet COMMENTS = TokenSet.create(
     TT2_HTML,
     LINE_COMMENT,
-    BLOCK_COMMENT
+    BLOCK_COMMENT,
+    TT2_OPEN_TAG,
+    TT2_CLOSE_TAG,
+    TT2_OUTLINE_TAG
   );
 
   public static final TokenSet WHITESPACES_AND_COMMENTS = TokenSet.orSet(
