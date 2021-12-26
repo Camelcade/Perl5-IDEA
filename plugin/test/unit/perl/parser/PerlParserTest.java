c5b82c357550c477109b794979c99e6d7c3f5136
Alexandr Evstigneev <hurricup@gmail.com>
2021-12-26 11:56:02 +0300

    Added tests covering escape sequences in different constructs with interpolation
    Fixes #2233
    
    See: perl5/issues/#17942

--- plugin/test/unit/perl/parser/PerlParserTest.java
+++ plugin/test/unit/perl/parser/PerlParserTest.java
@@ -27,6 +27,9 @@ public class PerlParserTest extends PerlParserTestBase {
   }
 
   @Test
+  public void testIssue2233() { doTest(); }
+
+  @Test
   public void testIssue2233re() { doTest(); }
 
   @Test
