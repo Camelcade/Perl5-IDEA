fa95605af16fda939b29b03a8db90bc7c4efcf83
Alexandr Evstigneev <hurricup@gmail.com>
2022-01-04 14:14:10 +0300

    SonarCloud: Long cast to the one of arguments

--- plugin/docker/src/com/perl5/lang/perl/idea/sdk/host/docker/PerlDockerFileSystem.java
+++ plugin/docker/src/com/perl5/lang/perl/idea/sdk/host/docker/PerlDockerFileSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 Alexandr Evstigneev
+ * Copyright 2015-2022 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -130,7 +130,7 @@ class PerlDockerFileSystem extends PerlPluggableVirtualFileSystem {
 
     @Override
     public long getLength() {
-      return myDescriptor.getSize() * 1024;
+      return myDescriptor.getSize() * 1024L;
     }
 
     @Override
