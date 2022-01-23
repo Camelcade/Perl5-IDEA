a635b9c7daef3f125d1cd3df77752d5ca5ecdc5a
Alexandr Evstigneev <hurricup@gmail.com>
2022-01-23 17:40:35 +0300

    Introduced tests for generating perl setters and getters
    #2280

--- plugin/test/actions/PerlActionTestCase.java
+++ plugin/test/actions/PerlActionTestCase.java
@@ -2,10 +2,16 @@ package actions;
 
 import base.PerlLightTestCase;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.ui.TestDialogManager;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class PerlActionTestCase extends PerlLightTestCase {
   protected abstract @NotNull AnAction getAction();
+
+  protected void doTest(@NotNull String testDialogInput) {
+    TestDialogManager.setTestInputDialog(message -> testDialogInput);
+    doTest();
+  }
 
   protected void doTest() {
     initWithFileSmart();
