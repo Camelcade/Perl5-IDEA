770a89d59a8e6cc18e9dce0a7d9c72eaceeb7f37
Alexandr Evstigneev <hurricup@gmail.com>
2022-01-23 23:16:15 +0300

    Implemented deparse xsubs action test
    #2280
    See #2453

--- plugin/test/run/PerlHeavyActionsTest.java
+++ plugin/test/run/PerlHeavyActionsTest.java
@@ -1,14 +1,21 @@
 package run;
 
 import base.PerlPlatformTestCase;
+import categories.Heavy;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.testFramework.LightVirtualFile;
 import com.intellij.testFramework.UsefulTestCase;
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
 import com.intellij.util.containers.ContainerUtil;
 import com.perl5.lang.perl.idea.actions.PerlDeparseFileAction;
+import com.perl5.lang.perl.idea.actions.PerlRegenerateXSubsAction;
+import com.perl5.lang.perl.util.PerlSubUtil;
+import com.perl5.lang.perl.xsubs.PerlXSubsState;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+@Category(Heavy.class)
 public class PerlHeavyActionsTest extends PerlPlatformTestCase {
   @Override
   protected String getBaseDataPath() {
@@ -29,5 +36,19 @@ public class PerlHeavyActionsTest extends PerlPlatformTestCase {
     var deparsedDocument = FileDocumentManager.getInstance().getDocument(deparsedFile);
     assertNotNull(deparsedDocument);
     UsefulTestCase.assertSameLinesWithFile(getTestResultsFilePath(""), deparsedDocument.getText());
+  }
+
+  @Test
+  public void testDeparseXSubsFile() {
+    runActionWithTestEvent(new PerlRegenerateXSubsAction());
+
+    CodeInsightTestFixtureImpl.ensureIndexesUpToDate(getProject());
+    assertEmpty(PerlSubUtil.getSubDeclarations(getProject(), "Hash::StoredIterator::hash_get_iterator"));
+
+    var moduleRoot = getModuleRoot();
+    moduleRoot.refresh(false, false);
+    var deparsedFile = moduleRoot.findFileByRelativePath(PerlXSubsState.DEPARSED_FILE_NAME);
+    assertNotNull(deparsedFile);
+    CodeInsightTestFixtureImpl.ensureIndexesUpToDate(getProject());
   }
 }
