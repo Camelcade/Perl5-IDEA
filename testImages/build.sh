137c0c5332df810758a45c8a47aaf6df42038129
Alexandr Evstigneev <hurricup@gmail.com>
2022-10-30 11:48:22 +0400

    Migrated to perl 5.36.0

--- testImages/build.sh
+++ testImages/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-PERL_VERSION="5.32.0"
+PERL_VERSION="5.36.0"
 docker build --tag hurricup/camelcade-test:perl5-$PERL_VERSION --build-arg PERL_VERSION=$PERL_VERSION --file perl5.Dockerfile .
 docker push hurricup/camelcade-test:perl5-$PERL_VERSION
