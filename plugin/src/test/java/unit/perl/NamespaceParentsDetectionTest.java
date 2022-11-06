7294b7d0291a8b4b39616a8a319b172fe1c1d9bf
Alexandr Evstigneev <hurricup@gmail.com>
2022-11-06 09:36:55 +0400

    Got rid of deprecated test method

--- plugin/src/test/java/unit/perl/NamespaceParentsDetectionTest.java
+++ plugin/src/test/java/unit/perl/NamespaceParentsDetectionTest.java
@@ -20,7 +20,6 @@ package unit.perl;
 import com.intellij.psi.PsiFile;
 import com.perl5.lang.perl.psi.PerlFile;
 import com.perl5.lang.perl.psi.mixins.PerlNamespaceDefinitionMixin;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -117,108 +116,107 @@ public class NamespaceParentsDetectionTest extends NamespaceTestCase {
 
   @Test
   public void testIsaAssign() {
-    doTest("isa_assign.pl", "Foo", new String[]{"superfoo1", "superbar1"});
+    doTest("superfoo1", "superbar1");
   }
 
   @Test
   public void testIsaAssignFullQualified() {
-    doTest("isa_assign_fullqualified.pl", "Foo", new String[]{"superfoo1", "superbar1"});
+    doTest("superfoo1", "superbar1");
   }
 
   @Test
   public void testIsaAssignFullQualifiedOtherPackage() {
-    doTest("isa_assing_fullqualified_other_package.pl", "Foo", new String[]{});
+    doTest();
   }
 
   @Test
   public void testIsaDeclare() {
-    doTest("isa_declare.pl", "Foo", new String[]{"superfoo2", "superbar2"});
+    doTest("superfoo2", "superbar2");
   }
 
   @Test
   public void testIsaDeclareParens() {
-    doTest("isa_declare_parens.pl", "Foo", new String[]{"superfoo3", "superbar3"});
+    doTest("superfoo3", "superbar3");
   }
 
   @Test
   public void testBase() {
-    doTest("base.pl", "Foo", new String[]{"superbase", "superbase2"});
+    doTest("superbase", "superbase2");
   }
 
   @Test
   public void testParent() {
-    doTest("parent.pl", "Foo", new String[]{"superparent", "superparent2"});
+    doTest("superparent", "superparent2");
   }
 
   @Test
   public void testParentQQ() {
-    doTest("parent_qq.pl", "Foo", new String[]{"superparent::duperparent"});
+    doTest("superparent::duperparent");
   }
 
   @Test
   public void testMojoBase() {
-    doTest("mojo_base.pl", "Foo", new String[]{"Mojo::Base"});
+    doTest("Mojo::Base");
   }
 
   @Test
   public void testMojoBaseNoBase() {
-    doTest("mojo_base_no_base.pl", "Foo", new String[]{});
+    doTest();
   }
 
   @Test
   public void testMojoBaseSpecific() {
-    doTest("mojo_base_specific.pl", "Foo", new String[]{"somebase1"});
+    doTest("somebase1");
   }
 
   @Test
   public void testMoo() {
-    doTest("moo.pl", "Foo", new String[]{"Moo::Object"});
+    doTest("Moo::Object");
   }
 
   @Test
   public void testMoose() {
-    doTest("moose.pl", "Foo", new String[]{"Moose::Object"});
+    doTest("Moose::Object");
   }
 
   @Test
   public void testRuntimeExtends() {
-    doTest("runtime_extends.pl", "Foo", new String[]{"someparent"});
+    doTest("someparent");
   }
 
   @Test
   public void testRuntimeWith() {
-    doTest("runtime_with.pl", "Foo", new String[]{"someparent1", "someparent2"});
+    doTest("someparent1", "someparent2");
   }
 
   @Test
   public void testRuntimeExtendsWith() {
-    doTest("runtime_extends_with.pl", "Foo",
-           new String[]{"someparent", "someotherparent2", "someotherparent3", "someotherparent4", "someotherparent5"});
+    doTest("someparent", "someotherparent2", "someotherparent3", "someotherparent4", "someotherparent5");
   }
 
   @Test
   public void testRuntimeExtendsVsAll() {
-    doTest("runtime_extends_vs_all.pl", "Foo", new String[]{"someparent"});
+    doTest("someparent");
   }
 
   @Test
   public void testRuntimeISAVsAll() {
-    doTest("runtime_isa_vs_all.pl", "Foo", new String[]{"someisa"});
+    doTest("someisa");
   }
 
   @Test
   public void testCompileVsRuntime() {
-    doTest("compile_vs_runtime.pl", "Foo", new String[]{"someisa", "someotherparent4", "someotherparent5"});
+    doTest("someisa", "someotherparent4", "someotherparent5");
   }
 
   @Test
   public void testParentWith() {
-    doTest("parent_with.pl", "Foo", new String[]{"someparents", "someotherparent4", "someotherparent5"});
+    doTest("someparents", "someotherparent4", "someotherparent5");
   }
 
   @Test
   public void testTypingISA() {
-    doTest("typing_isa.pl", "Foo", new String[]{});
+    doTestDumb();
   }
 
   private void doFileTest(String... parentsList) {
@@ -228,17 +226,17 @@ public class NamespaceParentsDetectionTest extends NamespaceTestCase {
     assertEquals(Arrays.asList(parentsList), ((PerlFile)file).getParentNamespacesNames());
   }
 
-  /**
-   * @deprecated consider using {@link #doTest(String...)}
-   */
-  @Deprecated
-  private void doTest(String fileName, @NotNull String namespaceName, String[] parents) {
-    PerlNamespaceDefinitionMixin namespaceDefinition = getNamespaceInFile(fileName, namespaceName);
-    assertEquals(Arrays.asList(parents), namespaceDefinition.getParentNamespacesNames());
+  private void doTestDumb(String... parents) {
+    initWithFileSmart();
+    doTestWithoutInit(parents);
   }
 
   private void doTest(String... parents) {
     initWithFileSmartWithoutErrors();
+    doTestWithoutInit(parents);
+  }
+
+  private void doTestWithoutInit(String[] parents) {
     var namespaceDefinition = getElementAtCaret(PerlNamespaceDefinitionMixin.class);
     assertNotNull(namespaceDefinition);
     assertEquals(Arrays.asList(parents), namespaceDefinition.getParentNamespacesNames());
--- plugin/src/test/resources/unit/perl/parents/typing_isa.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-
-package Foo;
-
-our @ISA =
