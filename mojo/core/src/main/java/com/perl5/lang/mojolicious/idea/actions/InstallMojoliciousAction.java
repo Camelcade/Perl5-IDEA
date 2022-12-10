ebb56b627936c2fab25ad343f1850f26856612cc
Alexandr Evstigneev <hurricup@gmail.com>
2022-12-10 09:42:24 +0400

    Install Mojolicious action is now hidden when disabled

--- mojo/core/src/main/java/com/perl5/lang/mojolicious/idea/actions/InstallMojoliciousAction.java
+++ mojo/core/src/main/java/com/perl5/lang/mojolicious/idea/actions/InstallMojoliciousAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2022 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,5 +43,10 @@ public class InstallMojoliciousAction extends PerlActionBase {
       return;
     }
     PackageManagerAdapter.create(sdk, e.getProject()).install(MojoUtil.MOJO_PACKAGE_NAME);
+  }
+
+  @Override
+  protected boolean alwaysHideDisabled() {
+    return true;
   }
 }
