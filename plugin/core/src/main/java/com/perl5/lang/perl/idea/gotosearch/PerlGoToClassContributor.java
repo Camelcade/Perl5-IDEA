245df947327a585fbd580f9077183fcee5e237d8
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-22 18:02:56 +0400

    [Qodana] Annotated PerlGoToClassContributor

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/gotosearch/PerlGoToClassContributor.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/gotosearch/PerlGoToClassContributor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,8 +33,8 @@ import java.util.Collections;
  */
 public class PerlGoToClassContributor extends PerlChooseByNameContributor implements GotoClassContributor {
   @Override
-  public @Nullable String getQualifiedName(NavigationItem navigationItem) {
-    return navigationItem instanceof PerlIdentifierOwner ? navigationItem.getName(): null;
+  public @Nullable String getQualifiedName(@NotNull NavigationItem navigationItem) {
+    return navigationItem instanceof PerlIdentifierOwner ? navigationItem.getName() : null;
   }
 
   @Override
