df8773045fb33f5efc176624a0f307230f374ae7
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-22 18:25:48 +0400

    [Qodana] Annotated PerlDelegatingLightNamedElement

--- plugin/core/src/main/java/com/perl5/lang/perl/psi/light/PerlDelegatingLightNamedElement.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/psi/light/PerlDelegatingLightNamedElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -193,7 +193,7 @@ public class PerlDelegatingLightNamedElement<Delegate extends PerlPolyNamedEleme
   }
 
   @Override
-  public @NotNull PsiElement[] getChildren() {
+  public PsiElement @NotNull [] getChildren() {
     return super.getChildren();
   }
 
