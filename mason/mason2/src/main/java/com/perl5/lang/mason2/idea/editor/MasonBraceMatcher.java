dc44d1c07649a6025c36fcaa571d8ea6abece984
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-22 13:03:59 +0400

    [Qodana] Fixed getPairs annotations

--- mason/mason2/src/main/java/com/perl5/lang/mason2/idea/editor/MasonBraceMatcher.java
+++ mason/mason2/src/main/java/com/perl5/lang/mason2/idea/editor/MasonBraceMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public class MasonBraceMatcher implements PairedBraceMatcher, Mason2ElementTypes
   };
 
   @Override
-  public @NotNull BracePair[] getPairs() {
+  public BracePair @NotNull [] getPairs() {
     return PAIRS;
   }
 
--- plugin/core/src/main/java/com/perl5/lang/perl/idea/editor/PerlBraceMatcher.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/editor/PerlBraceMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -70,7 +70,7 @@ public class PerlBraceMatcher implements PairedBraceMatcher, PerlElementTypes {
   };
 
   @Override
-  public @NotNull BracePair[] getPairs() {
+  public BracePair @NotNull [] getPairs() {
     return PAIRS;
   }
 
