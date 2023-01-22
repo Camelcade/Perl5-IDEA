a885a2a62a0b4511559d23fe9c32d0e2411783b2
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-22 17:57:21 +0400

    [Qodana] Annotated `PerlSyntaxHighlighter.getTokenHighlights` implementations

--- mojo/core/src/main/java/com/perl5/lang/mojolicious/idea/highlighter/MojoliciousSyntaxHighlighter.java
+++ mojo/core/src/main/java/com/perl5/lang/mojolicious/idea/highlighter/MojoliciousSyntaxHighlighter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,7 +62,7 @@ public class MojoliciousSyntaxHighlighter extends PerlSyntaxHighlighterEmbedded 
   }
 
   @Override
-  public @NotNull TextAttributesKey[] getTokenHighlights(IElementType tokenType) {
+  public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
     if (tokenType == MOJO_BEGIN || tokenType == MOJO_END) {
       tokenType = RESERVED_IF;
     }
