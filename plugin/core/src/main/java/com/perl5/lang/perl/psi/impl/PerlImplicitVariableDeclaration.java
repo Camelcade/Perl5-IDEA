edd8037ceba1517c395ac3f504c46de9533183b6
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-28 18:22:42 +0400

    [Qodana] Removed few unused external declarations related methods

--- plugin/core/src/main/java/com/perl5/lang/perl/psi/impl/PerlImplicitVariableDeclaration.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/psi/impl/PerlImplicitVariableDeclaration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -262,11 +262,6 @@ public class PerlImplicitVariableDeclaration extends PerlImplicitElement
   @Override
   public @NotNull PerlVariableAnnotations getLocalVariableAnnotations() {
     return PerlVariableAnnotations.empty();
-  }
-
-  @Override
-  public @Nullable PerlVariableAnnotations getExternalVariableAnnotations() {
-    return null;
   }
 
   @Override
