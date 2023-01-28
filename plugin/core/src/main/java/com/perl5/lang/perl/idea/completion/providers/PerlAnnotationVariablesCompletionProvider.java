a09893e8898fb0217363a43bd48eba2ac855dcab
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-28 18:25:54 +0400

    [Qodana] Removed redundant argument, changed methods names

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/completion/providers/PerlAnnotationVariablesCompletionProvider.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/completion/providers/PerlAnnotationVariablesCompletionProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,7 +77,7 @@ public class PerlAnnotationVariablesCompletionProvider extends PerlCompletionPro
     return !PerlVariableAnnotations.processAnnotations(variableDeclarationElement,
                                                        new PerlVariableAnnotations.VariableAnnotationProcessor() {
                                                          @Override
-                                                         public boolean process(@NotNull PerlAnnotationType annotationType) {
+                                                         public boolean processType(@NotNull PerlAnnotationType annotationType) {
                                                            return annotationType.equals(typeAnnotation) || annotationType.isWildCard();
                                                          }
                                                        });
