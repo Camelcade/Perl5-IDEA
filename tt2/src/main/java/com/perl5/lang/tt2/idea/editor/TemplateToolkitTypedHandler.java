eec58f9aa287e1c1a0b28e6d8624139e7cdd3cdf
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-28 17:54:16 +0400

    [Qodana] Removed unused method in TemplateToolkitTypedHandler

--- tt2/src/main/java/com/perl5/lang/tt2/idea/editor/TemplateToolkitTypedHandler.java
+++ tt2/src/main/java/com/perl5/lang/tt2/idea/editor/TemplateToolkitTypedHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,11 +40,11 @@ public class TemplateToolkitTypedHandler extends TypedHandlerDelegate implements
                                          @NotNull Editor editor,
                                          @NotNull PsiFile file,
                                          @NotNull FileType fileType) {
-    Result result = processChar(c, project, editor, file, fileType);
+    Result result = processChar(c, project, editor, file);
     return result == null ? super.beforeCharTyped(c, project, editor, file, fileType) : result;
   }
 
-  protected Result processChar(char c, Project project, Editor editor, PsiFile file, FileType fileType) {
+  protected Result processChar(char c, Project project, Editor editor, PsiFile file) {
     FileViewProvider viewProvider = file.getViewProvider();
     if (!(viewProvider instanceof TemplateToolkitFileViewProvider)) {
       return null;
