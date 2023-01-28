698cd2c60d0cd88066a6017cef18707bfb8240c6
Alexandr Evstigneev <hurricup@gmail.com>
2023-01-28 17:57:57 +0400

    [Qodana] Removed redundant stop() method from SelfStoppingVisitor

--- plugin/core/src/main/java/com/perl5/lang/perl/psi/PsiStubsAwareRecursiveVisitor.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/psi/PsiStubsAwareRecursiveVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,6 @@ public class PsiStubsAwareRecursiveVisitor extends PsiRecursiveElementVisitor im
     return myStopped;
   }
 
-  @Override
   public void stop() {
     myStopped = true;
   }
