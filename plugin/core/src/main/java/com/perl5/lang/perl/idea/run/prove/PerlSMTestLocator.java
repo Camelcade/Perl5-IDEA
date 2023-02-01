95a39c4afe6a18a7d91037fc681110d8174e2e56
Alexandr Evstigneev <hurricup@gmail.com>
2023-02-01 18:59:29 +0400

    [Qodana] Suppressed rawtype for locator
    See: IDEA-312166

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/run/prove/PerlSMTestLocator.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/run/prove/PerlSMTestLocator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ class PerlSMTestLocator implements SMTestLocator {
     myHostData = hostData;
   }
 
+  @SuppressWarnings("rawtypes")
   @Override
   public @NotNull List<Location> getLocation(@NotNull String protocol,
                                              @NotNull String path,
