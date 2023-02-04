0b6da3820227ef5bb0dbea9288ce4d9bf5588d30
Alexandr Evstigneev <hurricup@gmail.com>
2023-02-04 12:51:35 +0400

    [Qodana] fixed Bounded Wildcard in PerlDebuggerSettings

--- plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/PerlDebuggerSettings.java
+++ plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/PerlDebuggerSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -75,7 +75,7 @@ public class PerlDebuggerSettings extends XDebuggerSettings<PerlDebuggerSettings
   }
 
   @TestOnly
-  public void setDataRenderers(@NotNull List<Item> renderers) {
+  public void setDataRenderers(@NotNull List<? extends Item> renderers) {
     myDataRenderers = new ArrayList<>(renderers);
   }
 
