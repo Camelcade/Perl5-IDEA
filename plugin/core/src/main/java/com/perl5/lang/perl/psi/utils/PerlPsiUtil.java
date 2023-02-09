3cb97f8c47502e7ac67893a859846572106780a8
Alexandr Evstigneev <hurricup@gmail.com>
2023-02-09 09:15:23 +0400

    Moved and renamed PerlUtil.getParentsChain ->  PerlPsiUtil.dumpHierarchy

--- plugin/core/src/main/java/com/perl5/lang/perl/psi/utils/PerlPsiUtil.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/psi/utils/PerlPsiUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -871,6 +871,21 @@ public final class PerlPsiUtil implements PerlElementTypes {
       stubElement = ((StubBasedPsiElement<?>)element).getStub();
     }
     return stubElement;
+  }
+
+  public static @NotNull String dumpHierarchy(@Nullable PsiElement element) {
+    if (element == null) {
+      return "null";
+    }
+    StringBuilder sb = new StringBuilder();
+    while (true) {
+      sb.append(element.getClass()).append("(").append(PsiUtilCore.getElementType(element)).append(")").append(": ");
+      if (element instanceof PsiFile || element.getParent() == null) {
+        break;
+      }
+      element = element.getParent();
+    }
+    return sb.toString();
   }
 
   public abstract static class HeredocProcessor implements Processor<PsiElement> {
