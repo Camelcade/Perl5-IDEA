3914571081f34590d935a5b0dc6cc288c8ff5136
Alexandr Evstigneev <hurricup@gmail.com>
2023-03-08 08:39:33 +0400

    Cleaned up Terminal test code
    - got rid of deprecated methods
    - removed unused code

--- plugin/src/testFixtures/java/org/jetbrains/plugins/terminal/fixture/TestShellSession.java
+++ plugin/src/testFixtures/java/org/jetbrains/plugins/terminal/fixture/TestShellSession.java
@@ -3,19 +3,18 @@ package org.jetbrains.plugins.terminal.fixture;
 
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.project.Project;
-import com.jediterm.pty.PtyProcessTtyConnector;
+import com.intellij.terminal.pty.PtyProcessTtyConnector;
 import com.jediterm.terminal.TtyConnector;
 import com.pty4j.PtyProcess;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.terminal.JBTerminalSystemSettingsProvider;
 import org.jetbrains.plugins.terminal.LocalTerminalDirectRunner;
+import org.jetbrains.plugins.terminal.ShellStartupOptionsKt;
 import org.jetbrains.plugins.terminal.ShellTerminalWidget;
-import org.jetbrains.plugins.terminal.TerminalProcessOptions;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.function.BooleanSupplier;
 
@@ -29,22 +28,14 @@ public class TestShellSession {
     myWidget = new ShellTerminalWidget(project, settingsProvider, parentDisposable);
 
     LocalTerminalDirectRunner runner = LocalTerminalDirectRunner.createTerminalRunner(project);
-    PtyProcess process = runner.createProcess(new TerminalProcessOptions(project.getBasePath(), null, null), myWidget);
+    PtyProcess process = runner.createProcess(ShellStartupOptionsKt.shellStartupOptions(project.getBasePath()));
     TtyConnector connector = new PtyProcessTtyConnector(process, StandardCharsets.UTF_8);
     myWidget.start(connector);
-    myWatcher = new TestTerminalBufferWatcher(myWidget.getTerminalTextBuffer(), myWidget.getTerminal());
+    myWatcher = new TestTerminalBufferWatcher(myWidget.getTerminalTextBuffer());
   }
 
   public void executeCommand(@NotNull @NonNls String shellCommand) throws IOException {
     myWidget.executeCommand(shellCommand);
-  }
-
-  public void awaitScreenLinesEndWith(@NonNls List<String> expectedScreenLines, int timeoutMillis) {
-    myWatcher.awaitScreenLinesEndWith(expectedScreenLines, timeoutMillis);
-  }
-
-  public void awaitScreenLinesAre(@NonNls List<String> expectedScreenLines, int timeoutMillis) {
-    myWatcher.awaitScreenLinesAre(expectedScreenLines, timeoutMillis);
   }
 
   public void awaitBufferCondition(@NonNls BooleanSupplier condition, int timeoutMillis) {
