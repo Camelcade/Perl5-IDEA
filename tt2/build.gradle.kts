0ce0630cfe1c2b1d6239b63ced74b4ce00237230
Alexandr Evstigneev <hurricup@gmail.com>
2023-03-09 22:31:31 +0400

    #2620 set up fat jar generation for the tt2 plugin

--- tt2/build.gradle.kts
+++ tt2/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.intellij.tasks.PrepareSandboxTask
+
 /*
  * Copyright 2015-2021 Alexandr Evstigneev
  *
@@ -13,6 +15,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+plugins {
+  id("com.github.johnrengelman.shadow") version "8.1.0"
+}
+
 dependencies {
   listOf(
     ":plugin:core",
@@ -32,4 +38,15 @@ dependencies {
 
 intellij {
   plugins.set(listOf(project(":plugin")))
+}
+
+tasks {
+  withType<PrepareSandboxTask> {
+    pluginJar.set(shadowJar.flatMap {
+      it.archiveFile
+    })
+    runtimeClasspathFiles.set(project.files())
+
+    dependsOn(shadowJar)
+  }
 }
