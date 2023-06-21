fe180cfc9dec211406f8686d4aeac4ceff97ad57
Alexandr Evstigneev <hurricup@gmail.com>
2023-06-21 06:39:47 +0400

    Release 2023.3

--- plugin/core/src/main/java/com/perl5/errorHandler/YoutrackErrorHandler.java
+++ plugin/core/src/main/java/com/perl5/errorHandler/YoutrackErrorHandler.java
@@ -75,7 +75,7 @@ public class YoutrackErrorHandler extends ErrorReportSubmitter {
   public static final String YOUTRACK_PROPERTY_KEY = "youtrack.token";
   public static final String YOUTRACK_PROPERTY_VALUE = System.getProperty(YOUTRACK_PROPERTY_KEY);
   private static final String ADMIN_TOKEN = "Bearer " + YOUTRACK_PROPERTY_VALUE;
-  private static final String ACCESS_TOKEN = "Bearer perm:YXV0b3JlcG9ydGVy.NjEtMTg=.aUwXq1DfnSo6oWqAvwIkQdslqPIgwe";
+  private static final String ACCESS_TOKEN = "Bearer perm:YXV0b3JlcG9ydGVy.NjEtMTc=.9CG0QtdxziByP8GnxHaO0ssYxXeuVG";
 
   @Override
   public @NotNull String getReportActionText() {
