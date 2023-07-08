d6fad62d46f457df8160f983ef7b458d1fca6fd1
Alexandr Evstigneev <hurricup@gmail.com>
2023-07-08 14:30:50 +0400

    Fixed spacing for the fancy call
    Not sure why it started to fail right now, presumably caused by core modules update in #2714

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/formatter/PerlSpacingBuilderFactory.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/formatter/PerlSpacingBuilderFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,6 +64,7 @@ public class PerlSpacingBuilderFactory {
       .between(RESERVED_PACKAGE, PACKAGE).spaces(1)
       .between(PACKAGE, VERSION_ELEMENT).spaces(1)
       .between(RESERVED_FORMAT, SUB_NAME).spaces(1)
+      .betweenInside(SUB_NAME, PACKAGE, METHOD).spaces(1)
 
       .between(ATTRIBUTES, LEFT_PAREN).spaces(1)
       .beforeInside(LEFT_PAREN, SUB_OR_MODIFIER_DEFINITIONS_TOKENSET).spacing(
