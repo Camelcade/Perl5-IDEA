5337056b7b3bc313366aa26e2da7024b85b8bc22
Alexandr Evstigneev <hurricup@gmail.com>
2023-07-22 17:21:21 +0400

    Revert "Bump org.sonarqube from 4.2.1.3168 to 4.3.0.3225"
    This reverts commit 00ac4e570d649ce59404511b64629c2e892f5656.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ plugins {
   id("org.jetbrains.intellij") version "1.15.0"
   id("org.jetbrains.grammarkit") version "2022.3.1"
   id("com.github.kt3k.coveralls") version "2.12.2"
-  id("org.sonarqube") version "4.3.0.3225"
+  id("org.sonarqube") version "4.2.1.3168"
   id("org.jetbrains.qodana") version "0.1.13"
   id("org.jetbrains.kotlin.jvm") version "1.9.0"
 }
