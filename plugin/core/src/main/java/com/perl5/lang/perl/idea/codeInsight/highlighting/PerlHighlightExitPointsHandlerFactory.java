2330034af2919f5e72947b7e5d2d1044b5a47c62
Alexandr Evstigneev <hurricup@gmail.com>
2023-10-06 14:48:24 +0400

    CAMELCADE-12863 do not create handler for invalid target
    Actually this is a platform/sql fault, but may happen
    
    #CAMELCADE-12863 Fixed

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/codeInsight/highlighting/PerlHighlightExitPointsHandlerFactory.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/codeInsight/highlighting/PerlHighlightExitPointsHandlerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,6 +45,9 @@ public class PerlHighlightExitPointsHandlerFactory extends HighlightUsagesHandle
   public @Nullable HighlightUsagesHandlerBase<?> createHighlightUsagesHandler(@NotNull Editor editor,
                                                                               @NotNull PsiFile file,
                                                                               @NotNull PsiElement target) {
+    if( !target.isValid()){
+      return null;
+    }
     if (target instanceof PerlSubNameElement &&
         target.getParent() instanceof PsiPerlMethod &&
         target.getParent().getParent() instanceof PerlSubCallElement &&
