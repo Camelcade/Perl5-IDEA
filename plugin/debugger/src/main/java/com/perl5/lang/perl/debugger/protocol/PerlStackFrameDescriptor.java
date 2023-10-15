0d093a514f7ad673c21c53871b2ba88acf576404
Alexandr Evstigneev <hurricup@gmail.com>
2023-10-15 14:21:15 +0400

    #2780 Fixed line numbering for the frames
    Perl provides us zero-based line numbers

--- plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/protocol/PerlStackFrameDescriptor.java
+++ plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/protocol/PerlStackFrameDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2022 Alexandr Evstigneev
+ * Copyright 2015-2023 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,8 +33,12 @@ public class PerlStackFrameDescriptor {
     return file;
   }
 
-  public int getLine() {
+  public int getZeroBasedLine() {
     return line;
+  }
+
+  public int getOneBasedLine() {
+    return line + 1;
   }
 
   public PerlValueDescriptor[] getLexicals() {
