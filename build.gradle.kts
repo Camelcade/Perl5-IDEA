cf4044bec216d902f83319ad472869305b7651f9
Alexandr Evstigneev <hurricup@gmail.com>
2024-08-17 16:38:19 +0400

    Disabled plugin publishing for the root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -193,6 +193,10 @@ allprojects {
       verifyPlugin {
         enabled = false
       }
+      publishPlugin {
+        enabled = false
+      }
+
     }
     if( isPlugin ){
       publishPlugin {
