eac1718e68fe0f2b8eef189ccdfc9d07c5d47578
Alexandr Evstigneev <hurricup@gmail.com>
2024-10-19 19:09:29 +0400

    #2919 Improved annotations for unreachable code
    We now annotate only sub-ranges of an element if they are not reachable.
    
    Fixes #2919
    
    There is also a problem with CFG when die is in ternary operator condition. Technically it is wrong, but feels too artificial to handle.

--- plugin/core/src/main/java/com/perl5/lang/perl/idea/inspections/PerlUnreachableCodeInspection.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/idea/inspections/PerlUnreachableCodeInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2024 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,14 @@ package com.perl5.lang.perl.idea.inspections;
 import com.intellij.codeInsight.controlflow.ControlFlowUtil;
 import com.intellij.codeInsight.controlflow.Instruction;
 import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.psi.util.PsiUtilCore;
+import com.intellij.util.containers.ContainerUtil;
 import com.perl5.PerlBundle;
 import com.perl5.lang.perl.idea.codeInsight.controlFlow.PerlControlFlowBuilder;
 import com.perl5.lang.perl.psi.PerlMethodModifier;
@@ -32,8 +35,9 @@ import com.perl5.lang.perl.psi.PerlVisitor;
 import com.perl5.lang.perl.psi.PsiPerlSubExpr;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
 import static com.perl5.lang.perl.parser.PerlElementTypesGenerated.*;
 
@@ -43,9 +47,15 @@ public class PerlUnreachableCodeInspection extends PerlInspection {
     LP_OR_XOR_EXPR, EVAL_EXPR
   );
 
+  private final TokenSet NOT_ANNOTATABLE_TOKENS = TokenSet.create(
+    TokenType.WHITE_SPACE
+  );
+
   @Override
   public @NotNull PsiElementVisitor buildVisitor(@NotNull ProblemsHolder holder, boolean isOnTheFly) {
-    Set<PsiElement> notifiedElements = new HashSet<>();
+    var notifiedElements = new HashSet<PsiElement>();
+    var reachableRanges = new LinkedList<TextRange>();
+    var unreachableElements = new HashSet<PsiElement>();
     return new PerlVisitor() {
       @Override
       public void visitPerlSubDefinitionElement(@NotNull PerlSubDefinitionElement o) {
@@ -60,10 +70,40 @@ public class PerlUnreachableCodeInspection extends PerlInspection {
       private void processElement(@NotNull PsiElement o) {
         PerlControlFlowBuilder.iteratePrev(o, instruction -> {
           if (instruction.allPred().isEmpty() && instruction.num() != 0) {
-            processInstruction(instruction);
+            processUnreachableInstruction(instruction);
+          }
+          else {
+            registerReachableRange(instruction);
           }
           return ControlFlowUtil.Operation.NEXT;
         });
+        unreachableElements.forEach(this::annotateUnreachableElement);
+      }
+
+      private void registerReachableRange(@NotNull Instruction instruction) {
+        var element = instruction.getElement();
+        if (element == null || element instanceof PsiFile) {
+          return;
+        }
+        var elementRange = element.getTextRange();
+        if (elementRange.isEmpty()) {
+          return;
+        }
+        var rangesIterator = reachableRanges.iterator();
+        var toAdd = true;
+        while (rangesIterator.hasNext()) {
+          var range = rangesIterator.next();
+          if (elementRange.contains(range)) {
+            rangesIterator.remove();
+          }
+          else if (range.contains(elementRange)) {
+            toAdd = false;
+            break;
+          }
+        }
+        if (toAdd) {
+          reachableRanges.add(elementRange);
+        }
       }
 
       @Override
@@ -76,21 +116,61 @@ public class PerlUnreachableCodeInspection extends PerlInspection {
         processElement(file);
       }
 
-      private void processInstruction(@NotNull Instruction instruction) {
+      private void processUnreachableInstruction(@NotNull Instruction instruction) {
         PsiElement element = instruction.getElement();
-        if (element == null || !notifiedElements.add(element)) {
+        if (element == null) {
           return;
         }
 
         if (TRANSPARENT_ELEMENTS.contains(PsiUtilCore.getElementType(element))) {
           instruction.allSucc().forEach(it -> {
-            // this is just a weak check for next unreachable instruction
+            // this is just a weak check for the next unreachable instruction
             if (it.allPred().size() < 2) {
-              processInstruction(it);
+              processUnreachableInstruction(it);
             }
           });
         }
         else {
+          unreachableElements.add(element);
+        }
+      }
+
+      private void annotateUnreachableElement(PsiElement element) {
+        var firstChild = element.getFirstChild();
+        if (firstChild == null || firstChild == element.getLastChild()) {
+          doRegisterProblem(element);
+          return;
+        }
+
+        var reachableKids = new ArrayList<PsiElement>();
+        var unreachableKids = new ArrayList<PsiElement>();
+        var elementRange = element.getTextRange();
+        var reachableSubRanges = new LinkedList<>(ContainerUtil.filter(reachableRanges, elementRange::contains));
+        var childElement = firstChild;
+        while (childElement != null) {
+          var childElementType = PsiUtilCore.getElementType(childElement);
+          if (!NOT_ANNOTATABLE_TOKENS.contains(childElementType)) {
+            var childElementRange = childElement.getTextRange();
+            if (reachableSubRanges.removeIf(it -> it.contains(childElementRange))) {
+              reachableKids.add(childElement);
+            }
+            else {
+              unreachableKids.add(childElement);
+            }
+          }
+          childElement = childElement.getNextSibling();
+        }
+
+        if (reachableKids.isEmpty()) {
+          doRegisterProblem(element);
+        }
+        else {
+          unreachableKids.forEach(this::doRegisterProblem);
+        }
+      }
+
+      private void doRegisterProblem(@NotNull PsiElement element) {
+        if (notifiedElements.add(element)) {
           registerProblem(holder, element, PerlBundle.message("perl.inspection.unreachable.code"));
         }
       }
