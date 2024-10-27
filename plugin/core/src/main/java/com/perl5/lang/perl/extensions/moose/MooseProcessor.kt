106ddda6c0c9886700483ab9c2fe0ef95b29b907
Alexandr Evstigneev <hurricup@gmail.com>
2024-10-27 11:48:17 +0400

    [Qodana] Got rid of static initialization in MooseProcessor

--- plugin/core/src/main/java/com/perl5/lang/perl/extensions/moose/MooseProcessor.kt
+++ plugin/core/src/main/java/com/perl5/lang/perl/extensions/moose/MooseProcessor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Alexandr Evstigneev
+ * Copyright 2015-2024 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,63 +13,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.perl.extensions.moose
 
-package com.perl5.lang.perl.extensions.moose;
+import com.perl5.lang.perl.extensions.packageprocessor.PerlExportDescriptor
+import com.perl5.lang.perl.extensions.packageprocessor.PerlPackageLoader
+import com.perl5.lang.perl.extensions.packageprocessor.PerlPackageParentsProvider
+import com.perl5.lang.perl.extensions.packageprocessor.impl.BaseStrictWarningsProvidingProcessor
+import com.perl5.lang.perl.parser.moose.MooseSyntax
+import com.perl5.lang.perl.parser.moose.MooseSyntax.*
+import com.perl5.lang.perl.psi.impl.PerlUseStatementElement
+import com.perl5.lang.perl.util.PerlPackageUtil
+import kotlinx.collections.immutable.toImmutableList
 
-import com.perl5.lang.perl.extensions.packageprocessor.PerlExportDescriptor;
-import com.perl5.lang.perl.extensions.packageprocessor.PerlPackageLoader;
-import com.perl5.lang.perl.extensions.packageprocessor.PerlPackageParentsProvider;
-import com.perl5.lang.perl.extensions.packageprocessor.impl.BaseStrictWarningsProvidingProcessor;
-import com.perl5.lang.perl.psi.impl.PerlUseStatementElement;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static com.perl5.lang.perl.parser.moose.MooseSyntax.*;
-import static com.perl5.lang.perl.util.PerlPackageUtil.*;
-
-
-public class MooseProcessor extends BaseStrictWarningsProvidingProcessor implements
-                                                                         PerlPackageParentsProvider,
-                                                                         PerlPackageLoader {
-  protected static final List<String> LOADED_CLASSES = Arrays.asList(
-    PACKAGE_MOOSE_OBJECT, PACKAGE_CARP, PACKAGE_SCALAR_UTIL
-  );
-  protected static final List<String> PARENT_CLASSES = Collections.singletonList(PACKAGE_MOOSE_OBJECT);
-  static final List<PerlExportDescriptor> EXPORTS;
-
-  static {
-    List<PerlExportDescriptor> exports = new ArrayList<>();
-    exports.add(PerlExportDescriptor.create(PACKAGE_CARP, "confess"));
-    exports.add(PerlExportDescriptor.create(PACKAGE_SCALAR_UTIL, "blessed"));
-    exports.add(PerlExportDescriptor.create(PACKAGE_CLASS_MOP_MIXIN, MOOSE_KEYWORD_META));
-    Arrays.asList(MOOSE_KEYWORD_AFTER, MOOSE_KEYWORD_AROUND, MOOSE_KEYWORD_AUGMENT, MOOSE_KEYWORD_BEFORE, MOOSE_KEYWORD_EXTENDS,
-                  MOOSE_KEYWORD_HAS, MOOSE_KEYWORD_OVERRIDE, MOOSE_KEYWORD_WITH, MOOSE_KEYWORD_INNER, MOOSE_KEYWORD_SUPER).forEach(
-      it -> exports.add(PerlExportDescriptor.create(PACKAGE_MOOSE, it)));
-    EXPORTS = List.copyOf(exports);
+open class MooseProcessor : BaseStrictWarningsProvidingProcessor(), PerlPackageParentsProvider, PerlPackageLoader {
+  private val myLoadedClasses: List<String> by lazy {
+    listOf(PerlPackageUtil.PACKAGE_MOOSE_OBJECT, PerlPackageUtil.PACKAGE_CARP, PerlPackageUtil.PACKAGE_SCALAR_UTIL)
   }
 
-  @Override
-  public @NotNull List<String> getLoadedPackageNames(PerlUseStatementElement useStatement) {
-    return LOADED_CLASSES;
+  private val myParentClasses: List<String> = listOf(PerlPackageUtil.PACKAGE_MOOSE_OBJECT)
+
+  private val myExports: List<PerlExportDescriptor> by lazy {
+    (listOf(
+      PerlExportDescriptor.create(PerlPackageUtil.PACKAGE_CARP, "confess"),
+      PerlExportDescriptor.create(PerlPackageUtil.PACKAGE_SCALAR_UTIL, "blessed"),
+      PerlExportDescriptor.create(PerlPackageUtil.PACKAGE_CLASS_MOP_MIXIN, MooseSyntax.MOOSE_KEYWORD_META)
+    ) +
+      listOf(
+        MOOSE_KEYWORD_AFTER,
+        MOOSE_KEYWORD_AROUND,
+        MOOSE_KEYWORD_AUGMENT,
+        MOOSE_KEYWORD_BEFORE,
+        MOOSE_KEYWORD_EXTENDS,
+        MOOSE_KEYWORD_HAS,
+        MOOSE_KEYWORD_OVERRIDE,
+        MOOSE_KEYWORD_WITH,
+        MOOSE_KEYWORD_INNER,
+        MOOSE_KEYWORD_SUPER
+      ).map { keyword -> PerlExportDescriptor.create(PerlPackageUtil.PACKAGE_MOOSE, keyword) })
+      .toImmutableList()
   }
 
-  @Override
-  public void changeParentsList(@NotNull PerlUseStatementElement useStatement, @NotNull List<? super String> currentList) {
-    currentList.clear();
-    currentList.addAll(PARENT_CLASSES);
+  override fun getLoadedPackageNames(useStatement: PerlUseStatementElement): List<String> = myLoadedClasses
+
+  override fun changeParentsList(useStatement: PerlUseStatementElement, currentList: MutableList<in String>) {
+    currentList.clear()
+    currentList.addAll(myParentClasses)
   }
 
-  @Override
-  public boolean hasPackageFilesOptions() {
-    return false;
-  }
+  override fun hasPackageFilesOptions(): Boolean = false
 
-  @Override
-  public @NotNull List<PerlExportDescriptor> getImports(@NotNull PerlUseStatementElement useStatement) {
-    return EXPORTS;
-  }
+  override fun getImports(useStatement: PerlUseStatementElement): List<PerlExportDescriptor> = myExports
 }
