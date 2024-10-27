aa491a0d2fff0780dafba2e10fa16974d4ad5379
Alexandr Evstigneev <hurricup@gmail.com>
2024-10-27 09:24:43 +0400

    Hanging watcher tuning
    - echo jinfo
    - echo jstack
    - kill multiple times. For some reason may not kill at first attempt

--- watcher.sh
+++ watcher.sh
@@ -12,12 +12,20 @@ do
       echo Found stuck process $pid
       echo Writing jinfo.txt
       jinfo $pid > jinfo.txt
+      jinfo $pid
       echo Writing jstack.txt
       jstack -l -e $pid >jstack.txt
+      jstack -l -e $pid
       echo Writing snapshot.hprof
       jmap -dump:format=b,file=snapshot.hprof $pid
       echo KILLING HANGED PROCESS $pid
       kill -9 $pid
-      exit
+      # Wait briefly and verify termination
+      sleep 1
+      if kill -0 $pid 2>/dev/null; then
+       echo "Process $pid still alive after SIGKILL, this shouldn't happen"
+       # Log this unusual situation for investigation
+       ps -p $pid -o pid,ppid,user,stat,pcpu,comm
+      fi
     fi
 done
