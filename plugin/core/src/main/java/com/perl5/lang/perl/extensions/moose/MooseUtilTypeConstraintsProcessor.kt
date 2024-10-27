f1b2085ca65d0bf14bfce5e7210e5131f17960f5
Alexandr Evstigneev <hurricup@gmail.com>
2024-10-27 12:05:10 +0400

    [Qodana] Got rid of static initialization in MooseUtilTypeConstraintsProcessor

--- plugin/core/src/main/java/com/perl5/lang/perl/extensions/moose/MooseUtilTypeConstraintsProcessor.kt
+++ plugin/core/src/main/java/com/perl5/lang/perl/extensions/moose/MooseUtilTypeConstraintsProcessor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 Alexandr Evstigneev
+ * Copyright 2015-2024 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,33 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.perl.extensions.moose
 
-package com.perl5.lang.perl.extensions.moose;
+import com.perl5.lang.perl.extensions.packageprocessor.PerlExportDescriptor
+import com.perl5.lang.perl.extensions.packageprocessor.impl.BaseStrictWarningsProvidingProcessor
+import com.perl5.lang.perl.psi.impl.PerlUseStatementElement
+import com.perl5.lang.perl.util.PerlPackageUtil
+import kotlinx.collections.immutable.toImmutableList
 
-import com.perl5.lang.perl.extensions.packageprocessor.PerlExportDescriptor;
-import com.perl5.lang.perl.extensions.packageprocessor.impl.BaseStrictWarningsProvidingProcessor;
-import com.perl5.lang.perl.psi.impl.PerlUseStatementElement;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static com.perl5.lang.perl.util.PerlPackageUtil.PACKAGE_MOOSE_UTIL_TYPE_CONSTRAINTS;
-
-public class MooseUtilTypeConstraintsProcessor extends BaseStrictWarningsProvidingProcessor {
-  private static final List<PerlExportDescriptor> EXPORTS = new ArrayList<>();
-
-  static {
-    for (String name : Arrays.asList(
+class MooseUtilTypeConstraintsProcessor : BaseStrictWarningsProvidingProcessor() {
+  private val EXPORTS: List<PerlExportDescriptor> by lazy {
+    listOf(
       "as", "class_type", "coerce", "duck_type", "enum", "find_type_constraint", "from", "inline_as", "match_on_type", "maybe_type",
-      "message", "register_type_constraint", "role_type", "subtype", "type", "union", "via", "where")) {
-      EXPORTS.add(PerlExportDescriptor.create(PACKAGE_MOOSE_UTIL_TYPE_CONSTRAINTS, name));
-    }
+      "message", "register_type_constraint", "role_type", "subtype", "type", "union", "via", "where"
+    ).map { name -> PerlExportDescriptor.create(PerlPackageUtil.PACKAGE_MOOSE_UTIL_TYPE_CONSTRAINTS, name) }
+      .toImmutableList()
   }
 
-  @Override
-  public @NotNull List<PerlExportDescriptor> getImports(@NotNull PerlUseStatementElement useStatement) {
-    return EXPORTS;
-  }
+  override fun getImports(useStatement: PerlUseStatementElement): List<PerlExportDescriptor> = EXPORTS
 }
