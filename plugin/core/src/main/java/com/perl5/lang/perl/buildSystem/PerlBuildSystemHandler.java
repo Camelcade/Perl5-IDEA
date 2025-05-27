2b070941ad6b84614ba4112db416068855d764be
Alexandr Evstigneev <hurricup@gmail.com>
2025-05-27 09:42:12 +0400

    Cleaned up unnecessary modifiers

--- plugin/core/src/main/java/com/perl5/lang/perl/buildSystem/PerlBuildSystemHandler.java
+++ plugin/core/src/main/java/com/perl5/lang/perl/buildSystem/PerlBuildSystemHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Alexandr Evstigneev
+ * Copyright 2015-2025 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ public interface PerlBuildSystemHandler {
    * @return build system handler that {@link #handlesTests(Module)} this module. First wins.
    */
   @Contract("null->null")
-  public static @Nullable PerlBuildSystemHandler getTestsHandler(@Nullable Module module){
+  static @Nullable PerlBuildSystemHandler getTestsHandler(@Nullable Module module) {
     if( module == null){
       return null;
     }
