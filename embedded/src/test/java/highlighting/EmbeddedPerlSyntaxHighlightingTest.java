a60fca1ec311b2a80f99c2df8de18fa21fa4b96f
Alexandr Evstigneev <hurricup@gmail.com>
2025-05-30 08:50:04 +0400

    Suppressed SameParameterValue warning

--- embedded/src/test/java/highlighting/EmbeddedPerlSyntaxHighlightingTest.java
+++ embedded/src/test/java/highlighting/EmbeddedPerlSyntaxHighlightingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2020 Alexandr Evstigneev
+ * Copyright 2015-2025 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ public class EmbeddedPerlSyntaxHighlightingTest extends EmbeddedPerlLightTestCas
     doTest(true);
   }
 
-  private void doTest(boolean checkErrors) {
+  private void doTest(@SuppressWarnings("SameParameterValue") boolean checkErrors) {
     doTestHighlighter(checkErrors);
   }
 }
