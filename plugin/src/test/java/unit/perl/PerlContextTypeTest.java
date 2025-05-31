9716685537505c19c2e9163e9b9556d8c09aae37
Alexandr Evstigneev <hurricup@gmail.com>
2025-05-31 11:25:13 +0400

    Unused methods

--- plugin/src/test/java/unit/perl/PerlContextTypeTest.java
+++ plugin/src/test/java/unit/perl/PerlContextTypeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2019 Alexandr Evstigneev
+ * Copyright 2015-2025 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -199,10 +199,6 @@ public class PerlContextTypeTest extends PerlLightTestCase {
 
   private void assertList(@NotNull String text, @NotNull Class<? extends PsiElement> clazz) {
     doTest(text, clazz, PerlContextType.LIST);
-  }
-
-  private void assertVoid(@NotNull String text, @NotNull Class<? extends PsiElement> clazz) {
-    doTest(text, clazz, PerlContextType.VOID);
   }
 
   private void doTest(@NotNull String text, @NotNull Class<? extends PsiElement> clazz, @NotNull PerlContextType expectedType) {
