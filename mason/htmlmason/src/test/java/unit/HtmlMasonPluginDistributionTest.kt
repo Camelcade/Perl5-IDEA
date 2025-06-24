b50b7874f19a3240e883760f21a5fbb074c4349c
Alexandr Evstigneev <hurricup@gmail.com>
2025-06-24 17:19:25 +0400

    #3019 Added distribution tests introspecting test running environment
    Todo: make some consistency checks, like circular dependencies. Ideally - check if compilation deps matching runtime ones
    
    Also #3014

---
+++ mason/htmlmason/src/test/java/unit/HtmlMasonPluginDistributionTest.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2015-2025 Alexandr Evstigneev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit
+
+import base.PerlDistributionTestCase
+import org.junit.Test
+
+class HtmlMasonPluginDistributionTest : PerlDistributionTestCase() {
+  override fun getTestLibPath(): String = TEST_LIB_PATH_FROM_NESTED
+
+  @Test
+  fun testPlugin() = testDistribution("com.perl5.lang.mason.htmlmason")
+}
