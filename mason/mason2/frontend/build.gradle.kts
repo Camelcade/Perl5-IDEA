07b67a541f46073ccffc6bdf766b119643e8cd78
Alexandr Evstigneev <hurricup@gmail.com>
2025-06-24 15:15:56 +0400

    #2842 Added frontend modules for the Mason2 plugin
    - `.core` - backend (will rename it later, not decided yet)
    - `.common` - shared code
    - `.frontend` - UI
    - `.frontend.split` - JBC-only code

--- mason/mason2/frontend/build.gradle.kts
+++ mason/mason2/frontend/build.gradle.kts
@@ -13,36 +13,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-fun properties(key: String) = providers.gradleProperty(key)
 
 dependencies {
-  // packaging, which modules to include into this one
   listOf(
+    ":plugin.common",
     ":lang.mason.mason2.common",
-    ":lang.mason.mason2.core",
-    ":lang.mason.mason2.frontend",
-    ":lang.mason.mason2.frontend.split",
-  ).forEach {
-    runtimeOnly(project(it))
-  }
-
-  // compilation dependencies
-  listOf(
-    ":plugin.core",
-    ":lang.mason.mason2.core",
-    ":lang.mason.framework",
   ).forEach {
     compileOnly(project(it))
-    testCompileOnly(project(it))
   }
-  testImplementation(testFixtures(project(":plugin.testFixtures")))
-
   intellijPlatform {
     val platformVersionProvider: Provider<String> by rootProject.extra
-    create("IC", platformVersionProvider.get(), useInstaller = properties("useInstaller").get().toBoolean())
-    listOf(
-      ":plugin",
-      ":lang.mason.framework"
-    ).forEach { localPlugin(project(it)) }
+    create("IC", platformVersionProvider.get(), useInstaller = providers.gradleProperty("useInstaller").get().toBoolean())
   }
 }
