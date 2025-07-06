c9b67acf95b7c46540f37a2ce7244092aa0c2e54
Alexandr Evstigneev <hurricup@gmail.com>
2025-07-06 10:26:32 +0400

    Java to Kotlin

--- mojo/backend/src/main/java/com/perl5/lang/mojolicious/MojoliciousElementTypes.kt
+++ mojo/backend/src/main/java/com/perl5/lang/mojolicious/MojoliciousElementTypes.kt
@@ -13,41 +13,76 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.mojolicious
 
-package com.perl5.lang.mojolicious;
+import com.intellij.psi.templateLanguages.TemplateDataElementType
+import com.intellij.psi.tree.IElementType
+import com.perl5.lang.perl.psi.stubs.PerlFileElementType
+import com.perl5.lang.pod.elementTypes.PodTemplatingElementType
 
-import com.intellij.psi.templateLanguages.TemplateDataElementType;
-import com.intellij.psi.tree.IElementType;
-import com.perl5.lang.perl.psi.stubs.PerlFileElementType;
-import com.perl5.lang.pod.elementTypes.PodTemplatingElementType;
+interface MojoliciousElementTypes : MojoliciousSyntaxElements {
+  companion object {
+    @JvmField
+    val MOJO_TEMPLATE_BLOCK_HTML: IElementType = MojoTemplateTokenType()
 
+    @JvmField
+    val MOJO_OUTER_ELEMENT_TYPE: IElementType = MojoliciousTokenType("MOJO_OUTER_ELEMENT_TYPE")
 
-public interface MojoliciousElementTypes extends MojoliciousSyntaxElements {
-  IElementType MOJO_TEMPLATE_BLOCK_HTML = new MojoTemplateTokenType();
-  IElementType MOJO_OUTER_ELEMENT_TYPE = new MojoliciousTokenType("MOJO_OUTER_ELEMENT_TYPE");
-  IElementType MOJO_HTML_TEMPLATE_DATA =
-    new TemplateDataElementType("MOJO_HTML_TEMPLATE_DATA", MojoliciousLanguage.INSTANCE, MOJO_TEMPLATE_BLOCK_HTML, MOJO_OUTER_ELEMENT_TYPE);
-  IElementType MOJO_POD_TEMPLATE_DATA = new PodTemplatingElementType("MOJO_POD_TEMPLATE_DATA", MojoliciousLanguage.INSTANCE);
+    @JvmField
+    val MOJO_HTML_TEMPLATE_DATA: IElementType =
+      TemplateDataElementType("MOJO_HTML_TEMPLATE_DATA", MojoliciousLanguage.INSTANCE, MOJO_TEMPLATE_BLOCK_HTML, MOJO_OUTER_ELEMENT_TYPE)
 
-  IElementType MOJO_BLOCK_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_OPENER);
-  IElementType MOJO_BLOCK_EXPR_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_EXPR_OPENER);
-  IElementType MOJO_BLOCK_EXPR_ESCAPED_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_EXPR_ESCAPED_OPENER);
+    @JvmField
+    val MOJO_POD_TEMPLATE_DATA: IElementType = PodTemplatingElementType("MOJO_POD_TEMPLATE_DATA", MojoliciousLanguage.INSTANCE)
 
-  IElementType MOJO_BLOCK_CLOSER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_CLOSER);
-  IElementType MOJO_BLOCK_NOSPACE_CLOSER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_EXPR_NOSPACE_CLOSER);
-  IElementType MOJO_BLOCK_CLOSER_SEMI = new MojoliciousTokenType(";" + KEYWORD_MOJO_BLOCK_CLOSER);
-  IElementType MOJO_BLOCK_EXPR_NOSPACE_CLOSER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_EXPR_NOSPACE_CLOSER);
-  IElementType MOJO_BLOCK_EXPR_CLOSER = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_CLOSER);
+    @JvmField
+    val MOJO_BLOCK_OPENER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_OPENER)
 
-  IElementType MOJO_LINE_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_LINE_OPENER);
-  IElementType MOJO_LINE_EXPR_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_LINE_EXPR_OPENER);
-  IElementType MOJO_LINE_EXPR_ESCAPED_OPENER = new MojoliciousTokenType(KEYWORD_MOJO_LINE_EXPR_ESCAPED_OPENER);
+    @JvmField
+    val MOJO_BLOCK_EXPR_OPENER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_EXPR_OPENER)
 
-  IElementType MOJO_BLOCK_OPENER_TAG = new MojoliciousTokenType(KEYWORD_MOJO_BLOCK_OPENER_TAG);
-  IElementType MOJO_LINE_OPENER_TAG = new MojoliciousTokenType(KEYWORD_MOJO_LINE_OPENER_TAG);
+    @JvmField
+    val MOJO_BLOCK_EXPR_ESCAPED_OPENER: IElementType =
+      MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_EXPR_ESCAPED_OPENER)
 
-  IElementType MOJO_BEGIN = new MojoliciousTokenType(KEYWORD_MOJO_BEGIN);
-  IElementType MOJO_END = new MojoliciousTokenType(KEYWORD_MOJO_END);
+    @JvmField
+    val MOJO_BLOCK_CLOSER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_CLOSER)
 
-  PerlFileElementType FILE = new PerlFileElementType("Mojolicious Perl5 Template", MojoliciousLanguage.INSTANCE);
+    @JvmField
+    val MOJO_BLOCK_NOSPACE_CLOSER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_EXPR_NOSPACE_CLOSER)
+
+    @JvmField
+    val MOJO_BLOCK_CLOSER_SEMI: IElementType = MojoliciousTokenType(";" + MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_CLOSER)
+
+    @JvmField
+    val MOJO_BLOCK_EXPR_NOSPACE_CLOSER: IElementType =
+      MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_EXPR_NOSPACE_CLOSER)
+
+    @JvmField
+    val MOJO_BLOCK_EXPR_CLOSER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_CLOSER)
+
+    @JvmField
+    val MOJO_LINE_OPENER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_LINE_OPENER)
+
+    @JvmField
+    val MOJO_LINE_EXPR_OPENER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_LINE_EXPR_OPENER)
+
+    @JvmField
+    val MOJO_LINE_EXPR_ESCAPED_OPENER: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_LINE_EXPR_ESCAPED_OPENER)
+
+    @JvmField
+    val MOJO_BLOCK_OPENER_TAG: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BLOCK_OPENER_TAG)
+
+    @JvmField
+    val MOJO_LINE_OPENER_TAG: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_LINE_OPENER_TAG)
+
+    @JvmField
+    val MOJO_BEGIN: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_BEGIN)
+
+    @JvmField
+    val MOJO_END: IElementType = MojoliciousTokenType(MojoliciousSyntaxElements.KEYWORD_MOJO_END)
+
+    @JvmField
+    val FILE: PerlFileElementType = PerlFileElementType("Mojolicious Perl5 Template", MojoliciousLanguage.INSTANCE)
+  }
 }
