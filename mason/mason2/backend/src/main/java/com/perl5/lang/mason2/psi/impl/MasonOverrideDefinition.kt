25f93cfcba4ad6ec290aac9cf82e93218e93ddf8
Alexandr Evstigneev <hurricup@gmail.com>
2025-07-07 11:01:46 +0400

    J2K: MasonOverrideDefinition

--- mason/mason2/backend/src/main/java/com/perl5/lang/mason2/psi/impl/MasonOverrideDefinition.kt
+++ mason/mason2/backend/src/main/java/com/perl5/lang/mason2/psi/impl/MasonOverrideDefinition.kt
@@ -13,84 +13,58 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.mason2.psi.impl
 
-package com.perl5.lang.mason2.psi.impl;
+import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.IncorrectOperationException
+import com.perl5.lang.perl.extensions.PerlImplicitVariablesProvider
+import com.perl5.lang.perl.parser.moose.psi.impl.PerlMooseOverrideStatement
+import com.perl5.lang.perl.psi.PerlSubNameElement
+import com.perl5.lang.perl.psi.PerlVariableDeclarationElement
+import com.perl5.lang.perl.psi.PsiPerlBlock
+import com.perl5.lang.perl.psi.impl.PerlImplicitVariableDeclaration
+import com.perl5.lang.perl.psi.stubs.subsdefinitions.PerlSubDefinitionStub
 
-import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.tree.LeafPsiElement;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.IncorrectOperationException;
-import com.perl5.lang.perl.extensions.PerlImplicitVariablesProvider;
-import com.perl5.lang.perl.parser.moose.psi.impl.PerlMooseOverrideStatement;
-import com.perl5.lang.perl.psi.PerlSubNameElement;
-import com.perl5.lang.perl.psi.PerlVariableDeclarationElement;
-import com.perl5.lang.perl.psi.PsiPerlBlock;
-import com.perl5.lang.perl.psi.impl.PerlImplicitVariableDeclaration;
-import com.perl5.lang.perl.psi.stubs.subsdefinitions.PerlSubDefinitionStub;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+class MasonOverrideDefinition : PerlMooseOverrideStatement, PerlImplicitVariablesProvider {
+  private val myImplicitVariables: List<PerlVariableDeclarationElement> by lazy { buildImplicitVariables() }
 
-import java.util.ArrayList;
-import java.util.List;
+  constructor(node: ASTNode) : super(node)
 
+  constructor(stub: PerlSubDefinitionStub, nodeType: IElementType) : super(stub, nodeType)
 
-public class MasonOverrideDefinition extends PerlMooseOverrideStatement implements PerlImplicitVariablesProvider {
-  protected List<PerlVariableDeclarationElement> myImplicitVariables;
-
-  public MasonOverrideDefinition(@NotNull ASTNode node) {
-    super(node);
-  }
-
-  public MasonOverrideDefinition(@NotNull PerlSubDefinitionStub stub, @NotNull IElementType nodeType) {
-    super(stub, nodeType);
-  }
-
-  protected @NotNull List<PerlVariableDeclarationElement> buildImplicitVariables() {
-    List<PerlVariableDeclarationElement> newImplicitVariables = new ArrayList<>();
+  protected fun buildImplicitVariables(): MutableList<PerlVariableDeclarationElement> {
+    val newImplicitVariables: MutableList<PerlVariableDeclarationElement> = ArrayList()
     if (isValid()) {
-      newImplicitVariables.add(PerlImplicitVariableDeclaration.createInvocant(this));
+      newImplicitVariables.add(PerlImplicitVariableDeclaration.createInvocant(this))
     }
-    return newImplicitVariables;
+    return newImplicitVariables
   }
 
-  @Override
-  public @NotNull PsiPerlBlock getSubDefinitionBody() {
-    return findNotNullChildByClass(PsiPerlBlock.class);
-  }
+  override fun getSubDefinitionBody(): PsiPerlBlock = findNotNullChildByClass(PsiPerlBlock::class.java)
 
-  @Override
-  public @Nullable PsiElement getNameIdentifier() {
-    return PsiTreeUtil.getChildOfType(this, PerlSubNameElement.class);
-  }
+  override fun getNameIdentifier(): PsiElement? = PsiTreeUtil.getChildOfType(this, PerlSubNameElement::class.java)
 
-  @Override
-  protected @Nullable String getSubNameHeavy() {
-    PsiElement subNameElement = getNameIdentifier();
-    // fixme manipulator?
-    return subNameElement == null ? null : subNameElement.getNode().getText();
-  }
+  override val subNameHeavy: String?
+    get() = getNameIdentifier()?.getNode()?.getText()
 
-  @Override
-  public PsiElement setName(@NotNull String name) throws IncorrectOperationException {
+  @Throws(IncorrectOperationException::class)
+  override fun setName(name: String): PsiElement {
     if (name.isEmpty()) {
-      throw new IncorrectOperationException("You can't set an empty method name");
+      throw IncorrectOperationException("You can't set an empty method name")
+    }
+    val nameIdentifier = getNameIdentifier()
+    if (nameIdentifier is LeafPsiElement) {
+      nameIdentifier.replaceWithText(name)
     }
 
-    PsiElement nameIdentifier = getNameIdentifier();
-    if (nameIdentifier instanceof LeafPsiElement leafPsiElement) {
-      leafPsiElement.replaceWithText(name);
-    }
-
-    return this;
+    return this
   }
 
-  @Override
-  public @NotNull List<PerlVariableDeclarationElement> getImplicitVariables() {
-    if (myImplicitVariables == null) {
-      myImplicitVariables = buildImplicitVariables();
-    }
-    return myImplicitVariables;
+  override fun getImplicitVariables(): List<PerlVariableDeclarationElement> {
+    return myImplicitVariables
   }
 }
