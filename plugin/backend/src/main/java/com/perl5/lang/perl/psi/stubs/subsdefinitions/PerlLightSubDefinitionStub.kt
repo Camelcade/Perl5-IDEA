62ef954cd93bee2682e1d944347af51e0f153a55
Alexandr Evstigneev <hurricup@gmail.com>
2025-07-07 11:44:06 +0400

    J2K: PerlLightSubDefinitionStub

--- plugin/backend/src/main/java/com/perl5/lang/perl/psi/stubs/subsdefinitions/PerlLightSubDefinitionStub.kt
+++ plugin/backend/src/main/java/com/perl5/lang/perl/psi/stubs/subsdefinitions/PerlLightSubDefinitionStub.kt
@@ -13,61 +13,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.perl.psi.stubs.subsdefinitions
 
-package com.perl5.lang.perl.psi.stubs.subsdefinitions;
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.stubs.PsiFileStubImpl
+import com.intellij.psi.stubs.StubElement
+import com.intellij.psi.tree.IElementType
+import com.intellij.util.IncorrectOperationException
+import com.perl5.lang.perl.idea.codeInsight.typeInference.value.PerlValue
+import com.perl5.lang.perl.psi.stubs.PerlLightElementStub
+import com.perl5.lang.perl.psi.utils.PerlSubAnnotations
+import com.perl5.lang.perl.psi.utils.PerlSubArgument
 
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.stubs.PsiFileStubImpl;
-import com.intellij.psi.stubs.StubElement;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.util.IncorrectOperationException;
-import com.perl5.lang.perl.idea.codeInsight.typeInference.value.PerlValue;
-import com.perl5.lang.perl.psi.stubs.PerlLightElementStub;
-import com.perl5.lang.perl.psi.utils.PerlSubAnnotations;
-import com.perl5.lang.perl.psi.utils.PerlSubArgument;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+class PerlLightSubDefinitionStub(
+  private val myParent: StubElement<*>?,
+  packageName: String?,
+  subName: String,
+  arguments: List<PerlSubArgument>,
+  annotations: PerlSubAnnotations?,
+  returnValueFromCode: PerlValue,
+  elementType: IElementType
+) : PerlSubDefinitionStub(PsiFileStubImpl<PsiFile?>(null), packageName, subName, arguments, annotations, returnValueFromCode, elementType),
+    PerlLightElementStub {
+  private var myIsImplicit = false
 
-import java.util.List;
+  override fun getParentStub(): StubElement<*>? = myParent
 
-public class PerlLightSubDefinitionStub extends PerlSubDefinitionStub implements PerlLightElementStub {
-  private final @Nullable StubElement<?> myParent;
+  override fun <E : PsiElement?> getParentStubOfType(parentClass: Class<E?>) = throw IncorrectOperationException("NYI")
 
-  private boolean myIsImplicit = false;
+  override fun toString(): String = elementType.toString() + ":" + super.toString()
 
-  public PerlLightSubDefinitionStub(@Nullable StubElement<?> parent,
-                                    String packageName,
-                                    String subName,
-                                    @NotNull List<PerlSubArgument> arguments,
-                                    PerlSubAnnotations annotations,
-                                    @NotNull PerlValue returnValueFromCode,
-                                    IElementType elementType) {
-    super(new PsiFileStubImpl<>(null), packageName, subName, arguments, annotations, returnValueFromCode, elementType);
-    myParent = parent;
-  }
+  override fun isImplicit(): Boolean = myIsImplicit
 
-  @Override
-  public StubElement<?> getParentStub() {
-    return myParent;
-  }
-
-  @Override
-  public @Nullable <E extends PsiElement> E getParentStubOfType(@NotNull Class<E> parentClass) {
-    throw new IncorrectOperationException("NYI");
-  }
-
-  @Override
-  public String toString() {
-    return getElementType() + ":" + super.toString();
-  }
-
-  @Override
-  public boolean isImplicit() {
-    return myIsImplicit;
-  }
-
-  @Override
-  public void setImplicit(boolean isImplicit) {
-    myIsImplicit = isImplicit;
+  override fun setImplicit(isImplicit: Boolean) {
+    myIsImplicit = isImplicit
   }
 }
