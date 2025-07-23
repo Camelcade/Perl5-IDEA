579a54b5026fe7198898973beccf40dc4f553d2a
Alexandr Evstigneev <hurricup@gmail.com>
2025-07-23 12:08:34 +0400

    #2842 Moved class accessor stuff to the common

--- plugin/common/src/main/java/com/perl5/lang/perl/parser/Class/Accessor/psi/impl/PerlClassAccessorMethod.kt
+++ plugin/common/src/main/java/com/perl5/lang/perl/parser/Class/Accessor/psi/impl/PerlClassAccessorMethod.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.perl5.lang.perl.parser.Class.Accessor.psi.impl
 
 import com.intellij.openapi.util.NotNullFactory
