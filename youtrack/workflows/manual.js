54ba2c6491b39bf6d1960ad26fbe6c741eb02431
Alexandr Evstigneev <hurricup@gmail.com>
2025-10-18 13:24:58 +0400

    Added auto-duplication for CAMELCADE-24513
    Moved from list of maps to list of lists. Function can't be the key of the map.

--- youtrack/workflows/manual.js
+++ youtrack/workflows/manual.js
@@ -8,20 +8,26 @@ exports.rule = entities.Issue.onChange({
     return issue.is(ctx.State, ctx.State.Submitted);
   },
   action: function (ctx) {
-    const issuesMap = [
-      {'java.lang.Throwable: Stub index points to a file (file =': 'CAMELCADE-833'},
-      {'com.intellij.diagnostic.PluginException: Non-idempotent computation: it returns': 'CAMELCADE-2253'},
-      {'com.intellij.diagnostic.PluginException: Cannot load class com.perl5.lang.pod.idea.editor.PodSpellCheckingStrategy': 'CAMELCADE-24430'},
-      {'com.intellij.diagnostic.PluginException: Cannot load class com.perl5.lang.perl.idea.editor.PerlSpellCheckingStrategy': 'CAMELCADE-24430'},
-      {'com.intellij.util.io.CorruptedException: Storage corrupted': 'CAMELCADE-5481'},
-      {'com.intellij.psi.stubs.UpToDateStubIndexMismatch: PSI and index do not match.': 'CAMELCADE-13188'},
+    const patterns = [
+      ['java.lang.Throwable: Stub index points to a file (file =', 'CAMELCADE-833'],
+      ['com.intellij.diagnostic.PluginException: Non-idempotent computation: it returns', 'CAMELCADE-2253'],
+      ['com.intellij.diagnostic.PluginException: Cannot load class com.perl5.lang.pod.idea.editor.PodSpellCheckingStrategy',
+        'CAMELCADE-24430'],
+      ['com.intellij.diagnostic.PluginException: Cannot load class com.perl5.lang.perl.idea.editor.PerlSpellCheckingStrategy',
+        'CAMELCADE-24430'],
+      ['com.intellij.util.io.CorruptedException: Storage corrupted', 'CAMELCADE-5481'],
+      ['com.intellij.psi.stubs.UpToDateStubIndexMismatch: PSI and index do not match.', 'CAMELCADE-13188'],
+      [function (issue) {
+        return issue.description.includes('Read access is allowed from inside read-action only') &&
+            issue.description.includes('PerlXNamedValue.computeInlineDebuggerData');
+      }, 'CAMELCADE-24513'],
     ];
 
     let issue = ctx.issue;
     let description = issue.description;
 
-    for (const [pattern, targetIssueId] of Object.entries(issuesMap)) {
-      if (description.includes(pattern)) {
+    for (const [pattern, targetIssueId] of patterns) {
+      if (typeof pattern === 'function' && pattern(issue) || description.includes(pattern)) {
         let targetIssue = entities.Issue.findById(targetIssueId);
         if (!targetIssue) {
           console.error(`Issue ${targetIssueId} not found`);
