498eb82a279d5df85334c58384e1c276b3769a75
Alexandr Evstigneev <hurricup@gmail.com>
2026-03-01 11:51:34 +0400

    [qodana] Converted Java to Kotlin

--- plugin/frontend/src/main/java/com/perl5/lang/perl/idea/editor/smartkeys/options/PerlSmartKeyOptions.kt
+++ plugin/frontend/src/main/java/com/perl5/lang/perl/idea/editor/smartkeys/options/PerlSmartKeyOptions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,46 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.perl5.lang.perl.idea.editor.smartkeys.options
 
-package com.perl5.lang.perl.idea.editor.smartkeys.options;
+import com.intellij.openapi.options.BeanConfigurable
+import com.intellij.openapi.options.UnnamedConfigurable
+import com.intellij.openapi.util.Setter
+import com.intellij.ui.IdeBorderFactory
+import com.perl5.PerlBundle.message
+import com.perl5.lang.perl.idea.codeInsight.Perl5CodeInsightSettings
+import javax.swing.JComponent
 
-import com.intellij.openapi.options.BeanConfigurable;
-import com.intellij.openapi.options.UnnamedConfigurable;
-import com.intellij.ui.IdeBorderFactory;
-import com.perl5.PerlBundle;
-import com.perl5.lang.perl.idea.codeInsight.Perl5CodeInsightSettings;
-import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-
-
-public class PerlSmartKeyOptions extends BeanConfigurable<Perl5CodeInsightSettings> implements UnnamedConfigurable {
-  public PerlSmartKeyOptions() {
-    super(Perl5CodeInsightSettings.getInstance());
-    Perl5CodeInsightSettings settings = Perl5CodeInsightSettings.getInstance();
-    checkBox(PerlBundle.message("perl.options.auto.heredoc"),
-             () -> settings.HEREDOC_AUTO_INSERTION,
-             value -> settings.HEREDOC_AUTO_INSERTION = value);
-    checkBox(PerlBundle.message("perl.options.auto.colon"),
-             () -> settings.AUTO_INSERT_COLON,
-             value -> settings.AUTO_INSERT_COLON = value);
-    checkBox(PerlBundle.message("perl.options.auto.brace.substitution.hex"),
-             () -> settings.AUTO_BRACE_HEX_SUBSTITUTION,
-             value -> settings.AUTO_BRACE_HEX_SUBSTITUTION = value);
-    checkBox(PerlBundle.message("perl.options.auto.brace.substitution.oct"),
-             () -> settings.AUTO_BRACE_OCT_SUBSTITUTION,
-             value -> settings.AUTO_BRACE_OCT_SUBSTITUTION = value);
-    checkBox(PerlBundle.message("perl.options.smart.comma.sequence"),
-             () -> settings.SMART_COMMA_SEQUENCE_TYPING,
-             value -> settings.SMART_COMMA_SEQUENCE_TYPING = value);
+class PerlSmartKeyOptions : BeanConfigurable<Perl5CodeInsightSettings>(Perl5CodeInsightSettings.getInstance()), UnnamedConfigurable {
+  init {
+    val settings = Perl5CodeInsightSettings.getInstance()
+    checkBox(
+      message("perl.options.auto.heredoc"),
+      { settings.HEREDOC_AUTO_INSERTION },
+      Setter { value: Boolean? -> settings.HEREDOC_AUTO_INSERTION = value!! })
+    checkBox(
+      message("perl.options.auto.colon"),
+      { settings.AUTO_INSERT_COLON },
+      Setter { value: Boolean? -> settings.AUTO_INSERT_COLON = value!! })
+    checkBox(
+      message("perl.options.auto.brace.substitution.hex"),
+      { settings.AUTO_BRACE_HEX_SUBSTITUTION },
+      Setter { value: Boolean? -> settings.AUTO_BRACE_HEX_SUBSTITUTION = value!! })
+    checkBox(
+      message("perl.options.auto.brace.substitution.oct"),
+      { settings.AUTO_BRACE_OCT_SUBSTITUTION },
+      Setter { value: Boolean? -> settings.AUTO_BRACE_OCT_SUBSTITUTION = value!! })
+    checkBox(
+      message("perl.options.smart.comma.sequence"),
+      { settings.SMART_COMMA_SEQUENCE_TYPING },
+      Setter { value: Boolean? -> settings.SMART_COMMA_SEQUENCE_TYPING = value!! })
   }
 
-  @Override
-  public @NotNull JComponent createComponent() {
-    JComponent panel = super.createComponent();
+  override fun createComponent(): JComponent {
+    val panel = super.createComponent()
     panel.setBorder(
-      IdeBorderFactory.PlainSmallWithIndent.createTitledBorder(null, PerlBundle.message("border.title.perl5"), 0, 0, null, null));
+      IdeBorderFactory.PlainSmallWithIndent.createTitledBorder(null, message("border.title.perl5"), 0, 0, null, null)
+    )
 
-    return panel;
+    return panel
   }
 }
