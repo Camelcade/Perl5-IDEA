4b48ec686a65c9f0d5ddfc4231ad959381c5cfb9
Alexandr Evstigneev <hurricup@gmail.com>
2026-03-09 09:59:42 +0400

    [qodana] Fixed several capitalization problems

--- plugin/src/test/java/completion/PodCompletionTest.java
+++ plugin/src/test/java/completion/PodCompletionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -100,6 +100,7 @@ public class PodCompletionTest extends PodLightTestCase {
     doTestCompletionFromText(text);
   }
 
+  @SuppressWarnings("SameParameterValue")
   private void doTest(@NotNull String text, @NotNull String... expected) {
     doTestCompletionFromText(text, (element, presentation) -> {
       for (String string : expected) {
