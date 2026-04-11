99a454b58c053adf38a6097d6f37d327175fbe29
Alexandr Evstigneev <hurricup@gmail.com>
2026-04-11 14:54:45 +0400

    Improved toString for the light methods
    It is better for the test data and more readable in logs.

--- plugin/common/src/main/java/com/perl5/lang/perl/psi/light/PerlLightNamespaceDefinitionElement.java
+++ plugin/common/src/main/java/com/perl5/lang/perl/psi/light/PerlLightNamespaceDefinitionElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -171,6 +171,14 @@ public class PerlLightNamespaceDefinitionElement extends PerlDelegatingLightName
 
   @Override
   public String toString() {
-    return super.toString() + "@" + getNamespaceName();
+    return "PerlLightNamespaceDefinitionElement{" +
+           "myName='" + myName + '\'' +
+           ", myMroType=" + myMroType +
+           ", myParentNamespacesNames=" + myParentNamespacesNames +
+           ", myAnnotations=" + myAnnotations +
+           ", myExport=" + myExport +
+           ", myExportOk=" + myExportOk +
+           ", myExportTags=" + myExportTags +
+           '}';
   }
 }
