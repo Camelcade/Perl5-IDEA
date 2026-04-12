b1e7aa6912adbfba6f277413abc7108dc3199a2d
Alexandr Evstigneev <hurricup@gmail.com>
2026-04-12 12:49:17 +0400

    [qodana] Suppressed the warning for the serialization constructor

--- plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/protocol/PerlLoadedFileDescriptor.java
+++ plugin/debugger/src/main/java/com/perl5/lang/perl/debugger/protocol/PerlLoadedFileDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ public class PerlLoadedFileDescriptor {
   @Expose private @NlsSafe @NotNull String path;
   @Expose private @NlsSafe @Nullable String name;
 
+  @SuppressWarnings("unused")
   public PerlLoadedFileDescriptor() {
   }
 
