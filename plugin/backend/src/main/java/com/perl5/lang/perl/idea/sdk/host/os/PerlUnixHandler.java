1a1098f980621b38e38e298d0b4fb3843b9acb9b
Alexandr Evstigneev <hurricup@gmail.com>
2026-04-13 11:35:30 +0400

    [qodana] Removed now unused constructor from PerlUnixHandler

--- plugin/backend/src/main/java/com/perl5/lang/perl/idea/sdk/host/os/PerlUnixHandler.java
+++ plugin/backend/src/main/java/com/perl5/lang/perl/idea/sdk/host/os/PerlUnixHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,6 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 
 class PerlUnixHandler extends PerlOsHandler {
-  public PerlUnixHandler(@NotNull String name) {
-    super(name);
-  }
-
   public PerlUnixHandler(@NotNull String name, @Nullable Icon icon) {
     super(name, icon);
   }
