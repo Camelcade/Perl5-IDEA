2c9db1170610a20f53e546e6c55b4b73e2b30458
Alexandr Evstigneev <hurricup@gmail.com>
2026-05-23 15:06:56 +0400

    #3214 Moved structure view code to a separate optional module
    `plugin.structureView` depends on the `plugin.backend` because it intensively uses indexes

---
+++ plugin/structureView/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+
+dependencies {
+  listOf(
+    ":plugin.common",
+    ":plugin.backend",
+  ).forEach {
+    compileOnly(project(it))
+    testCompileOnly(project(it))
+  }
+
+  intellijPlatform {
+    val platformVersionProvider: Provider<String> by rootProject.extra
+    create(IntelliJPlatformType.IntellijIdeaUltimate, platformVersionProvider.get()){
+      useInstaller = providers.gradleProperty("useInstaller").get().toBoolean()
+    }
+    bundledPlugins(
+      "com.intellij.moduleSet.structureView",
+    )
+  }
+}
+
