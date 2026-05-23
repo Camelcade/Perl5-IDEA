b1bd933d9ca828478e0ce1eaa2aeb258a7dfdb23
Alexandr Evstigneev <hurricup@gmail.com>
2026-05-23 15:27:31 +0400

    #3214 created a structure view module for the mason2 plugin

---
+++ mason/mason2/structureView/build.gradle.kts
@@ -0,0 +1,18 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+
+dependencies {
+  listOf(
+    ":plugin.structureView",
+    ":lang.mojo.backend",
+  ).forEach {
+    compileOnly(project(it))
+    testCompileOnly(project(it))
+  }
+  intellijPlatform {
+    val platformVersionProvider: Provider<String> by rootProject.extra
+    create(IntelliJPlatformType.IntellijIdeaUltimate, platformVersionProvider.get()){
+      useInstaller = providers.gradleProperty("useInstaller").get().toBoolean()
+    }
+
+  }
+}
