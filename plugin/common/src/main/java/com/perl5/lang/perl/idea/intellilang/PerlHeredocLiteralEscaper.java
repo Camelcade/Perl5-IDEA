6e62adb898673712daa9b331fc07df4624a887a9
Alexandr Evstigneev <hurricup@gmail.com>
2026-08-23 15:08:09 +0400

    #3244 Marked heredoc escaper as one-line
    This is not true, but fixes things. Or I don't understand semantics, or it is just documented wrong.

--- plugin/common/src/main/java/com/perl5/lang/perl/idea/intellilang/PerlHeredocLiteralEscaper.java
+++ plugin/common/src/main/java/com/perl5/lang/perl/idea/intellilang/PerlHeredocLiteralEscaper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Alexandr Evstigneev
+ * Copyright 2015-2026 Alexandr Evstigneev
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ public class PerlHeredocLiteralEscaper extends LiteralTextEscaper<PerlHeredocEle
 
   @Override
   public boolean isOneLine() {
-    return false;
+    return true;
   }
 
   @Override
