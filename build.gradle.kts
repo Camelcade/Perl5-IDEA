dc18358dd3405b2a5d6643d25a458b09a3142699
Alexandr Evstigneev <hurricup@gmail.com>
2026-08-24 10:51:28 +0400

    Gradle plugin 2.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,8 @@ plugins {
   id("com.hurricup.gradle.fixcompress")
   id("idea")
   id("jacoco")
-  id("org.jetbrains.intellij.platform") version "2.18.1"
-  id("org.jetbrains.intellij.platform.grammarkit") version "2.18.1"
+  id("org.jetbrains.intellij.platform") version "2.19.0"
+  id("org.jetbrains.intellij.platform.grammarkit") version "2.19.0"
   id("com.github.nbaztec.coveralls-jacoco") version "1.2.20"
   id("org.sonarqube") version "7.5.0.8588"
   id("org.jetbrains.kotlin.jvm") version "2.4.10"
