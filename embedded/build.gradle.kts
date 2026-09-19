3f5e1c083249927f95ed0afe0a873348f3a5f143
Alexandr Evstigneev <hurricup@gmail.com>
2026-09-19 11:07:26 +0400

    Build 2026.3-EAP.5153.40

--- embedded/build.gradle.kts
+++ embedded/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     }
 
     testBundledPlugins(
-      "intellij.libraries.misc.plugin",
+      "com.intellij.modules.ultimate",
       "intellij.structuralSearch.plugin",
       "intellij.structureView.plugin",
       "intellij.testRunner.plugin",
--- mason/htmlmason/build.gradle.kts
+++ mason/htmlmason/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     }
 
     testBundledPlugins(
-      "intellij.libraries.misc.plugin",
+      "com.intellij.modules.ultimate",
       "intellij.structuralSearch.plugin",
       "intellij.structureView.plugin",
       "intellij.testRunner.plugin",
